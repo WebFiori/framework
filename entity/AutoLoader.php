5f8f7df8e53a80aee7bfcaf7072f5189c71d7fb1
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2018-03-06 13:17:36 +0300

    Autoloader implementation.

--- entity/AutoLoader.php
+++ entity/AutoLoader.php
@@ -1,1 +1,31 @@
-
+<?php
+/**
+ * PHP autoloader. It loads php classes as needed. First file to include.
+ */
+spl_autoload_register(function ($name) {
+    /**
+     * The root include directory.
+     */
+    if(defined('ROOT_DIR')){
+        $root = ROOT_DIR;
+    }
+    else{
+        $root = 'C:/Server/apache2/htdocs/y-project';
+    }
+    /**
+     * Folders to search in for required classes. Modify as needed.
+     */
+    $searchFolders = array(
+        '/entity',
+        '/entity/mysql',
+        '/entity/jsonx',
+        '/functions',
+        '/apis'
+    );
+    foreach ($searchFolders as $value) {
+        $f = $root.$value.'/'.$name.'.php';
+        if(file_exists($f)){
+            require $root.$value.'/'.$name.'.php';
+        }
+    }
+});

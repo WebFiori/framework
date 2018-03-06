1973632803a432ef62a5880b2f85702c9e5ad504
Ibrahim <ibinshikh@hotmail.com>
2018-03-07 01:23:46 +0300

    Updated Session Manager

--- entity/SessionManager.php
+++ entity/SessionManager.php
@@ -2,9 +2,44 @@
 /**
  * A helper class to manage system sessions.
  * @author Ibrahim <ibinshikh@hotmail.com>
- * @version 1.2
+ * @version 1.3
  */
 class SessionManager{
+    /**
+     * A constant that indicates the name of database host is missing.
+     * @var string Constant that indicates the name of database host is missing.
+     * @since 1.3
+     * @see @see SessionManager::useDb($dbAttrs=array())
+     */
+    const MISSING_DB_HOST = 'missing_db_host';
+    /**
+     * A constant that indicates the name of the database is missing.
+     * @var string Constant that indicates the name of the database is missing.
+     * @since 1.3
+     * @see 
+     */
+    const MISSING_DB_NAME = 'missing_db_name';
+    /**
+     * A constant that indicates username of the database is missing.
+     * @var string Constant that indicates username of the database is missing.
+     * @since 1.3
+     * @see 
+     */
+    const MISSING_DB_USER = 'missing_db_user';
+    /**
+     * A constant that indicates the user password of the database is missing.
+     * @var string Constant that indicates the user password of the database is missing.
+     * @since 1.3
+     * @see 
+     */
+    const MISSING_DB_PASS = 'missing_db_password';
+    /**
+     * A constant that indicates a database connection error has occur.
+     * @var string Constant that indicates a database connection error has occur.
+     * @since 1.3
+     * @see 
+     */
+    const DB_CONNECTION_ERR = 'unable_to_connect_to_db';
     private static $singleton;
     /**
      * An array of supported languages.
@@ -36,7 +71,7 @@ class SessionManager{
      * must be in the array <b>SessionManager::SUPOORTED_LANGS</b>.
      * @since 1.2
      */
-    private function initLang(){
+    public function initLang(){
         $lang = filter_input(INPUT_GET, 'lang');
         if($lang == FALSE || $lang == NULL){
             $lang = filter_input(INPUT_POST, 'lang');
@@ -119,54 +154,94 @@ class SessionManager{
         }
         return NULL;
     }
-
     /**
-     * Initialize the session.
-     * @param string $name The name of the session.
-     * @param string $dbhost The name of database host.
-     * @param string $dbUser The username of the database user.
-     * @param string $dbPass The password of the user.
-     * @param string $dbName The name of the selected database.
-     * @return boolean <b>TRUE</b> if the initialization was successful. <b>FALSE</b> 
-     * in case of error.
-     * @since 1.0
+     * Use database connection in the session.
+     * @param array $dbAttrs An associative array that contains database connection 
+     * parameters. The indices are: <b>'host'</b> (the value should be host name), 
+     * <b>'user'</b> (the value should database username), 
+     * <b>'pass'</b> (the value should be database username password)
+     * and <b>'db-name'</b> (the value should be database instance name). 
+     * @return boolean If the connection is established, the function will return <b>TRUE</b>. 
+     * In case the host name is missing, the function will return 
+     * <b>SessionManager::MISSING_DB_HOST</b>.
+     * In case the username is missing, the function will return 
+     * <b>SessionManager::MISSING_DB_USER</b>.
+     * In case the password is missing, the function will return 
+     * <b>SessionManager::MISSING_DB_PASS</b>.
+     * In case the database name is missing, the function will return 
+     * <b>SessionManager::MISSING_DB_NAME</b>. 
+     * In case of connection error, the function will return <b>SessionManger::DB_CONNECTION_ERR</b>. 
+     * To get more information about the connection error, you can get <b>DatabaseLink<b> 
+     * object using the function <b>SessionManager->getDBLink()</b>. 
+     * @since 1.3
      */
-    public function initSession($name='pa-sid',$dbhost='localhost',$dbUser='root',$dbPass='1',$dbName='test'){
-        if(!self::isStarted()){
-            session_name($name);
-            session_start();
-            $_SESSION['db'] = new DatabaseLink($dbhost,$dbUser,$dbPass);
-            if($_SESSION['db']->isConnected()){
-                if($_SESSION['db']->setDB($dbName)){
-                    $this->initLang();
-                    return TRUE;
+    public function useDb($dbAttrs=array()){
+        if($this->isStarted()){
+            if(isset($dbAttrs['host'])){
+                if(isset($dbAttrs['user'])){
+                    if(isset($dbAttrs['pass'])){
+                        if(isset($dbAttrs['db-name'])){
+                            $_SESSION['db'] = new DatabaseLink($dbAttrs['host'],$dbAttrs['user'],$dbAttrs['pass']);
+                            if($_SESSION['db']->isConnected()){
+                                if($_SESSION['db']->setDB($dbAttrs['db-name'])){
+                                    return TRUE;
+                                }
+                            }
+                            else{
+                                return self::DB_CONNECTION_ERR;
+                            }
+                        }
+                        else{
+                            return self::MISSING_DB_NAME;
+                        }
+                    }
+                    else{
+                        return self::MISSING_DB_PASS;
+                    }
                 }
                 else{
-                    self::kill();
-                    return FALSE;
+                    return self::MISSING_DB_USER;
                 }
             }
             else{
-                self::kill();
-                return FALSE;
+                return self::MISSING_DB_HOST;
             }
-        }
-        else{
-            return TRUE;
         }
     }
     /**
+     * Initialize the session.
+     * @return boolean|string <b>TRUE</b> if the initialization was successful. <b>FALSE</b> 
+     * in case of error. Also it is possible that the function will return one 
+     * of the database error messages.
+     * @see SessionManager::useDb($dbAttrs=array())
+     * @since 1.0
+     */
+    public function initSession($name='pa-sid',$useDb=false,$dbAttributes=array()){
+        if(!self::isStarted()){
+            session_name($name);
+            $started = session_start();
+        }
+        if($started){
+            if($useDb == TRUE){
+                return $this->useDb($dbAttributes);
+            }
+        }
+        return $started;
+    }
+    /**
      * Stops the session
+     * @return boolean <b>TRUE</b> if the session stopped. <b>FALSE</b> if not.
      * @since 1.0
      */
     public function kill(){
-        session_destroy();
+        return session_destroy();
     }
 
     /**
      * Returns the link that is used to connect to the database.
      * @return DatabaseLink|NULL An instance of <b>DatabaseLink</b> if the 
-     * session is running. <b>NULL</b> if the session is not running.
+     * session is running. <b>NULL</b> if the session is not running or the 
+     * session does not use database.
      */
     public function getDBLink(){
         if(self::isStarted()){

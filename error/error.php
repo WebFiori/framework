4121e5f0056e046d09eb54d5a168441f9a85c631
Ibrahim <ibinshikh@hotmail.com>
2018-03-07 07:04:23 +0300

    Updated Error Page

--- error/error.php
+++ error/error.php
@@ -1,7 +1,9 @@
 <?php 
 require_once '../root.php';
-require_once '../publish/page-attr.php';
-require_once 'ERR_'. SessionManager::get()->getLang().'.php';
+Util::displayErrors();
+PageAttributes::get()->loadTranslation(TRUE);
+PageAttributes::get()->loadTheme();
+require_once 'ERR_'.PageAttributes::get()->getLang().'.php';
 $error;
 $errInfo;
 $error = filter_input(INPUT_GET, 'err');
@@ -16,24 +18,26 @@ if($error != FALSE && $error != NULL){
         case 505:{$errInfo = ERR_505;break;}
     }
 }
+PageAttributes::get()->setTitle(ERR_PAGE_LANG['error'].' '.$error);
+PageAttributes::get()->setDescription(ERR_PAGE_LANG['error'].' '.$error.'. '.$errInfo['message']);
 ?>
 <!DOCTYPE html>
-<html <?php echo 'lang="'.SessionManager::get()->getLang().'"'?> >
+<html <?php echo 'lang="'.PageAttributes::get()->getLang().'"'?> >
     <head>
-        <?php echo staticHeadTag('errors/error?err='.$error, $_SESSION['lang'])?>
+        <?php echo staticHeadTag('errors/error?err='.$error, PageAttributes::get()->getLang())?>
     </head>
     <body itemscope itemtype="http://schema.org/WebPage">
         <div class="pa-container">
             <div class="pa-row">
                 <div class="pa-row">
-                    <div id="pa-main-content" itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage" dir="<?php echo PAGE_DIR?>" class="<?php echo 'pa-'.PAGE_DIR.'-col-twelve'?> show-border">
+                    <div id="pa-main-content" itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage" dir="<?php echo PageAttributes::get()->getWritingDir()?>" class="<?php echo 'pa-'.PageAttributes::get()->getWritingDir().'-col-twelve'?> show-border">
                         <header id="header" itemscope itemtype="http://schema.org/WPHeader" class="pa-row">
-                            <h1 name="page-title" itemprop="name" dir="<?php echo PAGE_DIR?>" id="page-title"><?php echo ERR_PAGE_LANG['error'].' '.$error?></h1>
+                            <h1 name="page-title" itemprop="name" dir="<?php echo PageAttributes::get()->getWritingDir()?>" id="page-title"><?php echo ERR_PAGE_LANG['error'].' '.$error?></h1>
                         </header>
                         <div class="pa-row">
                             <div class="pa-row">
                                 <h2 id="post-title"><?php echo $errInfo['type']?></h2>
-                                <div class="<?php echo 'pa-'.PAGE_DIR.'-col-twelve'?>">
+                                <div class="<?php echo 'pa-'.PageAttributes::get()->getWritingDir().'-col-twelve'?>">
                                     <p><?php echo $errInfo['message']?></p>
                                     <p><?php echo ERR_PAGE_LANG['req-url'].' '.Util::getRequestedURL()?></p>
                                     <p><a href="<?php echo SiteConfig::get()->getHomePage()?>"><?php echo ERR_PAGE_LANG['go-home']?></a></p>

607824117d8a2190e407f04f3cced8264a206748
Ibrahim <ibinshikh@hotmail.com>
2018-03-12 01:45:31 +0300

    Fix and update theme

--- publish/themes/greeny/aside-nav.php
+++ publish/themes/greeny/aside-nav.php
@@ -1,22 +1,27 @@
 <?php
 /**
  * This function is used to include aside navigation menu in the page.
- * @param string $asidMenuPath A path to .php file that has the method groupAside($active).
- * usually, each group of pages has one aside navigation menu.
+ * @param string $dir 
  * @param int $activeURL The number of the currently open page.
  * @return string HTML code.
  */
-function staticAsideNav($asidMenuPath,$activeURL=0){
-    require_once $asidMenuPath.'/aside-menu.php';
-    $aside = groupAside($activeURL);
-    return $aside.'';
+function staticAsideNav($dir,$activeURL=0){
+    $menu = new AsideMenu($dir);
+    $menu->addLink('pages/logout',LANGUAGE['aside']['logout']);
+    $menu->addLink('pages/home',LANGUAGE['aside']['home']);
+    $menu->addLink('pages/profile?user-id='.SessionManager::get()->getUser()->getID(),LANGUAGE['aside']['profile']);
+    $menu->addLink('pages/view-users',LANGUAGE['aside']['view-users']);
+    $menu->addLink('pages/register',LANGUAGE['aside']['add-user']);
+    $menu->addLink('pages/sys-info',LANGUAGE['aside']['sys-info']);
+    $menu->setActive($activeURL);
+    return ''.$menu;
 }
 /**
  * Returns a string of PHP code that can be used to include aside navigation in 
  * the page dynamically. 
  * @return string
  */
-function dynamicAsideNanv($path,$active=0){
-    return '<?php echo staticAsideNav(\''.$path.'\','.$active.')?>'; 
+function dynamicAsideNanv($dir,$active=0){
+    return '<?php echo staticAsideNav(\''.$dir.'\','.$active.')?>'; 
 }
 

7c2a3606f9ec752fb804076fce12d2934b709aaa
Ibrahim <ibinshikh@hotmail.com>
2018-04-02 16:02:30 +0300

    Added timezone

--- root.php
+++ root.php
@@ -4,6 +4,10 @@ mb_http_output('UTF-8');
 mb_http_input('UTF-8');
 mb_regex_encoding('UTF-8'); 
 /**
+ * See http://php.net/manual/en/timezones.php for supported time zones
+ */
+date_default_timezone_set('Asia/Riyadh');
+/**
  * The root directory that is used to load all other required system files.
  */
 define('ROOT_DIR',__DIR__);

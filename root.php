d4fcf0cac8d98634aa299d4ade1834dd7427e4c2
Ibrahim <ibinshikh@hotmail.com>
2018-05-15 19:29:19 +0300

    Added check for system configurartion files.

--- root.php
+++ root.php
@@ -17,3 +17,24 @@ define('ROOT_DIR',__DIR__);
 define('RES_FOLDER','res');
 require_once ROOT_DIR.'/entity/AutoLoader.php';
 Util::displayErrors();
+
+//at this stage, only check for configuration files
+//other errors checked as needed later.
+$GLOBALS['SYS_STATUS'] = Util::checkSystemStatus();
+if($GLOBALS['SYS_STATUS'] !== TRUE){
+    if($GLOBALS['SYS_STATUS'] == Util::MISSING_CONF_FILE){
+        SystemFunctions::get()->createConfigFile();
+        $GLOBALS['SYS_STATUS'] = Util::checkSystemStatus();
+    }
+    if(gettype($GLOBALS['SYS_STATUS']) == 'string' && $GLOBALS['SYS_STATUS'] == Util::MISSING_SITE_CONF_FILE){
+        SystemFunctions::get()->createSiteConfigFile();
+        $GLOBALS['SYS_STATUS'] = Util::checkSystemStatus();
+    }
+    if(gettype($GLOBALS['SYS_STATUS']) == 'string' && $GLOBALS['SYS_STATUS'] == Util::NEED_CONF && !defined('SETUP_MODE')){
+        header('content-type:application/json');
+        http_response_code(500);
+        die('{"message":"'.$GLOBALS['SYS_STATUS'].'","type":"error",'
+                . '"details":"System needs to be configured before using it.",'
+                . '"config-page":"'.Util::getBaseURL().'pages/setup/welcome"}');
+    }
+}

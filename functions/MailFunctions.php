17d8d9ebc1638b98c54bbb2740618a2a56ccb84c
Ibrahim <ibinshikh@hotmail.com>
2018-05-27 08:52:11 +0300

    Updated mail functions

--- functions/MailFunctions.php
+++ functions/MailFunctions.php
@@ -28,9 +28,15 @@
  * A class for the functions that is related to mailing.
  *
  * @author Ibrahim
- * @version 1.0
+ * @version 1.1
  */
 class MailFunctions extends Functions{
+    /**
+     * A constant that indicates a mail server address or its port 
+     * is invalid.
+     * @since 1.1
+     */
+    const INV_HOST_OR_PORT = 'inv_mail_host_or_port';
     /**
      *
      * @var MailFunctions 
@@ -49,9 +55,104 @@ class MailFunctions extends Functions{
         self::$instance = new MailFunctions();
         return self::$instance;
     }
-    
     public function __construct() {
         parent::__construct();
+    }
+    /**
+     * Creates the file 'MailConfig.php' if it does not exist.
+     * @since 1.0
+     */
+    public function createEmailConfigFile(){
+        if(!class_exists('MailConfig')){
+            $this->writeMailConfig(array());
+        }
+    }
+    /**
+     * A function to save changes to mail configuration file.
+     * @param type $emailAccountsArr An array that contains an objects of 
+     * type <b>EmailAccount</b>. 
+     * @since 1.1
+     */
+    private function writeMailConfig($emailAccountsArr){
+        $fh = new FileHandler(ROOT_DIR.'/MailConfig_test.php');
+        $fh->write('<?php', TRUE, TRUE);
+        $fh->write('/**
+ * A file that contains system email addresses configurations.
+ *
+ * @author Ibrahim
+ * @version 1.0
+ */', TRUE, TRUE);
+        $fh->write('class MailConfig{', TRUE, TRUE);
+        $fh->addTab();
+        //stat here
+        $fh->write('private $emailAccounts;
+    /**
+     *
+     * @var MailConfig 
+     * @since 1.0
+     */
+    private static $inst;
+    /**
+     * 
+     * @return MailConfig
+     * @since 1.0
+     */
+    public static function get(){
+        if(self::$inst !== NULL){
+            return self::$inst;
+        }
+        self::$inst = new MailConfig();
+        return self::$inst;
+    }', TRUE, TRUE);
+        $fh->write('private function __construct() {', TRUE, TRUE);
+        $fh->addTab();
+        $fh->reduceTab();
+        //adding email accounts
+        foreach ($emailAccountsArr as $emailAcc){
+            $fh->write('$acc1 = new EmailAccount();
+        $acc1->setServerAddress(\''.$emailAcc->getServerAddress().'\');
+        $acc1->setAddress(\''.$emailAcc->getAddress().'\');
+        $acc1->setUsername(\''.$emailAcc->getUsername().'\');
+        $acc1->setPassword(\''.$emailAcc->getPassword().'\');
+        $acc1->setName(\''.$emailAcc->getName().'\');
+        $acc1->setPort('.$emailAcc->getPort().');
+        $this->addAccount($acc1, \'no-replay\');',TRUE,TRUE);
+        }
+        $fh->write('}', TRUE, TRUE);
+        $fh->reduceTab();
+        $fh->write('/**
+     * Adds an email account.
+     * @param EmailAccount $acc an object of type <b>EmailAccount</b>.
+     * @param string $name A name to associate with the email account.
+     * @since 1.0
+     */
+    private function addAccount($acc,$name){
+        $this->emailAccounts[$name] = $acc;
+    }
+    /**
+     * Returns an email account given its name.
+     * @param string $name The name of the account.
+     * @return EmailAccount|null If the account is found, The function 
+     * will return an object of type <b>EmailAccount</b>. Else, the 
+     * function will return <b>NULL</b>.
+     * @since 1.0
+     */
+    public function getAccount($name){
+        if(isset($this->emailAccounts[$name])){
+            return $this->emailAccounts[$name];
+        }
+        return NULL;
+    }
+    /**
+     * Returns an array that contains all email accounts.
+     * @return array An array that contains all email accounts.
+     * @since 1.0
+     */
+    public function getAccounts(){
+        return $this->emailAccounts;
+    }', TRUE, TRUE);
+        $fh->write('}', TRUE, TRUE);
+        $fh->close();
     }
     /**
      * Sends a welcome email to a newly added account.
@@ -60,16 +161,24 @@ class MailFunctions extends Functions{
      */
     public function sendWelcomeEmail($user) {
         $noReplayAcc = MailConfig::get()->getAccount('no-replay');
-        $mailer = $this->getSocketMailer($noReplayAcc);
-        $mailer->addReceiver($user->getUserName(), $user->getEmail());
-        $mailer->setSubject('Activate Your Account');
-        $msg = '<p>Dear Mr. '.$user->getUserName().', Welcome to <b>'.SiteConfig::get()->getWebsiteName().'</b>.</p>';
-        $msg .= '<p>A new user account has been created for you. In order to start using '
-                . 'the system, you must activate your account by clicking on the '
-                . '<a href="'.SiteConfig::get()->getBaseURL().'apis/UserAPIs?action=activate-account&activation-token='.$user->getActivationTok().'" _target="_blank"><b>This Link</b></a> and logging in.</p>';
-        $msg .= '<p>Thank you for your time.</p>';
-        $msg .= '<p><b>'.$noReplayAcc->getName().'</b></p>';
-        $mailer->write($msg,TRUE);
+        if($noReplayAcc instanceof EmailAccount){
+            $mailer = $this->getSocketMailer($noReplayAcc);
+            $mailer->addReceiver($user->getUserName(), $user->getEmail());
+            $mailer->setSubject('Activate Your Account');
+            $msg = '<p>Dear Mr. '.$user->getUserName().', Welcome to <b>'.SiteConfig::get()->getWebsiteName().'</b>.</p>';
+            $msg .= '<p>A new user account has been created for you. In order to start using '
+                    . 'the system, you must activate your account by clicking on the '
+                    . '<a href="'.SiteConfig::get()->getBaseURL().'apis/UserAPIs?action=activate-account&activation-token='.$user->getActivationTok().'" _target="_blank"><b>This Link</b></a> and logging in.</p>';
+            $msg .= '<p>Thank you for your time.</p>';
+            $msg .= '<p><b>'.$noReplayAcc->getName().'</b></p>';
+            $mailer->write($msg,TRUE);
+        }
+    }
+    public function updateEmailAccount($emailAccount) {
+        if($emailAccount instanceof EmailAccount){
+            $sm = $this->getSocketMailer($emailAccount);
+            
+        }
     }
     /**
      * Returns a new instance of the class <b>SocketMailer</b>.

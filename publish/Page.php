cc4ef0945f1e2cf5ea34cf705d125fb240b5a94d
Ibrahim <ibinshikh@hotmail.com>
2018-06-03 22:03:57 +0300

    Updated Page class
    - Added attribute 'theme'
    - Added functions related to theme.
    - Changed way theme is loaded

--- publish/Page.php
+++ publish/Page.php
@@ -6,7 +6,7 @@ define('THEMES_DIR','publish/themes');
 /**
  * A class used to initialize main page components.
  * @author Ibrahim <ibinshikh@hotmail.com>
- * @version 1.4
+ * @version 1.6
  */
 class Page{
     /**
@@ -76,6 +76,12 @@ class Page{
      */
     private $isThemeLoaded;
     /**
+     * An array that contains loaded theme info.
+     * @var array 
+     * @since 1.6
+     */
+    private $theme;
+    /**
      * Sets the canonical URL of the page.
      * @since 1.2
      * @param string $url The canonical URL of the page.
@@ -125,6 +131,9 @@ class Page{
      */
     public function setTitle($val){
         $this->title = $val;
+        if($this->document != NULL){
+            $this->document->getHeadNode()->setTitle($this->getTitle().SiteConfig::get()->getTitleSep().SiteConfig::get()->getWebsiteName());
+        }
     }
     /**
      * Returns the document that is associated with the page.
@@ -141,6 +150,9 @@ class Page{
                 $this->document->setHeadNode($this->getHead($dynamic));
                 $this->document->addNode($this->getHeader($dynamic));
                 $this->document->addNode($this->getFooter($dynamic));
+                $this->document->getHeadNode()->setBase(SiteConfig::get()->getBaseURL());
+                $this->document->getHeadNode()->addMeta('description', $this->getDescription());
+                $this->document->getHeadNode()->setTitle($this->getTitle().SiteConfig::get()->getTitleSep().SiteConfig::get()->getWebsiteName());
             }
             return $this->document;
         }
@@ -170,6 +182,9 @@ class Page{
      */
     public function setDescription($val){
         $this->description = $val;
+        if($this->document != NULL){
+            $this->document->getHeadNode()->addMeta('description', $val);
+        }
     }
     /**
      * Returns the description of the page.
@@ -256,7 +271,8 @@ class Page{
     }
     /**
      * Returns an array that contains the meta data of all available themes. 
-     * @return array An array that contains the meta data of all available themes.
+     * @return array An associative array that contains all themes information. The key 
+     * is the theme name and the value is theme info.
      * @throws Exception If the constant 'ROOT_DIR' is not defined.
      * @since 1.1 
      */
@@ -265,8 +281,11 @@ class Page{
             $themeNames = array();
             $themesDirs = array_diff(scandir(ROOT_DIR.'/'.THEMES_DIR), array('..', '.'));
             foreach ($themesDirs as $dir){
-                include ROOT_DIR.THEMES_DIR.'/'.$dir.'/theme.php';
-                array_push($themeNames, $GLOBALS['THEME_META']);
+                include ROOT_DIR.'/'.THEMES_DIR.'/'.$dir.'/theme.php';
+                if(isset($GLOBALS['THEME'])){
+                    $themeNames[$GLOBALS['THEME']['META']['name']] = $GLOBALS['THEME'];
+                    unset($GLOBALS['THEME']);
+                }
             }
             return $themeNames;
         }
@@ -277,36 +296,116 @@ class Page{
      * @return boolean <b>TRUE</b> if selected theme is loaded.
      * @since 1.0
      * @throws Exception If the constant 'ROOT_DIR' is not defined.
+     * @deprecated since version 1.4
      */
     public function loadAdminTheme(){
+        return $this->usingTheme(SiteConfig::get()->getAdminThemeName());
+    }
+    /**
+     * Loads a theme given its name.
+     * @param string $themeName [Optional] The name of the theme as specified by the 
+     * variable 'name' in theme definition. If the given name is <b>NULL</b>, the 
+     * function will load the default theme as specified by the function 
+     * <b>SiteConfig::getBaseThemeName()</b>.
+     * @return boolean The function will return <b>TRUE</b> once the 
+     * theme is loaded.
+     * @throws Exception The first case that the function will throw an exception 
+     * is when the constant 'ROOT_DIR' is not defined. Also the function will throw 
+     * an exception if no theme was found which has the given name. Another case is 
+     * when the file 'theme.php' of the theme is missing. Also if the variabale 
+     * 'THEME_COMPONENTS' is not defined. Finally, an exception will be thrown 
+     * if theme component is not found.
+     * @since 1.4
+     */
+    public function usingTheme($themeName=null) {
         if(defined('ROOT_DIR')){
-            $themeDir = ROOT_DIR.'/'.SiteConfig::get()->getAdminThemeDir();
-            require_once $themeDir.'/theme.php';
-            foreach ($GLOBALS['THEME_COMPONENTS'] as $component){
-                require_once $themeDir.'/'.$component;
+            if($themeName == NULL){
+                $themeName = SiteConfig::get()->getBaseThemeName();
             }
-            $this->isThemeLoaded = TRUE;
-            return TRUE;
+            $themes = $this->getAvailableThemes();
+            if(isset($themes[$themeName])){
+                $this->theme = $themes[$themeName];
+                $theme = $this->getTheme();
+                $themeDir = ROOT_DIR.'/'.THEMES_DIR.'/'.$theme['META']['directory'];
+                require_once $themeDir.'/theme.php';
+                if(isset($theme['COMPONENTS'])){
+                    foreach ($theme['COMPONENTS'] as $component){
+                        if(file_exists($themeDir.'/'.$component)){
+                            require_once $themeDir.'/'.$component;
+                        }
+                        else{
+                            throw new Exception('Component \''.$component.'\' of the theme not found. Eather define it or remove it from the array \'COMPONENTS\'.');
+                        }
+                    }
+                    $this->isThemeLoaded = TRUE;
+                    return TRUE;
+                }
+                throw new Exception('The variable \'COMPONENTS\' is missing from the theme definition.');
+            }
+            throw new Exception('No such theme: \''.$themeName.'\'.');
         }
         throw new Exception('Unable to load theme because root directory is not defined.');
+    }
+    /**
+     * Returns the directory at which CSS files of the theme exists.
+     * @return string The directory at which CSS files of the theme exists 
+     * (e.g. 'publish/my-theme/css' ). 
+     * If the theme is not loaded, the function will return empty string.
+     * @since 1.6
+     */
+    public function getThemeCSSDir() {
+        if($this->isThemeLoaded()){
+            $theme = $this->getTheme();
+            return THEMES_DIR.'/'.$theme['css-dir'];
+        }
+        return '';
+    }
+    /**
+     * Returns the directory at which image files of the theme exists.
+     * @return string The directory at which image files of the theme exists 
+     * (e.g. 'publish/my-theme/images' ). 
+     * If the theme is not loaded, the function will return empty string.
+     * @since 1.6
+     */
+    public function getThemeImagesDir() {
+        if($this->isThemeLoaded()){
+            $theme = $this->getTheme();
+            return THEMES_DIR.'/'.$theme['images-dir'];
+        }
+        return '';
+    }
+    /**
+     * Returns the directory at which JavaScript files of the theme exists.
+     * @return string The directory at which JavaScript files of the theme exists 
+     * (e.g. 'publish/my-theme/js' ). 
+     * If the theme is not loaded, the function will return empty string.
+     * @since 1.6
+     */
+    public function getThemeJSDir() {
+        if($this->isThemeLoaded()){
+            $theme = $this->getTheme();
+            return THEMES_DIR.'/'.$theme['js-dir'];
+        }
+        return '';
+    }
+    /**
+     * Returns an array that contains theme information.
+     * @return array|NULL An array that contains theme information. If the theme 
+     * is not loaded, the function will return <b>NULL</b>.
+     * @since 1.6
+     */
+    public function getTheme() {
+        return $this->theme;
     }
     /**
      * Loads the selected web site theme.
      * @return boolean <b>TRUE</b> if selected theme is loaded.
      * @since 1.0
      * @throws Exception If the constant 'ROOT_DIR' is not defined.
+     * @deprecated since version 1.4
      */
     public function loadTheme(){
-        if(defined('ROOT_DIR')){
-            $themeDir = ROOT_DIR.'/'.SiteConfig::get()->getThemeDir();
-            require_once $themeDir.'/theme.php';
-            foreach ($GLOBALS['THEME_COMPONENTS'] as $component){
-                require_once $themeDir.'/'.$component;
-            }
-            $this->isThemeLoaded = TRUE;
-            return TRUE;
-        }
-        throw new Exception('Unable to load theme because root directory is not defined.');
+        return $this->usingTheme(SiteConfig::get()->getBaseThemeName());
     }
     /**
      * Returns the writing direction of the page.
@@ -381,19 +480,20 @@ class Page{
         if($this->hasHeader()){
             if($dynamic){
                 $node = new HTMLNode('', FALSE, TRUE);
-                $node->setText('<?php getHeaderNode() ?>');
+                $node->setText('<?php echo getHeaderNode() ?>');
                 return $node;
             }
             else{
-                return dynamicPageHeader();
+                return getHeaderNode();
             }
         }
     }
+    
     private function getFooter($dynamic=true){
         if($this->hasFooter()){
             if($dynamic){
                 $node = new HTMLNode('', FALSE, TRUE);
-                $node->setText('<?php getFooterNode() ?>');
+                $node->setText('<?php echo getFooterNode() ?>');
                 return $node;
             }
             else{
@@ -405,10 +505,10 @@ class Page{
     private function getHead($dynamic=true){
         if($dynamic){
             $textNode = new HTMLNode('', FALSE, TRUE);
-            $textNode->setText('<?php getHeadNode(\''.$this->getCanonical().'\',\''.$this->getTitle().'\',\''.$this->getDescription().'\') ?>');
+            $textNode->setText('<?php echo getHeadNode() ?>');
             return $textNode;
         }
-        return getHeadNode($this->getCanonical(), $this->getTitle(), $this->getDescription());
+        return getHeadNode();
     }
 }
 

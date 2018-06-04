edae45a99987b2687d4bea4c691657efd332ea29
Ibrahim <ibinshikh@hotmail.com>
2018-06-04 13:21:32 +0300

    Updated mail config file

--- MailConfig.php
+++ MailConfig.php
@@ -1,36 +1,11 @@
 <?php
-
-/*
- * The MIT License
- *
- * Copyright 2018 Ibrahim.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 /**
- * A file that contains system emails configurations.
+ * A file that contains system email addresses configurations.
  *
  * @author Ibrahim
  * @version 1.0
  */
-class MailConfig {
+class MailConfig{
     private $emailAccounts;
     /**
      *
@@ -50,9 +25,8 @@ class MailConfig {
         self::$inst = new MailConfig();
         return self::$inst;
     }
-    
     private function __construct() {
-        $acc1 = new EmailAccount();
+    $acc1 = new EmailAccount();
         $acc1->setServerAddress('mail.programmingacademia.com');
         $acc1->setAddress('no-replay@programmingacademia.com');
         $acc1->setUsername('no-replay@programmingacademia.com');
@@ -60,9 +34,8 @@ class MailConfig {
         $acc1->setName('Programming Academia Team');
         $acc1->setPort(25);
         $this->addAccount($acc1, 'no-replay');
-        
     }
-    /**
+/**
      * Adds an email account.
      * @param EmailAccount $acc an object of type <b>EmailAccount</b>.
      * @param string $name A name to associate with the email account.

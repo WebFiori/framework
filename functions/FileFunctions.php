c812656d3bb807f331da0222d1c9e71e84505c0c
Ibrahim <ibinshikh@hotmail.com>
2018-06-18 10:18:29 +0300

    Fully implemented controller

--- functions/FileFunctions.php
+++ functions/FileFunctions.php
@@ -25,30 +25,47 @@
  */
 
 /**
- * Description of FileFunctions
+ * A basic file related operations controller. Extend it to 
+ * implement more complex file related operations.
  *
- * @author Ibrahim
+ * @author Ibrahim <ibinshikh@hotmail.com>
  * @version 1.0
  */
 class FileFunctions extends Functions{
+    /**
+     * A constant that is used to indicates no upload types where added.
+     * @since 1.0
+     */
     const NO_TYPES = 'no_types_added';
+    /**
+     * A constant that is used to indicates upload directory does not exists.
+     * @since 1.0
+     */
     const NO_SUCH_DIR = 'no_such_dir';
+    
     const NOT_EXIST = 'not_exist';
+    /**
+     * A constant that is used to indicates uploaded file type is not allowed.
+     * @since 1.0
+     */
     const NOT_ALLOWED = 'not_allowed_type';
+    /**
+     * A constant that is used to indicates that a file does not exists.
+     * @since 1.0
+     */
     const NO_SUCH_FILE = 'no_such_file';
     /**
-     *
-     * @var type 
+     * An associative array of allowed upload file types.
+     * @var array 
      * @since 1.0
      */
     private $uploadTypes;
     /**
-     *
-     * @var type 
+     * An object of type <b>FileQuery</b>.
+     * @var fileQuery 
      * @since 1.0
      */
     private $fileQuery;
-    private $files;
     /**
      * @since 1.0
      */
@@ -58,9 +75,12 @@ class FileFunctions extends Functions{
         $this->uploadTypes = array();
     }
     /**
-     * 
-     * @param type $type
-     * @return boolean
+     * Adds a file type from the allowed upload types.
+     * @param string $type File extention (such as 'jpg' or 'mp4'). An array 
+     * of supported types can be found at the constant array <b>Uploader::ALLOWED_FILE_TYPES</b>.
+     * @return boolean The function will return <b>TRUE</b> once the type 
+     * is added. <b>FALSE</b> if it is not added. The type won't be added 
+     * if its not supported.
      * @since 1.0
      */
     public function addFileType($type) {
@@ -73,9 +93,10 @@ class FileFunctions extends Functions{
         return FALSE;
     }
     /**
-     * 
-     * @param type $type
-     * @return boolean
+     * Removes a file type from the allowed upload types.
+     * @param string $type File extention (such as 'jpg' or 'mp4').
+     * @return boolean The function will return <b>TRUE</b> once the type 
+     * is removed. <b>FALSE</b> if it is not exists on the added types.
      * @since 1.0
      */
     public function removeFileType($type) {
@@ -86,13 +107,13 @@ class FileFunctions extends Functions{
         return FALSE;
     }
     /**
-     *
+     * A single instance of the class.
      * @var FileFunctions
      * @since 1.0 
      */
     private static $instance;
     /**
-     * 
+     * Returns a single instance of the class.
      * @return FileFunctions
      * @since 1.0
      */
@@ -103,13 +124,68 @@ class FileFunctions extends Functions{
         self::$instance = new FileFunctions();
         return self::$instance;
     }
-    public function getFileInfo($fileId) {
-        
+    /**
+     * Removes a file from the database given its ID.
+     * @param int $fId The ID of the file.
+     * @return File|string If the file was removed, the function will 
+     * return <b>TRUE</b>. If a database error has occurred while removing the file, 
+     * the function will return <b>MySQLQuery::QUERY_ERR</b>. If the file does not exists, 
+     * the function will return <b>FileFunctions::NO_SUCH_FILE</b>.
+     * @since 1.0
+     */
+    public function removeFile($fId) {
+        $this->useDatabase();
+        $file = $this->getFile($fId);
+        if($file instanceof File){
+            $this->fileQuery->removeFile($fId);
+            if($this->excQ($this->fileQuery)){
+                return $file;
+            }
+            return MySQLQuery::QUERY_ERR;
+        }
+        return $file;
     }
     /**
-     * 
-     * @param type $fileId
-     * @return File
+     * Returns the size of a file in bytes given its ID.
+     * @param int $id The ID of the file.
+     * @return int|string The function will return the size of the file in bytes
+     * if it was found. If a database error has occurred while getting the size, 
+     * the function will return <b>MySQLQuery::QUERY_ERR</b>. If the file does not exists, 
+     * the function will return <b>FileFunctions::NO_SUCH_FILE</b>.
+     * @since 1.0
+     */
+    public function getFileSize($id){
+        $this->useDatabase();
+        $this->fileQuery->fileSize($id);
+        if($this->excQ($this->fileQuery)){
+            $row = $this->getRow();
+            if($row != NULL){
+                return intval($row['file_size']);
+            }
+            return self::NO_SUCH_FILE;
+        }
+        return MySQLQuery::QUERY_ERR;
+    }
+    /**
+     * Sets the value of the object that is used to create file 
+     * queries.
+     * @param FileQuery $fQuery An object that is a sub class of the class 
+     * <b>FileQuery</b>.
+     * @since 1.0
+     */
+    public function setFileQuery($fQuery) {
+        if($fQuery instanceof FileQuery){
+            $this->fileQuery = $fQuery;
+        }
+    }
+    /**
+     * Returns a file from the database given its ID.
+     * @param int $fileId The ID of the file.
+     * @return File|string If the file was found the function will return 
+     * an object of type <b>File</b> which contains all needed file data. If a database error has occurred while getting the file, 
+     * the function will return <b>MySQLQuery::QUERY_ERR</b>. If the file does not exists, 
+     * the function will return <b>FileFunctions::NO_SUCH_FILE</b>.
+     * @since 1.0
      */
     public function getFile($fileId){
         $this->useDatabase();
@@ -121,6 +197,7 @@ class FileFunctions extends Functions{
                 $file->setRawData($row[$this->fileQuery->getColName('file')]);
                 $file->setMIMEType($row[$this->fileQuery->getColName('mime-type')]);
                 $file->setName($row[$this->fileQuery->getColName('file-name')]);
+                $file->setSize($this->getFileSize($fileId));
                 return $file;
             }
             return self::NO_SUCH_FILE;
@@ -129,8 +206,36 @@ class FileFunctions extends Functions{
     }
     /**
      * 
-     * @param type $options
-     * @return type
+     * @param array $options An associative array that contains upload options. 
+     * the array will have the following keys:
+     * <ul>
+     * <li><b>database-upload</b>: A boolean value. Set to <b>TRUE</b> if 
+     * the file will be stored in the database.</li>
+     * <li><b>upload-path</b>: A string. A directory that will contain file 
+     * uploads.</li>
+     * <li><b>replace-in-path</b>: A boolean value. If set to <b>TRUE</b> 
+     * and there was a file which has the same type and name in upload directory, 
+     * the file will be replaced.</li>
+     * <li><b>upload-and-remove</b>: A boolean value. If set to <b>TRUE</b>, 
+     * the file will be uploaded temporary to upload path and then removed. 
+     * It is good practice to set it to <b>TRUE</b> if the uploads will 
+     * be stored to database.</li>
+     * <li><b>create-path</b>: A boolean value. If set to <b>TRUE</b> and <b>upload-path</b> 
+     * does not exists, it will be created.</li>
+     * @return array|string If files uploaded, the function will return an associative 
+     * array which will contain upload process information. The information will 
+     * include the following: 
+     * <ul>
+     * <li><b>upload-options</b>: An associative array that will contain the 
+     * settings that where used in upload process.</li>
+     * <li><b>names</b>: An array that will contain the indices at which the files are 
+     * stored in the array <b>$_FILES</b>.</li>
+     * <li><b>files</b>: All uploaded files info such as the name, the size, 
+     * the upload status and so on.</li>
+     * </ul>
+     * If there was no types added, the function will return the constant 
+     * <b>FileFunctions::NO_TYPES</b>.
+     * @since 1.0
      */
     public function upload($namesArr,$options=array(
         'upload-to-database'=>false,
@@ -223,6 +328,14 @@ class FileFunctions extends Functions{
         }
         return $statusArr;
     }
+    /**
+     * Returns the ID of the last file that was added to the 
+     * database.
+     * @return string|int The ID of the last file that was added to the 
+     * database. If a database error has occurred while getting the ID, 
+     * the function will return <b>MySQLQuery::QUERY_ERR</b>.
+     * @since 1.0
+     */
     public function getLastAddedFileId() {
         $this->fileQuery->getLastFileID();
         if($this->excQ($this->fileQuery)){

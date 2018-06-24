20d9a9a9ffe1200adb93ec5c524e1b47abe0a405
Ibrahim <ibinshikh@hotmail.com>
2018-06-24 10:10:01 +0300

    Added error codes

--- entity/langs/Language_EN.php
+++ entity/langs/Language_EN.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * English language definition.
+ */
 $Language = new Language('ltr', 'en', array(
     'general',
+    'general/http-codes/200',
+    'general/http-codes/201',
+    'general/http-codes/400',
+    'general/http-codes/403',
+    'general/http-codes/404',
+    'general/http-codes/405',
+    'general/http-codes/408',
+    'general/http-codes/415',
+    'general/http-codes/500',
+    'general/http-codes/501',
+    'general/http-codes/505',
     'pages/setup/setup-steps',
     'pages/setup/welcome/help',
     'pages/setup/database-setup/labels',
@@ -35,6 +49,61 @@ $Language = new Language('ltr', 'en', array(
     'pages/new-password/errors',
     'pages/new-password/status',
     'email/'
+));
+$Language->setMultiple('general/http-codes/200', array(
+    'code'=>200,
+    'type'=>'OK',
+    'message'=>''
+));
+$Language->setMultiple('general/http-codes/201', array(
+    'code'=>201,
+    'type'=>'Created',
+    'message'=>''
+));
+$Language->setMultiple('general/http-codes/400', array(
+    'code'=>400,
+    'type'=>'Bad Request',
+    'message'=>'Server could not understand the request due to invalid syntax.'
+));
+$Language->setMultiple('general/http-codes/403', array(
+    'code'=>403,
+    'type'=>'Forbidden',
+    'message'=>'You are not allowed to view the content of the requested resource.'
+));
+$Language->setMultiple('general/http-codes/404', array(
+    'code'=>404,
+    'type'=>'Not Found',
+    'message'=>'The requested resource cannot be found.'
+));
+$Language->setMultiple('general/http-codes/405', array(
+    'code'=>405,
+    'type'=>'Method Not Allowed',
+    'message'=>'The method that is used to get the resource is not allowed.'
+));
+$Language->setMultiple('general/http-codes/408', array(
+    'code'=>408,
+    'type'=>'Request Timeout',
+    'message'=>''
+));
+$Language->setMultiple('general/http-codes/415', array(
+    'code'=>415,
+    'type'=>'Unsupported Media Type',
+    'message'=>'The payload format is not supported by the server.'
+));
+$Language->setMultiple('general/http-codes/500', array(
+    'code'=>500,
+    'type'=>'Server Error',
+    'message'=>'Internal server error.'
+));
+$Language->setMultiple('general/http-codes/501', array(
+    'code'=>501,
+    'type'=>'Not Implemented',
+    'message'=>'The request method is not supported.'
+));
+$Language->setMultiple('general/http-codes/505', array(
+    'code'=>505,
+    'type'=>'HTTP Version Not Supported',
+    'message'=>'The HTTP version used in the request is not supported by the server.'
 ));
 $Language->set('general', 'wait', 'Please wait a moment...');
 $Language->set('general', 'loading', 'Loading...');
@@ -263,212 +332,3 @@ $Language->setMultiple('pages/new-password/errors', array(
     'password-missmatch'=>'The given two passwords do not match.',
     'inv-email'=>'The given email address is invalid.',
 ));
-/**
- * A language file that represents English language.
- * @author Ibrahim <ibinshik@hotmail.com>
- * @version 1.0
- */
-//const LANGUAGE = array(
-//    'dir'=>'ltr',
-//    'general'=>array(
-//        'wait'=>'Please wait a moment 🙂',
-//        'loading'=>'Loading...',
-//        'saving'=>'Saving...',
-//        'next'=>'Next',
-//        'saved'=>'Saved!',
-//        'prev'=>'Previous',
-//        'skip'=>'Skip',
-//        'connected'=>'Connected!',
-//        'disconnected'=>'Please check that you are connected to the internet.',
-//        'save'=>'Save Changes',
-//        'finish'=>'Finish'
-//    ),
-//    'pages'=>array(
-//        'activate-account'=>array(
-//            'title'=>'Account Activation',
-//            'description'=>'A page to activate user account.',
-//            'success'=>'Account Activated!',
-//            'labels'=>array(
-//                'main'=>'Activate Your Account',
-//                'activation-token'=>'Activation Token:'
-//            ),
-//            'placeholders'=>array(
-//                'activation-token'=>'Enter your activation token here.'
-//            ),
-//            'actions'=>array(
-//                'activate'=>'Activate'
-//            ),
-//            'errors'=>array(
-//                'inncorect-token'=>'Inncorrect Activation token!'
-//            )
-//        ),
-//        'settings'=>array(
-//            'title'=>'',
-//            'description'=>'',
-//            'settings'=>array(
-//                'website-settings'=>array(
-//                    
-//                ),
-//                'database-settings'=>array(
-//                    
-//                ),
-//                'smtp-settings'=>array(
-//                    'labels'=>array(
-//                        'name'=>'Account Name:',
-//                        'server-address'=>'Server Address:',
-//                        'email-address'=>'Email Address:',
-//                        'username'=>'Username',
-//                        'password'=>'Password:',
-//                        'check-connection'=>'Check Connection',
-//                        'connected'=>'Connection Established Successfully!',
-//                        'port'=>'Server Port:'
-//                    ),
-//                    'placeholders'=>array(
-//                        'name'=>'Something like: \'Programming Academia Team\'',
-//                        'server-address'=>'mail.example.com',
-//                        'email-address'=>'ma_address@example.com',
-//                        'username'=>'Server username.',
-//                        'password'=>'Login password.',
-//                        'port'=>'25'
-//                    ),
-//                    'status'=>array(
-//                        'checking-connection'=>'Validating connection info...',
-//                    ),
-//                    'errors'=>array(
-//                        'inv_mail_host_or_port'=>'Incorrect server address or port.',
-//                        'inv_username_or_pass'=>'Incorrect username or password.'
-//                    )
-//                )
-//            )
-//        ),
-//        'login'=>array(
-//            'description'=>'Login to the system.',
-//            'title'=>'Login Page',
-//            'success'=>'You are logged in.',
-//            'actions'=>array(
-//                'login' => 'Login',
-//            ),
-//            'labels'=>array(
-//                'main'=>'Login to The System',
-//                'username' => 'Username or email:',
-//                'password' => 'Password:',
-//                'keep-me-logged'=>'Keep me logged in for one week.',
-//            ),
-//            'placeholders'=>array(
-//                'username' => 'Enter Your Username or Email',
-//                'password' => 'Enter Your Password Here',
-//            ),
-//            'errors'=>array(
-//                'something-wrong'=>'Something went wrong. Try again in few moments. Sorry about that 😲',
-//                'incorrect-login-params' => 'Inccorrect username, email or password!',
-//                'err-missing-pass' => 'Missing Password!',
-//                'err-missing-username' => 'Missing Username!'
-//            )
-//        ),
-//        'home'=>array(
-//            'title'=>'Home Page',
-//            'description'=>'This is the home page'
-//        ),
-//        'sys-info'=>array(
-//            'title'=>'System Information',
-//            'description'=>'About the System.'
-//        ),
-//        'profile'=>array(
-//            'title'=>'My Profile',
-//            'description'=>'View your own profile.',
-//            'labels'=>array(
-//                'username'=>'Username:',
-//                'display-name'=>'Display Name:',
-//                'email'=>'Email:',
-//                'status'=>'Status:',
-//                'reg-date'=>'Registration Date',
-//                'last-login'=>'Last Login',
-//                'access-level'=>'Access Level:',
-//                'activation-token'=>'Activation Token:',
-//                'actions'=>'Profile Actions:',
-//                'update-email'=>'Update Email',
-//                'update-disp-name'=>'Update Display Name',
-//                'update-password'=>'Update Password'
-//            )
-//        ),
-//        'update-disp-name'=>array(
-//            'title'=>'Update Display Name',
-//            'description'=>'Change User Display Name.',
-//            'labels'=>array(
-//                'empty-name'=>'Display name cannot be empty.',
-//                'disp-name'=>'New Display Name:',
-//                'update'=>'Update Display Name',
-//                'updated'=>'<b style="color:green">Display Name Updated!</b>'
-//            )
-//        ),
-//        'update-email'=>array(
-//            'title'=>'Update User Email Address',
-//            'description'=>'Change User Email Address.',
-//            'labels'=>array(
-//                'empty-email'=>'Email cannot be empty.',
-//                'email'=>'New Email Address:',
-//                'old-pass'=>'New Email:',
-//                'update'=>'Update Email',
-//                'updated'=>'<b style="color:green">Email updated!</b>'
-//            )
-//        ),
-//        'update-pass'=>array(
-//            'title'=>'Update User Password',
-//            'description'=>'Change User Password.',
-//            'labels'=>array(
-//                'old-pass'=>'Old Password:',
-//                'new-pass'=>'New Password:',
-//                'conf-pass'=>'Confirm New Password:',
-//                'update'=>'Update Password',
-//                'pass-missmatch'=>'Confirmation password is incorrect!',
-//                'empty-old-pass'=>'Old password cannot be empty.',
-//                'empty-new-password'=>'New password cannot be empty.',
-//                'incorrect-old-pass'=>'Incorrect old password!',
-//                'updated'=>'<b style="color:green">Password updated!</b>'
-//            )
-//        ),
-//        'view-users'=>array(
-//            'title'=>'System Users',
-//            'description'=>'List of System Users.',
-//            'labels'=>array(
-//                'username'=>'Username',
-//                'disp-name'=>'Display Name',
-//                'email'=>'Email',
-//                'status'=>'Status',
-//                'reg-date'=>'Registration Date',
-//                'last-login'=>'Last Login'
-//            )
-//        ),
-//        'register'=>array(
-//            'title'=>'Create User Profile',
-//            'description'=>'Creating new profile.',
-//            'labels'=>array(
-//                'username'=>'Username:',
-//                'password'=>'Password:',
-//                'conf-pass'=>'Confirm Password:',
-//                'email'=>'Email:',
-//                'disp-name'=>'Display Name:',
-//                'reg'=>'Register',
-//                'acc-lvl'=>'Access Level:'
-//            ),
-//            'errors'=>array(
-//                'missing-acc-lvl'=>'Select Access Level!',
-//                'missing-username'=>'Username cannot be empty.',
-//                'missing-pass'=>'Password cannot be empty.',
-//                'pass-missmatch'=>'Check your confirmed password.',
-//                'missing-email'=>'Email cannot be empty.'
-//            )
-//        ),
-//    ),
-//    'aside'=>array(
-//        'logout'=>'Logout',
-//        'home'=>'Home Page',
-//        'profile'=>'User Profile',
-//        'add-user'=>'Create User',
-//        'view-users'=>'View Users',
-//        'sys-info'=>'System Info'
-//    ),
-//    'api-messages'=>array(
-//        
-//    )
-//);

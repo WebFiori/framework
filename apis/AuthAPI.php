b21887a7fb2f0c37020245cc59499696c5bd58f3
Ibrahim <ibinshikh@hotmail.com>
2018-06-27 11:54:38 +0300

    Updates and fixes
    Added API description
    Added check for if a user is already logged in and another is trying to loggin.
    Fixed logout action

--- apis/AuthAPI.php
+++ apis/AuthAPI.php
@@ -35,13 +35,25 @@ class AuthAPI extends API{
     public function __construct() {
         parent::__construct();
         $this->setVersion('1.0.0');
-        $a1 = new APIAction();
+        $a1 = new APIAction('login');
+        $a1->setDescription('Grant the user the access to the system.');
         $a1->addRequestMethod('POST');
-        $a1->setName('login');
         $a1->addParameter(new RequestParameter('username', 'string'));
+        $a1->getParameterByName('username')->setDescription('The user name of '
+                . 'the user or his email address.');
         $a1->addParameter(new RequestParameter('password', 'string'));
+        $a1->getParameterByName('password')->setDescription('The user account\'s password.');
         $a1->addParameter(new RequestParameter('session-duration', 'integer',TRUE));
-        $a1->addParameter(new RequestParameter('refresh-timeout', 'string',TRUE));
+        $a1->getParameterByName('session-duration')->setMinVal(0);
+        $a1->getParameterByName('session-duration')->setDefault(5);
+        $a1->getParameterByName('session-duration')->setDescription('The duration of '
+                . 'the session in minutes before the user is automatically logged off the system. Default '
+                . 'is 5 minutes.');
+        $a1->addParameter(new RequestParameter('refresh-timeout', 'boolean',TRUE));
+        $a1->getParameterByName('refresh-timeout')->setDefault('n');
+        $a1->getParameterByName('refresh-timeout')->setDescription('If set to true, '
+                . 'the session duration will be reset with every request the client sends. Default '
+                . 'is false.');
         $this->addAction($a1);
         
         $a2 = new APIAction();
@@ -54,52 +66,47 @@ class AuthAPI extends API{
         $inputs = $this->getInputs();
         $action = $this->getAction();
         if($action == 'login'){
-            if(isset($inputs['username'])){
-                if(isset($inputs['password'])){
-                    if(isset($inputs['session-duration'])){
-                        $duration = $inputs['session-duration'];
-                    }
-                    else{
-                        $duration = 30;
-                    }
-                    if(isset($inputs['refresh-timeout'])){
-                        if($inputs['refresh-timeout'] == 'true'){
-                            $refTimeout = TRUE;
-                        }
-                        else{
-                            $refTimeout = FALSE;
-                        }
-                    }
-                    else{
-                        $refTimeout = FALSE;
-                    }
-                    $r = UserFunctions::get()->authenticate($inputs['username'], $inputs['password'], $inputs['username'],$duration,$refTimeout);
-                    if($r == TRUE){
-                        if(UserFunctions::get()->getMainSession()->getUser()->getStatus() == 'S'){
-                            $this->sendResponse('Account Suspended',TRUE,401);
-                            UserFunctions::get()->getMainSession()->kill();
-                        }
-                        else{
-                            $this->sendResponse('Logged In', FALSE, 200, '"session":'.UserFunctions::get()->getMainSession()->toJSON());
-                        }
-                    }
-                    else if($r == MySQLQuery::QUERY_ERR){
-                        $this->databaseErr();
-                    }
-                    else{
-                        $this->sendResponse('Inncorect username, email or password.', TRUE, 401);
-                    }
+            $loggedId = UserFunctions::get()->getUserID();
+            if($loggedId != -1){
+                $this->sendResponse('A user is already logged in.', TRUE, 404);
+                return;
+            }
+            if(isset($inputs['session-duration'])){
+                $duration = $inputs['session-duration'];
+            }
+            else{
+                $duration = 30;
+            }
+            if(isset($inputs['refresh-timeout'])){
+                if($inputs['refresh-timeout'] == 'true'){
+                    $refTimeout = TRUE;
                 }
                 else{
-                    $this->missingParam('password');
+                    $refTimeout = FALSE;
                 }
             }
             else{
-                $this->missingParam('username');
+                $refTimeout = FALSE;
+            }
+            $r = UserFunctions::get()->authenticate($inputs['username'], $inputs['password'], $inputs['username'],$duration,$refTimeout);
+            if($r == TRUE){
+                if(UserFunctions::get()->getMainSession()->getUser()->getStatus() == 'S'){
+                    $this->sendResponse('Account Suspended',TRUE,401);
+                    UserFunctions::get()->getMainSession()->kill();
+                }
+                else{
+                    $this->sendResponse('Logged In', FALSE, 200, '"session":'.UserFunctions::get()->getMainSession()->toJSON());
+                }
+            }
+            else if($r == MySQLQuery::QUERY_ERR){
+                $this->databaseErr(UserFunctions::get()->getMainSession()->getDBLink()->getErrorMessage());
+            }
+            else{
+                $this->sendResponse('Inncorect username, email or password.', TRUE, 401);
             }
         }
         else if($action == 'logout'){
-            SessionManager::get()->kill();
+            UserFunctions::get()->getMainSession()->kill();
             $this->sendResponse('Logged Out', FALSE, 200);
         }
     }

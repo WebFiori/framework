abec2d8f75ac572de439511a0c336d53e74c1590
Ibrahim <ibinshikh@hotmail.com>
2018-06-29 16:54:40 +0300

    Implemented router

--- index.php
+++ index.php
@@ -2,30 +2,8 @@
 require_once 'root.php';
 $uri = filter_var($_SERVER['REQUEST_URI']);
 $split = Router::splitURI($uri);
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Index</title>
-    </head>
-    <body style="color:green;background-color: black">
-        <?php
-            Util::print_r($split);
-        ?>
-        <p>Hello Word! I'm A live!</p>
-        <p>This is the index page of generic PHP web applications building template.</p>
-        <?php
-            echo Config::get();
-            echo SiteConfig::get();
-        ?>
-        <p>Steps to Start Building Projects:</p>
-        <ul>
-            <li>Define Database Schema (Use the class 'MySQLQuery' as bases)</li>
-            <li>Build The logic (inside the folder /functions'</li>
-            <li>Define your APIs (Use the class 'API' as bases.)</li>
-            <li>Update 'AutoLoader' to load your classes as needed.</li>
-            <li>Build Web Pages UI (Use the class 'PageAttributes' as bases.)</li>
-        </ul>
-    </body>
-</html>
+$route = '';
+for($x = 1 ; $x < count($split['uri-broken']) ; $x++){
+    $route .= '/'.$split['uri-broken'][$x];
+}
+Router::get()->route($route);

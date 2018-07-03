ab05f8c57135d37d48a157b83dd6280295d4651a
Ibrahim <ibinshikh@hotmail.com>
2018-07-03 18:38:31 +0300

    New way for creating themes

--- publish/themes/greeny/theme.php
+++ publish/themes/greeny/theme.php
@@ -1,23 +1,31 @@
 <?php
-$GLOBALS['THEME'] = array(
-    'META'=>array(
-        'name'=>'Greeny By Ibrahim Ali',
-        'url'=>'',
-        'author'=>'Ibrahim Ali',
-        'author-url'=>'http://ibrahim-2017.blogspot.com',
-        'version'=>'1.0',
-        'license'=>'n/a',
-        'license-url'=>'',
-        'description'=>'First theme ever made. A nice green colored elements That '
-        . 'makes you thing about the nature.',
-        'directory'=>'greeny'
-    ),
-    'COMPONENTS'=>array(
-        'head.php', 'header.php', 'footer.php',
-        'aside.php','after.php'
-    ),
-    'css-dir'=>'greeny/css',
-    'js-dir'=>'greeny/js',
-    'images-dir'=>'geeny/images'
-);
+$theme = new Theme();
+$theme->setAuthor('Ibrahim Ali');
+$theme->setAuthorUrl('http://ibrahim-2017.blogspot.com');
+$theme->setName('Greeny By Ibrahim Ali');
+$theme->setVersion('1.0');
+$theme->setDescription('First theme ever made. A nice green colored elements That '
+        . 'makes you thing about the nature.');
+$theme->setDirectoryName('greeny');
+$theme->setImagesDirName('images');
+$theme->setJsDirName('js');
+$theme->setCssDirName('css');
+$theme->addComponents(array(
+    'head.php', 'header.php', 'footer.php',
+    'aside.php'
+));
+$theme->setAfterLoaded(function(){
+    $page = Page::get();
+    $page->setLang(WebsiteFunctions::get()->getMainSession()->getLang(TRUE));
+    $page->usingLanguage();
+    $page->getDocument()->getBody()->setClassName('pa-container');
+    $page->getDocument()->getChildByID('page-body')->setClassName('pa-row');
+    if($page->hasAside()){
+        $page->getDocument()->getChildByID('aside-container')->setClassName('pa-'.$page->getWritingDir().'-col-two');
+        $page->getDocument()->getChildByID('main-content-area')->setClassName('pa-'.$page->getWritingDir().'-col-ten');
+    }
+    else{
+        $page->getDocument()->getChildByID('main-content-area')->setClassName('pa-'.$page->getWritingDir().'-col-twelve');
+    }
+});
 

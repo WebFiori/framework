12987d3daadf473a149a357d9bf86176a3dac82c
Ibrahim <ibinshikh@hotmail.com>
2018-07-03 18:31:02 +0300

    A playground for testing the SessionManager

---
+++ tests/unit/SessionManager_Test.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Ibrahim.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+require_once '../../root.php';
+$j = new JsonX();
+$session = new SessionManager('sess-1');
+$session->initSession(FALSE, FALSE);
+$j->add('session-1', $session);
+
+$session2 = new SessionManager('sess-2');
+$session2->initSession(FALSE, FALSE);
+$j->add('session-2', $session2);
+
+
+$session3 = new SessionManager('sess-3');
+$session3->initSession(TRUE, TRUE);
+$j->add('session-3', $session3);
+
+$session4 = new SessionManager('sess-4');
+$session4->initSession(FALSE, FALSE);
+$j->add('session-4', $session4);
+
+echo $j;

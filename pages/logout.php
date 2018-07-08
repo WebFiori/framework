756bec643376437bc31a25324aa8cc349a55c110
Ibrahim <ibinshikh@hotmail.com>
2018-07-08 10:50:04 +0300

    Update for redirect

--- pages/logout.php
+++ pages/logout.php
@@ -2,4 +2,4 @@
 defined('ROOT_DIR') or die('Direct Access Not Allowed.');
 $lang = WebsiteFunctions::get()->getMainSession()->getLang();
 WebsiteFunctions::get()->getMainSession()->kill();
-header('location: '.SiteConfig::get()->getBaseURL().'pages/login?lang='.$lang);
+header('location: '.SiteConfig::get()->getBaseURL().'login?lang='.$lang);

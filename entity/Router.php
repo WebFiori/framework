d81e24250a39ec05489153d1163dee47a9ec56a8
Ibrahim <ibinshikh@hotmail.com>
2018-07-12 13:02:57 +0300

    Updated router

--- entity/Router.php
+++ entity/Router.php
@@ -31,7 +31,7 @@ if(!defined('ROOT_DIR')){
  * Description of Router
  *
  * @author Ibrahim
- * @version 1.0
+ * @version 1.1
  */
 class Router {
     /**
@@ -70,6 +70,12 @@ class Router {
      */
     private static $router;
     /**
+     *
+     * @var type 
+     * @since 1.1
+     */
+    private $baseUrl;
+    /**
      * Creates and Returns a single instance of the router.
      * @return Router
      * @since 1.0
@@ -82,7 +88,7 @@ class Router {
         return self::$router;
     }
     /**
-     * An associative array that contains all defined routes.
+     * An array which contains an objects of type <b>RouteUri</b>.
      * @var array
      * @since 1.0 
      */
@@ -94,62 +100,46 @@ class Router {
     private function __construct() {
         $this->routes = array();
         $this->onNotFound = function (){};
-//        $this->addRoute('/', 'default.html', self::VIEW_ROUTE);
-//        $this->addRoute('/index', 'default.html', self::VIEW_ROUTE);
+        $this->baseUrl = trim(SiteConfig::get()->getBaseURL(), '/');
+    }
+    public function getBase() {
+        return $this->baseUrl;
     }
     /**
      * 
-     * @param string $rquestedUri
+     * @param string $path
      * @param type $routeTo
      * @param type $routeType
      * @return boolean
      * @since 1.0
      */
-    public function addRoute($rquestedUri,$routeTo,$routeType) {
+    public function addRoute($path,$routeTo,$routeType) {
         if($routeType == self::API_ROUTE || 
            $routeType == self::VIEW_ROUTE || 
-           $routeType == self::CUSTOMIZED || $routeType == self::FUNCTION_ROUTE){
-            $requestedUriBoken = Router::splitURI($rquestedUri);
-            if($requestedUriBoken['protocol'] == ''){
-                $rquestedUri = trim(SiteConfig::get()->getBaseURL(),'/').$rquestedUri;
-                $requestedUriBoken = Router::splitURI($rquestedUri);
-            }
-            if($routeType != self::FUNCTION_ROUTE){
-                $routeBroken = Router::splitURI($routeTo);
-                $routeFile = ROOT_DIR.$routeType.'/'.$routeBroken['uri-without-query-string'];
-                if(file_exists($routeFile)){
-                    $this->routes[$requestedUriBoken['uri-without-query-string']] = array(
-                        'route-type'=>$routeType,
-                        'requested-uri-format'=>$requestedUriBoken['uri'],
-                        'route-to'=>$routeFile,
-                        'variables'=>array()
-                    );
-                    foreach ($requestedUriBoken['uri-broken'] as $val){
-                        $len = strlen($val);
-                        if($val[0] == '{' && $val[$len - 1] == '}'){
-                            array_push($this->routes[$requestedUriBoken['uri-without-query-string']]['variables'], $val);
-                        }
-                    }
-                    return TRUE;
-                }
-            }
-            else{
-                $this->routes[$requestedUriBoken['uri-without-query-string']] = array(
-                    'route-type'=>$routeType,
-                    'requested-uri-format'=>$requestedUriBoken['uri'],
-                    'route-to'=>$routeTo,
-                    'variables'=>array()
-                );
-                foreach ($requestedUriBoken['uri-broken'] as $val){
-                    $len = strlen($val);
-                    if($val[0] == '{' && $val[$len - 1] == '}'){
-                        array_push($this->routes[$requestedUriBoken['uri-without-query-string']]['variables'], $val);
-                    }
-                }
+           $routeType == self::CUSTOMIZED || 
+           $routeType == self::FUNCTION_ROUTE){
+            if(!$this->hasRoute($path)){
+                $routeUri = new RouterUri($this->getBase().$path, $routeTo);
+                $this->routes[] = $routeUri;
                 return TRUE;
             }
         }
         return FALSE;
+    }
+    /**
+     * Checks if a given path has a route or not.
+     * @param string $path The path which will be checked (such as '/path1/path2')
+     * @return boolean The function will return <b>TRUE</b> if the given path 
+     * has a route.
+     * @since 1.1
+     */
+    public function hasRoute($path) {
+        $hasRoute = FALSE;
+        $routeURI = new RouterUri($this->getBase().$path, '');
+        foreach ($this->routes as $route){
+            $hasRoute = $hasRoute || $routeURI->equals($route);
+        }
+        return $hasRoute;
     }
     /**
      * Sets a function to call in case a given rout is not found.
@@ -163,95 +153,13 @@ class Router {
         }
     }
     /**
-     * Breaks a URI into its basic components. This function can break 
-     * URIs which uses HTTP or HTTPS protocols. If a URI has different protocol, 
-     * the result will be unexpected.
-     * @param string $uri The URI that will be broken.
-     * @return array The function will return an associative array that 
-     * contains the components of the URI. The array will have the 
-     * following indices:
-     * <ul>
-     * <li><b>uri</b>: The original URI.</li>
-     * <li><b>domain</b>: Only set if the URI has a protocol (http or https).</li>
-     * <li><b>protocol</b>: http or https.</li>
-     * <li><b>uri-without-query-string</b>: The URI without query string.</li>
-     * <li><b>query-string</b>: Query string if the URI has any.</li>
-     * <li><b>uri-broken</b>: The URI after separating each component.</li>
-     * <li><b>query-string-breaked</b>: The query string broken into an arrays of keys and values.</li>
-     * </ul>
-     * @since 1.0
-     */
-    public static function splitURI($uri) {
-        $retVal = array(
-            'uri'=>$uri,
-            'domain'=>'',
-            'protocol'=>'',
-            'uri-without-query-string'=>'',
-            'query-string'=>'',
-            'uri-broken'=>array(),
-            'query-string-broken'=>array()
-        );
-        //first, split query string from the URI
-        $split = explode('?', $uri);
-        //Query string will be in $split[1] if any
-        $retVal['query-string'] = isset($split[1]) ? $split[1] : '';
-        //$split[0] will contain the URI without query string.
-        $retVal['uri-without-query-string'] = trim($split[0], '/');
-        //after that, we need to check the start of the URI if 
-        //it contains one of the following: 'http://', 'https://', 
-        $splitx = explode('://', $retVal['uri-without-query-string']);
-        $retVal['protocol'] = isset($splitx[1]) ? $splitx[0] : '';
-        
-        return $retVal;
-    }
-    /**
      * 
      * @param type $uri
      * @since 1.0
      */
     public function route($uri) {
         if(count($this->routes) != 0){
-            $uriSplit = Router::splitURI($uri);
-            if($uriSplit['protocol'] == ''){
-                $uri = trim(SiteConfig::get()->getBaseURL(),'/').$uri;
-                $uriSplit = Router::splitURI($uri);
-            }
-            $requestMethod = filter_var(getenv('REQUEST_METHOD'));
-            foreach ($this->routes as $route){
-                
-                $this->routes[$route['requested-uri-format']]['var-values'] = $this->extractVarsValue($uri, $route);
-            }
             
-            foreach ($this->routes as $route){
-                if(isset($route['var-values']) && count($route['var-values']) != 0){
-                    foreach ($route['var-values'] as $key => $value){
-                        $keyTrim = trim($key, '{');
-                        if($requestMethod == 'GET' || $requestMethod == 'DELETE'){
-                            $_GET[trim($keyTrim,'}')] = $value;
-                        }
-                        else if($requestMethod == 'POST' || $requestMethod == 'PUT'){
-                            $_POST[trim($keyTrim,'}')] = $value;
-                        }
-                        $uri = str_replace($value, $key, $uri);
-                    }
-                }
-                if(isset($this->routes[$uri])){
-                    break;
-                }
-            }
-            if(isset($this->routes[$uri])){
-                if($this->routes[$uri]['route-type'] != self::FUNCTION_ROUTE){
-                    require_once $this->routes[$uri]['route-to'];
-                }
-                else{
-                    call_user_func($this->routes[$uri]['route-to']);
-                }
-                return TRUE;
-            }
-            else{
-                call_user_func($this->onNotFound);
-                return FALSE;
-            }
         }
         else{
             die('No routes are available.');

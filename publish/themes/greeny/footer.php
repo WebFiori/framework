716c3074395bbbefb8c2840b3768293915bae080
Ibrahim <ibinshikh@hotmail.com>
2018-07-14 20:02:09 +0300

    Updated

--- publish/themes/greeny/footer.php
+++ publish/themes/greeny/footer.php
@@ -4,7 +4,7 @@ function getFooterNode(){
     $node->setAttribute('class', 'pa-row');
     $fNode = new HTMLNode('footer');
     $fNode->setAttribute('dir', Page::get()->getWritingDir());
-    $fNode->setAttribute('class','pa-'.Page::get()->getWritingDir().'-col-twelve show-border');
+    $fNode->setAttribute('class','pa-'.Page::get()->getWritingDir().'-col-twelve');
     $fNode->setAttribute('itemtype','http://schema.org/WPFooter');
     $fNav = new HTMLNode('nav');
     $fNavUl = new HTMLNode('ul');
@@ -12,19 +12,9 @@ function getFooterNode(){
     $fNode->addChild($fNav);
     $node->addChild($fNode);
     $div = new HTMLNode('div');
-    $div->setAttribute('class', 'pa-'.Page::get()->getWritingDir().'-col-twelve');
-    if(Page::get()->getLang() == "EN"){
-        $textNode = new HTMLNode('', FALSE, TRUE);
-        $textNode->setText('Programming Academia, All Rights Reserved © 2018');
-    }
-    else if(Page::get()->getLang() == 'AR'){
-        $textNode = new HTMLNode('', FALSE, TRUE);
-        $textNode->setText('أكاديميا البرمجة, جميع الحقوق محفوظة © 2018 ');
-    }
-    else{
-        $textNode = new HTMLNode('', FALSE, TRUE);
-        $textNode->setText('Programming Academia, All Rights Reserved © 2018');
-    }
+    $div->setAttribute('class', 'pa-ltr-col-twelve');
+    $textNode = new HTMLNode('', FALSE, TRUE);
+    $textNode->setText('<b style="color:gray;font-size:8pt;">Powered By: <a href="http://www.liskscode.org" target="_blank">LisksCode</a> v'.Config::get()->getTemplateVersion().'</b>');
     $div->addChild($textNode);
     $fNode->addChild($div);
     return $node;

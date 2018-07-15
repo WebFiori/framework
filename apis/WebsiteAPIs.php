e7713f9815615f5f336bd7cc87172365ee1dee5b
Ibrahim <ibinshikh@hotmail.com>
2018-07-15 20:02:25 +0300

    Added more APIS

--- apis/WebsiteAPIs.php
+++ apis/WebsiteAPIs.php
@@ -36,38 +36,100 @@ class WebsiteAPIs extends API{
     public function __construct() {
         parent::__construct();
         
-        $a1 = new APIAction();
-        $a1->setName('update-website-name');
-        $a1->addRequestMethod('post');
-        $a1->addParameter(new RequestParameter('name', 'string'));
-        $this->addAction($a1, TRUE);
-        
-        $a2 = new APIAction();
-        $a2->setName('update-site-info');
-        $a2->setDescription('Updates general website information.');
+        $a2 = new APIAction('update-site-info');
+        $a2->setDescription('Updates the basic settings of the website.');
         $a2->addRequestMethod('post');
         $a2->addParameter(new RequestParameter('title-sep', 'string', TRUE));
+        $a2->getParameterByName('title-sep')->setDescription('Optional parameter. '
+                . 'The value of this parameter can be acharacter or a string. It is simply '
+                . 'a value that is used to seperate the name of a web page and '
+                . 'the name of the website inside the tag \'title\'.');
         $a2->addParameter(new RequestParameter('home-page', 'string',TRUE));
+        $a2->getParameterByName('home-page')->setDescription('Optional parameter. The '
+                . 'value of this attribute must be a path to the home page. '
+                . 'If the given page does not exists, the API will responde with a message '
+                . 'that says this parameter is invalid.');
+        $a2->getParameterByName('home-page')->setCustomFilterFunction(function($val){
+            $homePage = $val['basic-filter-result'];
+            $router = Router::get();
+            if($router->hasRoute($homePage)){
+                return $homePage;
+            }
+            return FALSE;
+        });
         $a2->addParameter(new RequestParameter('site-theme', 'string',TRUE));
+        $a2->getParameterByName('site-theme')->setDescription('Optional parameter. The '
+                . 'value of this attribute is the name of the theme that is used '
+                . 'across website pages. If the given theme does not exists, Website '
+                . 'information won\'t update.');
         $this->addAction($a2, TRUE);
         
         $a3 = new APIAction();
         $a3->setName('get-website-info');
+        $a3->setDescription('Returns all general website information.');
         $a3->addRequestMethod('get');
         $this->addAction($a3);
         
         $this->setVersion('1.0.1');
         $a4 = new APIAction('add-website');
+        $a4->setDescription('Adds new website name and website description.');
+        $a4->addRequestMethod('post');
         $a4->addParameter(new RequestParameter('site-name', 'string'));
+        $a4->getParameterByName('site-name')->setDescription('The name of the website in '
+                . 'the language that will be added.');
         $a4->addParameter(new RequestParameter('language', 'string'));
+        $a4->getParameterByName('language')->setDescription('Two characters that represents language code. '
+                . 'It is the unique identifier for the language.');
+        $a4->getParameterByName('language')->setCustomFilterFunction(function($val){
+            $langCode = $val['basic-filter-result'];
+            if(strlen($langCode) == 2){
+                return $langCode;
+            }
+            return FALSE;
+        });
         $a4->addParameter(new RequestParameter('site-description', 'string'));
+        $a4->getParameterByName('site-description')->setDescription('A general description for the website in '
+                . 'the language that will be added.');
         $this->addAction($a4, TRUE);
         
         $a5 = new APIAction('update-website');
+        $a5->addRequestMethod('post');
         $a5->addParameter(new RequestParameter('site-name', 'string'));
+        $a5->getParameterByName('site-name')->setDescription('The name of the website in '
+                . 'the language that will be added.');
         $a5->addParameter(new RequestParameter('language', 'string'));
+        $a5->getParameterByName('language')->setDescription('Two characters that represents language code. '
+                . 'It is the unique identifier for the language.');
+        $a5->getParameterByName('language')->setCustomFilterFunction(function($val){
+            $langCode = $val['basic-filter-result'];
+            if(strlen($langCode) == 2){
+                return $langCode;
+            }
+            return FALSE;
+        });
         $a5->addParameter(new RequestParameter('site-description', 'string'));
-        $this->addAction($a4, TRUE);
+        $a5->getParameterByName('site-description')->setDescription('A general description for the website in '
+                . 'the language that will be updated.');
+        $this->addAction($a5, TRUE);
+        
+        $a6 = new APIAction('remove-website');
+        $a6->setDescription('Removes the name of a website and its description given its language code.');
+        $a6->addRequestMethod('delete');
+        $a6->addParameter(new RequestParameter('language', 'string'));
+        $a6->getParameterByName('language')->setCustomFilterFunction(function($val){
+            $langCode = $val['basic-filter-result'];
+            if(strlen($langCode) == 2){
+                return $langCode;
+            }
+            return FALSE;
+        });
+        $a6->getParameterByName('language')->setDescription('Two digit language code.');
+        $this->addAction($a6, TRUE);
+        
+        $a7 = new APIAction('get-available-themes');
+        $a7->setDescription('Returns all the information of installed themes.');
+        $a7->addRequestMethod('get');
+        $this->addAction($a7, TRUE);
     }
     public function isAuthorized() {
         return WebsiteFunctions::get()->getAccessLevel() == 0;
@@ -84,10 +146,56 @@ class WebsiteAPIs extends API{
             $this->updateSiteInfo();
         }
         else if($action == 'add-website'){
-            $this->actionNotImpl();
+            $i = $this->getInputs();
+            $siteInfoArr = WebsiteFunctions::get()->getSiteConfigVars();
+            $langCode = strtoupper($i['language']);
+            if(isset($siteInfoArr['website-names'][$langCode])){
+                $this->sendResponse('Already Added', FALSE, 200);
+            }
+            else{
+                $websiteName = $i['site-name'];
+                $websiteDesc = $i['site-description'];
+                $siteInfoArr['website-names'][$langCode] = $websiteName;
+                $siteInfoArr['site-descriptions'][$langCode] = $websiteDesc;
+                WebsiteFunctions::get()->updateSiteInfo($siteInfoArr);
+                $this->sendResponse('New Website Language Added.', FALSE, 201);
+            }
         }
         else if($action == 'update-website'){
-            $this->actionNotImpl();
+            $i = $this->getInputs();
+            $siteInfoArr = WebsiteFunctions::get()->getSiteConfigVars();
+            $langCode = strtoupper($i['language']);
+            if(isset($siteInfoArr['website-names'][$langCode])){
+                $websiteName = $i['site-name'];
+                $websiteDesc = $i['site-description'];
+                $siteInfoArr['website-names'][$langCode] = $websiteName;
+                $siteInfoArr['site-descriptions'][$langCode] = $websiteDesc;
+                WebsiteFunctions::get()->updateSiteInfo($siteInfoArr);
+                $this->sendResponse('Updated', FALSE, 200);
+            }
+            else{
+                $this->sendResponse('No such website language: '.$langCode, TRUE, 404);
+            }
+        }
+        else if($action == 'remove-website'){
+            $i = $this->getInputs();
+            $siteInfoArr = WebsiteFunctions::get()->getSiteConfigVars();
+            $langCode = strtoupper($i['language']);
+            if(isset($siteInfoArr['website-names'][$langCode])){
+                unset($siteInfoArr['website-names'][$langCode]);
+                unset($siteInfoArr['site-descriptions'][$langCode]);
+                WebsiteFunctions::get()->updateSiteInfo($siteInfoArr);
+                $this->sendResponse('Removed', FALSE, 200);
+            }
+            else{
+                $this->sendResponse('No such website language: '.$langCode, TRUE, 404);
+            }
+        }
+        else if($action == 'get-available-themes'){
+            $themes = Theme::getAvailableThemes();
+            $j = new JsonX();
+            $j->addArray('themes', $themes);
+            $this->send('application/json', $j);
         }
     }
     /**
@@ -96,10 +204,8 @@ class WebsiteAPIs extends API{
      */
     private function updateSiteInfo() {
         $i = $this->getInputs();
-        $cfgArr = array(
-            'website-name'=>$i['site-name'],
-            'description'=>$i['site-description']
-        );
+        
+        $cfgArr = WebsiteFunctions::get()->getSiteConfigVars();
         if(isset($i['title-sep'])){
             $cfgArr['title-separator'] = $i['title-sep'];
         }
@@ -107,10 +213,19 @@ class WebsiteAPIs extends API{
             $cfgArr['home-page'] = $i['home-page'];
         }
         if(isset($i['site-theme'])){
-            $cfgArr['theme-name'] = $i['site-theme'];
+            try{
+                Theme::usingTheme($i['site-theme']);
+                $cfgArr['theme-name'] = $i['site-theme'];
+                WebsiteFunctions::get()->updateSiteInfo($cfgArr);
+                $this->sendResponse('Site info updated.');
+            } catch (Exception $ex) {
+                $this->sendResponse('No theme was found which has the given name.', TRUE, 404);
+            }
         }
-        WebsiteFunctions::get()->updateSiteInfo($cfgArr);
-        $this->sendResponse('Site info updated.');
+        else{
+            WebsiteFunctions::get()->updateSiteInfo($cfgArr);
+            $this->sendResponse('Site info updated.');
+        }
             
     }
 

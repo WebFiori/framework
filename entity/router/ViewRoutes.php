c9752cec12123a8288d4e9c19584972801b7a9a8
Ibrahim <ibinshikh@hotmail.com>
2018-07-18 07:46:53 +0300

    Added function to generate views sitemap

--- entity/router/ViewRoutes.php
+++ entity/router/ViewRoutes.php
@@ -36,16 +36,37 @@ class ViewRoutes {
      * @since 1.0
      */
     public static function create(){
-        Router::view('/s/welcome', '/setup/welcome.php');
-        Router::view('/s/database-setup', '/setup/database-setup.php');
-        Router::view('/s/smtp-account', '/setup/email-account.php');
-        Router::view('/s/admin-account', '/setup/admin-account.php');
-        Router::view('/s/website-config', '/setup/website-config.php');
+        if(Util::checkSystemStatus() == Util::NEED_CONF){
+            Router::view('/s/welcome', '/setup/welcome.php');
+            Router::view('/s/database-setup', '/setup/database-setup.php');
+            Router::view('/s/smtp-account', '/setup/email-account.php');
+            Router::view('/s/admin-account', '/setup/admin-account.php');
+            Router::view('/s/website-config', '/setup/website-config.php');
+        }
         Router::view('/login', '/login.php');
         Router::view('/home', '/home.php');
         Router::view('/activate-account', '/activate-account.php');
         Router::view('/logout', '/logout.php');
         Router::view('/new-password', '/new-password.php');
         Router::view('/', '/default.html');
+    }
+    
+    public static function createSiteMap() {
+        $urlSet = new HTMLNode('urlset');
+        $urlSet->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+        $urlSet->setAttribute('xmlns:xhtml', 'http://www.w3.org/1999/xhtml');
+        $routes = Router::get()->getRoutes();
+        foreach ($routes as $route){
+            if($route->getType() == Router::VIEW_ROUTE){
+                $url = new HTMLNode('url');
+                $loc = new HTMLNode('loc');
+                $loc->addChild(HTMLNode::createTextNode($route->getUri()));
+                $url->addChild($loc);
+                $urlSet->addChild($url);
+            }
+        }
+        $retVal = '<?xml version="1.0" encoding="UTF-8"?>';
+        $retVal .= $urlSet->toHTML();
+        return $retVal;
     }
 }

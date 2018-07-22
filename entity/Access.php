0767cc238697d805a1205640c2372464059b27cf
Ibrahim <ibinshikh@hotmail.com>
2018-07-22 19:32:04 +0300

    Added more privileges and a new users group

--- entity/Access.php
+++ entity/Access.php
@@ -80,6 +80,8 @@ class Access {
         $this->_createPrivilege('SUPER_ADMIN', 'UPDATE_USER_DISPLAY_NAME_ALL');
         $this->_createPrivilege('SUPER_ADMIN', 'GET_USER_PROFILE');
         $this->_createPrivilege('SUPER_ADMIN', 'GET_USER_PROFILE_ALL');
+        $this->_createPrivilege('SUPER_ADMIN', 'GET_PRIVILEGES');
+        $this->_createPrivilege('SUPER_ADMIN', 'GET_USER_SESSION');
         
         $this->_createPrivilege('SUPER_ADMIN', 'UPDATE_USER_REGISTRATION_STATUS');
         $this->_createPrivilege('SUPER_ADMIN', 'UPDATE_DB');
@@ -89,6 +91,12 @@ class Access {
         $this->_createPrivilege('SUPER_ADMIN', 'UPLOAD_FILE');
         $this->_createPrivilege('SUPER_ADMIN', 'EDIT_UPLOADED_FILE');
         $this->_createPrivilege('SUPER_ADMIN', 'REMOVE_UPLOADED_FILE');
+        
+        $this->_createGroup('BASIC_USER');
+        $this->_createPrivilege('BASIC_USER', 'LOGIN');
+        $this->_createPrivilege('BASIC_USER', 'GET_USER_PROFILE');
+        $this->_createPrivilege('BASIC_USER', 'UPDATE_USER_DISPLAY_NAME');
+        $this->_createPrivilege('BASIC_USER', 'UPDATE_USER_EMAIL');
     }
     /**
      * Returns a single instance of the class.

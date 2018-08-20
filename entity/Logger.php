55e312b9e835c5b1dc4f8c83b3e648530f949c6d
Ibrahim <ibinshikh@hotmail.com>
2018-08-20 16:43:36 +0300

    Fully implemented logger class

--- entity/Logger.php
+++ entity/Logger.php
@@ -22,7 +22,23 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+if(!defined('ROOT_DIR')){
+    header("HTTP/1.1 403 Forbidden");
+    die(''
+        . '<!DOCTYPE html>'
+        . '<html>'
+        . '<head>'
+        . '<title>Forbidden</title>'
+        . '</head>'
+        . '<body>'
+        . '<h1>403 - Forbidden</h1>'
+        . '<hr>'
+        . '<p>'
+        . 'Direct access not allowed.'
+        . '</p>'
+        . '</body>'
+        . '</html>');
+}
 /**
  * A class that is used to log messages to a file.
  *
@@ -53,9 +69,22 @@ class Logger {
      * @since 1.0  
      */
     private $directory;
+    /**
+     * A boolean value that is set to true if logging is enabled.
+     * @var boolean
+     * @since 1.0
+     *  
+     */
+    private $isEnabled;
     private function __construct() {
-        $this->_setDirectory(ROOT_DIR.'/logs');
+        if(defined('ROOT_DIR')){
+            $this->_setDirectory(ROOT_DIR.'/logs');
+        }
+        else{
+            $this->_setDirectory('/logs');
+        }
         $this->_setLogName('log');
+        $this->isEnabled = FALSE;
     }
     /**
      * Returns a singleton of the class.
@@ -68,15 +97,32 @@ class Logger {
         }
         return self::$logger;
     }
-    
+    /**
+     * Enable, disable or check if logging is enabled.
+     * @param boolean $isEnabled If provided and set to true, logging will be 
+     * enabled. If provided and not true, logging will be disabled.
+     * @return boolean The function will return TRUE if logging is enabled. 
+     * false otherwise. Default return value is false which means that the 
+     * logger is disabled initially.
+     * @since 1.0
+     */
+    public static function enabled($isEnabled=null) {
+        if($isEnabled !== NULL){
+            self::_get()->_setEnabled($isEnabled);
+        }
+        return self::_get()->_isEnabled();
+    }
     /**
      * Writes a message to the log file.
      * @param string $message The message that will be written.
+     * @param string $logName [Optional] The name of the log file. If it is not 
+     * NULL, the log will be written to the given file name.
      * @param boolean $addDashes If set to true, a line of dashes will be inserted 
      * after the message. Used to organize log messages.
      * @since 1.0
      */
-    public static function log($message,$addDashes=false){
+    public static function log($message,$logName=null,$addDashes=false){
+        self::logName($logName);
         self::_get()->writeToLog($message,$addDashes);
     }
     /**
@@ -110,28 +156,85 @@ class Logger {
         }
         return self::_get()->_getLogName();
     }
-    
+    /**
+     * Removes the whole content of the log file.
+     * @since 1.0
+     */
+    public static function clear(){
+        self::_get()->clearLog();
+    }
+    /**
+     * @since 1.0
+     */
+    private function clearLog() {
+        file_put_contents($this->_getLogName(), "");
+    }
+    /**
+     * 
+     * @param type $bool
+     * @since 1.0
+     */
+    private function _setEnabled($bool){
+        $this->isEnabled = $bool === TRUE ? TRUE : FALSE;
+    }
+    /**
+     * 
+     * @return type
+     * @since 1.0
+     */
+    private function _isEnabled() {
+        return $this->isEnabled;
+    }
+    /**
+     * 
+     * @param type $name
+     * @since 1.0
+     */
     private function _setLogName($name) {
         $this->logFileName = $name;
     }
+    /**
+     * 
+     * @return type
+     * @since 1.0
+     */
     private function _getLogName() {
         return $this->logFileName;
     }
+    /**
+     * 
+     * @param type $dir
+     * @param type $create
+     * @since 1.0
+     */
     private function _setDirectory($dir,$create=true){
         if(Util::isDirectory($dir, $create)){
             $this->directory = $dir;
         }
     }
+    /**
+     * 
+     * @return type
+     * @since 1.0
+     */
     private function _getDirectory() {
         return $this->directory;
     }
+    /**
+     * 
+     * @param type $content
+     * @param type $addDashes
+     * @since 1.0
+     */
     private function writeToLog($content,$addDashes=false) {
-        $this->handelr = fopen($this->_getDirectory().'/'.$this->_getLogName().'.txt', 'a+');
-        $time = date('Y-m-d h:i:s');
-        fwrite($this->handelr, '['.$time.']  '.$content."\n");
-        if($addDashes === TRUE){
-            fwrite($this->handelr, '-------------------------------------'."\n");
+        if($this->_isEnabled()){
+            $this->handelr = fopen($this->_getDirectory().'/'.$this->_getLogName().'.txt', 'a+');
+            $time = date('Y-m-d h:i:s T');
+            fwrite($this->handelr, '['.$time.']  '.$content."\n");
+            if($addDashes === TRUE){
+                fwrite($this->handelr, '-------------------------------------'."\n");
+            }
+            fclose($this->handelr);
         }
-        fclose($this->handelr);
     }
 }

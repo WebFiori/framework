8c184bd21ffab4e7e6e51da3ce812b22bd7c858d
Ibrahim <ibinshikh@hotmail.com>
2018-09-08 20:03:34 +0300

    Updates

--- entity/cron/CronJob.php
+++ entity/cron/CronJob.php
@@ -1,32 +1,100 @@
 <?php
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Ibrahim.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 /**
- * Description of CronJob
+ * A class thar represents a cron job.
  *
  * @author Ibrahim <ibinshikh@hotmail.com>
+ * @version 1.0
  */
 class CronJob {
+    /**
+     * A constant that indicates a sub cron expression is of type 'range'.
+     * @since 1.0
+     */
     const RANGE_VAL = 'r';
+    /**
+     * A constant that indicates a sub cron expression is of type 'step value'.
+     * @since 1.0
+     */
     const STEP_VAL = 's';
+    /**
+     * A constant that indicates a sub cron expression is of type 'multi-value'.
+     * @since 1.0
+     */
     const ANY_VAL = '*';
+    /**
+     * A constant that indicates a sub cron expression is of type 'specific value'.
+     * @since 1.0
+     */
     const SPECIFIC_VAL = 'spe';
+    /**
+     * A constant that indicates a sub cron expression is invalid.
+     * @since 1.0
+     */
     const INV_VAL = 'inv';
+    /**
+     * An associative array which holds the names and the numbers of year 
+     * months.
+     * @since 1.0
+     */
     const MONTHS_NAMES = array(
         'JAN'=>1,'FEB'=>2,'MAR'=>3,'APR'=>4,'MAY'=>5,'JUN'=>6,
         'JUL'=>7,'AUG'=>8,'SEP'=>9,'OCT'=>10,'NOV'=>11,'DEC'=>12
     );
+    /**
+     * An associative array which holds the names and the numbers of week 
+     * days.
+     * @since 1.0
+     */
     const WEEK_DAYS = array(
         'SAT'=>6,'SUN'=>0,'MON'=>1,'TUE'=>2,'WED'=>3,'THU'=>4,'FRI'=>5
     );
+    /**
+     * An array which contains all job details after parsing cron expression.
+     * @var array 
+     * @since 1.0
+     */
     private $jobDetails;
+    /**
+     * An array which contains the events that will be executed if it is the time 
+     * to execute the job.
+     * @var array
+     * @since 1.0 
+     */
     private $events;
     /**
-     * 
+     * Creates new instance of the class.
      * @param string $when [Optional] A cron expression. An exception will be thrown if 
      * the given expression is invalid. Default is '* * * * *' which means run 
      * the job every minute.
      * @throws Exception
+     * @since 1.0
      */
     public function __construct($when='* * * * *') {
+        Logger::logFuncCall(__METHOD__);
+        Logger::log('Initializing cron job...');
         $this->jobDetails = array(
             'minutes'=>array(),
             'hours'=>array(),
@@ -35,14 +103,6 @@ class CronJob {
             'days-of-week'=>array()
         );
         $this->events = array(
-            'before'=>array(
-                'func'=>function(){},
-                'params'=>array()
-            ),
-            'after'=>array(
-                'func'=>function(){},
-                'params'=>array()
-            ),
             'on'=>array(
                 'func'=>function(){},
                 'params'=>array()
@@ -61,13 +121,17 @@ class CronJob {
             }
         }
         else{
+            Logger::log('No expression is given. Using default.');
             $this->cron();
         }
+        Logger::logFuncReturn(__METHOD__);
     }
     /**
-     * 
-     * @param type $when
-     * @return boolean
+     * Creates a cron job using specific cron expression.
+     * @param string $when A cron expression (such as '8 15 * * 1')
+     * @return boolean If the given cron expression is valid, the function will 
+     * set the time of cron job as specified by the expression and return 
+     * TRUE. If the expression is invalid, the function will return FALSE.
      * @since 1.0
      */
     public function cron($when='* * * * *'){
@@ -467,7 +531,7 @@ class CronJob {
      * @return boolean
      * @since 1.0
      */
-    public function _checkMonth($monthField){
+    private function _checkMonth($monthField){
         Logger::logFuncCall(__METHOD__);
         $isValidExpr = TRUE;
         $split = explode(',', $monthField);
@@ -551,39 +615,73 @@ class CronJob {
         Logger::logFuncReturn(__METHOD__);
         return $monthAttrs;
     }
+    /**
+     * Checks if current day of month in time is a day at which the job must be 
+     * executed.
+     * @return boolean The function will return TRUE if the current day of month in 
+     * time is a day at which the job must be executed.
+     * @since 1.0
+     */
     public function isDayOfMonth() {
+        Logger::logFuncCall(__METHOD__);
         $monthDaysArr = $this->jobDetails['days-of-month'];
+        Logger::log('Checking if the job will be executed every day of month...');
+        Logger::log('every-day = \''.$monthDaysArr['every-day'].'\'.', 'debug');
         if($monthDaysArr['every-day'] === TRUE){
-            return TRUE;
+            Logger::log('It will be executed every day of month.');
+            $retVal = TRUE;
         }
         else{
+            Logger::log('Checking if the current day is in range of days...');
             $retVal = FALSE;
             $current = intval(date('d'));
+            Logger::log('Current day of month = \''.$current.'\'.', 'debug');
             $ranges = $monthDaysArr['at-range'];
             foreach ($ranges as $range){
+                Logger::log('Min Range Value = \''.$range[0].'\', Max Range Value = \''.$range[1].'\'.', 'debug');
                 if($current >= $range[0] && $current <= $range[1]){
+                    Logger::log('It is in given range.');
                     $retVal = TRUE;
                     break;
                 }
             }
             if($retVal === FALSE){
+                Logger::log('Checking if day is in a set of specific days...');
                 $days = $monthDaysArr['at-every-x-day'];
                 $retVal = in_array($current, $days);
+                if($retVal === TRUE){
+                    Logger::log('It is a specific day.');
+                }
             }
         }
+        Logger::logReturnValue($retVal);
+        Logger::logFuncReturn(__METHOD__);
         return $retVal;
     }
+    /**
+     * Checks if current day of week in time is a day at which the job must be 
+     * executed.
+     * @return boolean The function will return TRUE if the current day of week in 
+     * time is a day at which the job must be executed.
+     * @since 1.0
+     */
     public function isDayOfWeek() {
+        Logger::logFuncCall(__METHOD__);
         $daysArr = $this->jobDetails['days-of-week'];
+        Logger::log('Checking if the job will be executed every day of week...');
+        Logger::log('every-day = \''.$daysArr['every-day'].'\'.', 'debug');
         if($daysArr['every-day'] === TRUE){
-            return TRUE;
+            Logger::log('It will be executed every day of week.');
+            $retVal = TRUE;
         }
         else{
             $retVal = FALSE;
             $current = intval(date('w'));
+            Logger::log('Current day of week = \''.$current.'\'.', 'debug');
             $ranges = $daysArr['at-range'];
             foreach ($ranges as $range){
                 if($current >= $range[0] && $current <= $range[1]){
+                    Logger::log('It is in given range.');
                     $retVal = TRUE;
                     break;
                 }
@@ -593,19 +691,35 @@ class CronJob {
                 $retVal = in_array($current, $days);
             }
         }
+        Logger::logReturnValue($retVal);
+        Logger::logFuncReturn(__METHOD__);
         return $retVal;
     }
+    /**
+     * Checks if current month in time is a month at which the job must be 
+     * executed.
+     * @return boolean The function will return TRUE if the current month in 
+     * time is a month at which the job must be executed.
+     * @since 1.0
+     */
     public function isMonth(){
+        Logger::logFuncCall(__METHOD__);
         $monthsArr = $this->jobDetails['months'];
+        Logger::log('Checking if the job will be executed every month...');
+        Logger::log('every-month = \''.$monthsArr['every-month'].'\'.', 'debug');
         if($monthsArr['every-month'] === TRUE){
-            return TRUE;
+            Logger::log('It will be executed every month.');
+            $retVal = TRUE;
         }
         else{
             $retVal = FALSE;
             $current = intval(date('m'));
+            Logger::log('Current month = \''.$current.'\'.', 'debug');
             $ranges = $monthsArr['at-range'];
             foreach ($ranges as $range){
+                Logger::log('Min Range Value = \''.$range[0].'\', Max Range Value = \''.$range[1].'\'.', 'debug');
                 if($current >= $range[0] && $current <= $range[1]){
+                    Logger::log('It is in given range.');
                     $retVal = TRUE;
                     break;
                 }
@@ -615,19 +729,35 @@ class CronJob {
                 $retVal = in_array($current, $months);
             }
         }
+        Logger::logReturnValue($retVal);
+        Logger::logFuncReturn(__METHOD__);
         return $retVal;
     }
+    /**
+     * Checks if current hour in time is an hour at which the job must be 
+     * executed.
+     * @return boolean The function will return TRUE if the current hour in 
+     * time is an hour at which the job must be executed.
+     * @since 1.0
+     */
     public function isHour(){
+        Logger::logFuncCall(__METHOD__);
         $hoursArr = $this->jobDetails['hours'];
+        Logger::log('Checking if the job will be executed every hour...');
+        Logger::log('every-hour = \''.$hoursArr['every-hour'].'\'.', 'debug');
         if($hoursArr['every-hour'] === TRUE){
-            return TRUE;
+            Logger::log('It will be executed every hour.');
+            $retVal = TRUE;
         }
         else{
             $retVal = FALSE;
             $current = intval(date('H'));
+            Logger::log('Current hour = \''.$current.'\'.', 'debug');
             $ranges = $hoursArr['at-range'];
             foreach ($ranges as $range){
+                Logger::log('Min Range Value = \''.$range[0].'\', Max Range Value = \''.$range[1].'\'.', 'debug');
                 if($current >= $range[0] && $current <= $range[1]){
+                    Logger::log('It is in given range.');
                     $retVal = TRUE;
                     break;
                 }
@@ -646,37 +776,61 @@ class CronJob {
                 }
             }
         }
+        Logger::logReturnValue($retVal);
+        Logger::logFuncReturn(__METHOD__);
         return $retVal;
     }
+    /**
+     * Checks if current minute in time is a minute at which the job must be 
+     * executed.
+     * @return boolean The function will return TRUE if the current minute in 
+     * time is a minute at which the job must be executed.
+     * @since 1.0
+     */
     public function isMinute(){
+        Logger::logFuncCall(__METHOD__);
         $minuteArr = $this->jobDetails['minutes'];
+        Logger::log('Checking if the job will be executed every minute...');
+        Logger::log('every-minute = \''.$minuteArr['every-minute'].'\'.', 'debug');
         if($minuteArr['every-minute'] === TRUE){
-            return TRUE;
+            Logger::log('It will be executed every minute.');
+            $retVal = TRUE;
         }
         else{
+            Logger::log('Checking if current minute is in ranges of minutes...');
             $retVal = FALSE;
             $current = intval(date('i'));
+            Logger::log('Current minute = \''.$current.'\'.', 'debug');
             $ranges = $minuteArr['at-range'];
             foreach ($ranges as $range){
+                Logger::log('Min Range Value = \''.$range[0].'\', Max Range Value = \''.$range[1].'\'.', 'debug');
                 if($current >= $range[0] && $current <= $range[1]){
+                    Logger::log('It is in given range.');
                     $retVal = TRUE;
                     break;
                 }
             }
             if($retVal === FALSE){
+                Logger::log('Checking if job will be executed at the specific minute...');
                 $minutes = $minuteArr['at-every-x-minute'];
                 $retVal = in_array($current, $minutes);
                 if($retVal === FALSE){
                     $minutes = $minuteArr['every-x-minutes'];
                     foreach ($minutes as $min){
                         if($current % $min == 0){
+                            Logger::log('Job will be executed at specific minute.');
                             $retVal = TRUE;
                             break;
                         }
                     }
                 }
+                else{
+                    Logger::log('Job will be executed at specific minute.');
+                }
             }
         }
+        Logger::logReturnValue($retVal);
+        Logger::logFuncReturn(__METHOD__);
         return $retVal;
     }
     /**
@@ -685,7 +839,7 @@ class CronJob {
      * @return boolean
      * @since 1.0
      */
-    public function _checkDayOfWeek($dayOfWeekField){
+    private function _checkDayOfWeek($dayOfWeekField){
         Logger::logFuncCall(__METHOD__);
         $isValidExpr = TRUE;
         $split = explode(',', $dayOfWeekField);
@@ -770,26 +924,47 @@ class CronJob {
         return $dayAttrs;
     }
     /**
-     * 
-     * @return array
+     * Returns an associative array which contains details about the timings 
+     * at which the job will be executed.
+     * @return array The array will have the following indices: 
+     * <ul>
+     * <li><b>minutes</b>: Contains sub arrays which has info about the minutes 
+     * at which the job will be executed.</li>
+     * <li><b>hours</b>: Contains sub arrays which has info about the hours 
+     * at which the job will be executed.</li>
+     * <li><b>days-of-month</b>: Contains sub arrays which has info about the days of month 
+     * at which the job will be executed.</li>
+     * <li><b>months</b>: Contains sub arrays which has info about the months 
+     * at which the job will be executed.</li>
+     * <li><b>days-of-week</b>: Contains sub arrays which has info about the days of week 
+     * at which the job will be executed.</li>
+     * </ul>
      * @since 1.0
      */
     public function getJobDetails() {
         return $this->jobDetails;
     }
     /**
-     * 
-     * @param type $func
-     * @param type $funcParams
+     * Sets the event that will be fired in case it is time to execute the job.
+     * @param function $func The function that will be executed if it is the 
+     * time to execute the job.
+     * @param array $funcParams An array which can hold some parameters that 
+     * can be passed to the function.
      * @since 1.0
      */
     public function setOnExecution($func,$funcParams=array()){
+        Logger::logFuncCall(__METHOD__);
+        Logger::log('Checking if first parameter is callable...');
         if(is_callable($func)){
+            Logger::log('It is callable. Setting the on execute event.');
             $this->events['on']['func'] = $func;
+            Logger::log('Checking if the second parameter is an array.');
             if(gettype($funcParams) == 'array'){
+                Logger::log('It is an array. Setting the array as callable parameters.');
                 $this->events['on']['params'] = $funcParams;
             }
         }
+        Logger::logFuncReturn(__METHOD__);
     }
     /**
      * @since 1.0

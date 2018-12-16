f96dca516e7ae1cda254d52d992014ced393205d
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2018-12-16 20:08:24 +0300

    Update ExamplePage.php

--- pages/ExamplePage.php
+++ pages/ExamplePage.php
@@ -53,7 +53,7 @@ class ExamplePage{
     public function __construct() {
         //load UI template components (JS, CSS and others)
         //it is optional. to use a theme but recomended
-        Page::theme($themeName='WebFiori Theme');
+        Page::theme($themeName='Greeny By Ibrahim Ali');
 
         //sets the title of the page
         $lang = Page::lang();
@@ -71,10 +71,23 @@ class ExamplePage{
             $p->addText('Hello from "WebFiori Framework"!');
             Page::insert($p);
         }
-        $image = new HTMLNode('img',FALSE);
-        $image->setAttribute('src', Page::imagesDir().'/image.png');
-        Page::insert($image);
-        //display the view
+        $sec = new HTMLNode('section');
+        Page::insert($sec);
+        $secH = new HTMLNode('h1');
+        $secH->addTextNode('Datagram');
+        $sec->addChild($secH);
+        $p2 = new PNode();
+        $p2->addText('A datagram is a basic transfer unit associated with a packet-switched network. Datagrams are typically structured in header and payload sections. Datagrams provide a connectionless communication service across a packet-switched network. The delivery, arrival time, and order of arrival of datagrams need not be guaranteed by the network. ');
+        $sec->addChild($p2);
+        
+        $sec = new HTMLNode('section');
+        Page::insert($sec);
+        $secH = new HTMLNode('h1');
+        $secH->addTextNode('History');
+        $sec->addChild($secH);
+        $p2 = new PNode();
+        $p2->addText('The term datagram appeared first within the project CYCLADES, a packet-switched network created in the early 1970s, and was coined by Louis Pouzin[1] by combining the words data and telegram. CYCLADES was the first network to make the hosts responsible for the reliable delivery of data, rather than the network itself, using unreliable datagrams and associated end-to-end principle.');
+        $sec->addChild($p2);
         Page::render();
     }
 }

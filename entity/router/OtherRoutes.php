a770f1f7e08be80f52c748dc574039fd5a73d47e
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-05 20:02:37 +0300

    Create OtherRoutes.php

---
+++ entity/router/OtherRoutes.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2018 ibrah.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace webfiori\entity\router;
+if(!defined('ROOT_DIR')){
+    header("HTTP/1.1 403 Forbidden");
+    die(''
+        . '<!DOCTYPE html>'
+        . '<html>'
+        . '<head>'
+        . '<title>Forbidden</title>'
+        . '</head>'
+        . '<body>'
+        . '<h1>403 - Forbidden</h1>'
+        . '<hr>'
+        . '<p>'
+        . 'Direct access not allowed.'
+        . '</p>'
+        . '</body>'
+        . '</html>');
+}
+/**
+ * A class that only has one method to initiate some of system routes.
+ * The class is meant to only initiate the routes which uses the method 
+ * Router::other().
+ * @author Eng.Ibrahim
+ * @version 1.0
+ */
+class OtherRoutes {
+    /**
+     * Create all other routes. Include your own here.
+     * @since 1.0
+     */
+    public static function create(){
+        //Router::other('my-custom/view-something', 'my-folder/file.php');
+    }
+}

6fd585730d4e10bee9a67195f91f94374911c1e0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-08 11:36:16 +0300

    Create InitAutoLoad.php
    Added this file to allow developers to add new class search-folders

---
+++ ini/InitAutoLoad.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Ibrahim.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace webfiori\ini;
+use webfiori\entity\AutoLoader;
+/**
+ * A class that has one method to initialize user-defined autoload directories.
+ *
+ * @author Ibrahim
+ * @version 1.0
+ */
+class InitAutoLoad {
+    /**
+     * Add user-defined directories to the set of directories at which the framework 
+     * will search for classes.
+     * The developer can use the method AutoLoader::newSearchFolder() to add 
+     * new search directory.
+     */
+    public static function init() {
+        $AU = AutoLoader::get();
+        //$AU->newSearchFolder('my-system');
+        //$AU->newSearchFolder('my-entities', FALSE);
+    }
+}

4cbab80b5db02e75276360e62ea5ed67319ed86f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-12 02:12:19 +0300

    Updated phMySQL library.
    Updated from v1.3.1 to 1.3.3.

--- entity/phMysql/MySQLQuery.php
+++ entity/phMysql/MySQLQuery.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Ibrahim BinAlshikh, phMysql library.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 namespace phMysql;
 use Exception;
 /**
  * A base class that is used to construct MySQL queries. It can be used as a base 
  * class for constructing other MySQL queries.
- * @author Ibrahim <ibinshikh@hotmail.com>
+ * @author Ibrahim
  * @version 1.8.5
  */
 abstract class MySQLQuery{
@@ -1092,7 +1115,7 @@ abstract class MySQLQuery{
      */
     public function selectMax($col,$rename='max'){
         return $this->select(array(
-            'column'=> $this->getColName($col),
+            'column'=> $col,
             'select-max'=>TRUE,
             'rename-to'=>$rename
         ));
@@ -1108,7 +1131,7 @@ abstract class MySQLQuery{
      */
     public function selectMin($col,$rename='min'){
         return $this->select(array(
-            'column'=>$this->getColName($col),
+            'column'=>$col,
             'select-min'=>TRUE,
             'rename-to'=>$rename
         ));

3a55961ac4d98548a6fae1948c1bca57071f2503
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-12 02:28:13 +0300

    Updated license headers.

--- entity/Logger.php
+++ entity/Logger.php
@@ -2,7 +2,7 @@
 /*
  * The MIT License
  *
- * Copyright 2018 ibrah.
+ * Copyright 2019 Ibrahim, WebFiori Framework.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

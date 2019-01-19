ff2d7f46231ca8d6cd06cb44aea6e79a08a74230
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-19 15:32:51 +0300

    Update ExamplePage.php

--- pages/ExamplePage.php
+++ pages/ExamplePage.php
@@ -1,9 +1,8 @@
 <?php
-
 /* 
  * The MIT License
  *
- * Copyright 2018 Ibrahim.
+ * Copyright 2019 Ibrahim, WebFiori Framework.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

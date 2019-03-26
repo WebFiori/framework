5a160744f192aed5b0d6bdd1e46d9477ba5cc359
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-03-26 10:15:58 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,22 @@
 <?php
+use webfiori\entity\AutoLoader;
 $root = trim(__DIR__,DIRECTORY_SEPARATOR.'tests');
-echo 'Root = \''.__DIR__.'\'.'."\n";
 echo 'Include Path: \''. get_include_path().'\''."\n";
 print_r(explode(DIRECTORY_SEPARATOR, $root));
 if(explode(DIRECTORY_SEPARATOR, $root)[0] == 'home'){
     //linux 
     require_once '/'.trim($root,'/\\').DIRECTORY_SEPARATOR.'entity'.DIRECTORY_SEPARATOR.'AutoLoader.php';
+    AutoLoader::get(array(
+        'search-folders'=>array(
+            'tests\\entity\\router'
+        ),
+        'root'=> $root
+    ));
+    AutoLoader::newSearchFolder(DIRECTORY_SEPARATOR.'home');
 }
 else{
     require_once trim($root,'/\\').DIRECTORY_SEPARATOR.'entity'.DIRECTORY_SEPARATOR.'AutoLoader.php';
 }
-use webfiori\entity\AutoLoader;
-AutoLoader::get(array(
-    'search-folders'=>array(
-        'tests\\entity\\router'
-    ),
-    'root'=> $root
-));
 echo 'Autoloader Initialized.'."\n";
 echo 'Root Directory: \''.AutoLoader::get()->getRoot().'\'.'."\n";
 echo 'Class Search Paths:'."\n";

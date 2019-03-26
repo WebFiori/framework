c82e224b2a9a21c8596b618bf824fcd014935420
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-03-26 11:02:26 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 use webfiori\entity\AutoLoader;
 $root = trim(__DIR__,DIRECTORY_SEPARATOR.'tests');
-echo 'Include Path: \''. get_include_path().'\''."\n";
+//echo 'Include Path: \''. get_include_path().'\''."\n";
 if(explode(DIRECTORY_SEPARATOR, $root)[0] == 'home'){
     //linux 
     require_once '/'.trim($root,'/\\').DIRECTORY_SEPARATOR.'entity'.DIRECTORY_SEPARATOR.'AutoLoader.php';
@@ -16,10 +16,10 @@ AutoLoader::get(array(
     'root'=> $root,
     'on-load-failure'=>'do-nothing'
 ));
-echo 'Autoloader Initialized.'."\n";
-echo 'Root Directory: \''.AutoLoader::get()->getRoot().'\'.'."\n";
-echo 'Class Search Paths:'."\n";
-$dirs = AutoLoader::getFolders();
-foreach ($dirs as $dir){
-    echo $dir."\n";
-}
+//echo 'Autoloader Initialized.'."\n";
+//echo 'Root Directory: \''.AutoLoader::get()->getRoot().'\'.'."\n";
+//echo 'Class Search Paths:'."\n";
+//$dirs = AutoLoader::getFolders();
+//foreach ($dirs as $dir){
+//    echo $dir."\n";
+//}

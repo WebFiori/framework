27397db51889830d1e7cf9ea5eaa843944774dc9
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-05 17:31:35 +0300

    Update FunctionsTest.php

--- tests/entity/functions/FunctionsTest.php
+++ tests/entity/functions/FunctionsTest.php
@@ -22,10 +22,34 @@ class FunctionsTest extends TestCase{
      * @test
      */
     public function testSetConnection01() {
-        $connection = new DBConnectionInfo('root', '123456', 'test_db');
+        $connection = new DBConnectionInfo('root', '123456', 'testing_db');
         WebFiori::getConfig()->addDbConnection('test-connection', $connection);
         $func = new Functions();
         $result = $func->setConnection('test-connection');
         $this->assertTrue($result);
+    }
+    /**
+     * @test
+     */
+    public function testUseDatabase00() {
+        $func = new Functions();
+        $result = $func->useDatabase('not-exist');
+        $this->assertEquals(Functions::NO_SUCH_CONNECTION,$result);
+        $errDetails = $func->getDBErrDetails();
+        $this->assertEquals(-1,$errDetails['error-code']);
+        $this->assertEquals('No database connection was found which has the name \'not-exist\'.',$errDetails['error-message']);
+    }
+    /**
+     * @test
+     */
+    public function testUseDatabase01() {
+        $connection = new DBConnectionInfo('root', '12345', 'testing_db');
+        WebFiori::getConfig()->addDbConnection('test-connection', $connection);
+        $func = new Functions();
+        $result = $func->useDatabase('test-connection');
+        $this->assertFalse($result);
+        $errDetails = $func->getDBErrDetails();
+        $this->assertEquals(1045,$errDetails['error-code']);
+        $this->assertEquals("Access denied for user 'root'@'localhost' (using password: YES)",$errDetails['error-message']);
     }
 }

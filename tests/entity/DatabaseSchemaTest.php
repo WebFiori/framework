e35c168696a661527791257d5f198850f3386565
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-19 15:03:16 +0300

    Create DatabaseSchemaTest.php

---
+++ tests/entity/DatabaseSchemaTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace webfiori\tests\entity;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A test class for testing the class 'webfiori\entity\DatabaseSchema'.
+ *
+ * @author Ibrahim
+ */
+class DatabaseSchemaTest extends TestCase{
+    
+}

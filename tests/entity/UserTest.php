d5ddc0cb67842917149f17cbffb111e433925801
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-19 15:02:56 +0300

    Create UserTest.php

---
+++ tests/entity/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace webfiori\tests\entity;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A test class for testing the class 'webfiori\entity\User'.
+ *
+ * @author Ibrahim
+ */
+class UserTest extends TestCase{
+    
+}

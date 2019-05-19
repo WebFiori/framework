c995cfab646fc1fb9d3001e8229f0a348078231f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-19 15:03:11 +0300

    Create FileTest.php

---
+++ tests/entity/FileTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace webfiori\tests\entity;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A test class for testing the class 'webfiori\entity\File'.
+ *
+ * @author Ibrahim
+ */
+class FileTest extends TestCase{
+    
+}

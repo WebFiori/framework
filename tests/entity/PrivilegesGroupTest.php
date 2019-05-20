30964607a4ad98d1d4b3755602a556d8ba583b55
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-20 14:56:38 +0300

    Update PrivilegesGroupTest.php

--- tests/entity/PrivilegesGroupTest.php
+++ tests/entity/PrivilegesGroupTest.php
@@ -43,6 +43,26 @@ class PrivilegesGroupTest extends TestCase{
         $this->assertSame('2',$group->getName());
     }
     /**
+     * @test
+     */
+    public function testConstructor04() {
+        $group = new PrivilegesGroup('','');
+        $this->assertEquals('GROUP',$group->getID());
+        $this->assertEquals('G_NAME',$group->getName());
+    }
+    /**
+     * @test
+     */
+    public function testSetParentGroup00() {
+        $child = new PrivilegesGroup('CH_GROUP_1', 'Child Group #1');
+        $parentGroup = new PrivilegesGroup('PARENT_1', 'Parent Group #1');
+        $this->assertFalse($child->setParentGroup($child));
+        $this->assertTrue($child->setParentGroup($parentGroup));
+        $this->assertSame($parentGroup,$child->getParentGroup());
+        $this->assertEquals(1,count($child->getParentGroup()->childGroups()));
+        $this->assertSame($child,$parentGroup->childGroups()[0]);
+    }
+    /**
      * 
      * @param PrivilegesGroup $group
      * @depends testConstructor02

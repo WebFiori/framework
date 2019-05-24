2f9991f6e3528f3a3e1d66f41ef9448cc7e28b8f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-24 23:28:02 +0300

    Update FileTest.php

--- tests/entity/FileTest.php
+++ tests/entity/FileTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace webfiori\tests\entity;
 use PHPUnit\Framework\TestCase;
+use webfiori\entity\File;
 
 /**
  * A test class for testing the class 'webfiori\entity\File'.
@@ -8,5 +9,48 @@ use PHPUnit\Framework\TestCase;
  * @author Ibrahim
  */
 class FileTest extends TestCase{
-    
+    /**
+     * @test
+     */
+    public function test00() {
+        $file = new File();
+        $this->assertEquals('',$file->getName());
+        $this->assertEquals('',$file->getPath());
+        $this->assertEquals(-1,$file->getID());
+        $this->assertNull($file->getRawData());
+        $this->assertEquals('application/octet-stream',$file->getFileMIMEType());
+        return $file;
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $file = new File('text-file.txt',ROOT_DIR.DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'entity');
+        $this->assertEquals('text-file.txt',$file->getName());
+        $this->assertEquals(ROOT_DIR.DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'entity',$file->getPath());
+        $this->assertEquals(-1,$file->getID());
+        $this->assertNull($file->getRawData());
+        $this->assertEquals('application/octet-stream',$file->getFileMIMEType());
+        return $file;
+    }
+    /**
+     * @test
+     * @depends test00
+     * @param File $file
+     */
+    public function testRead00($file) {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('File absolute path is invalid.');
+        $file->read();
+    }
+    /**
+     * @test
+     * @depends test01
+     * @param File $file
+     */
+    public function testRead01($file) {
+        $file->read();
+        $this->assertEquals('text/plain',$file->getFileMIMEType());
+        $this->assertEquals("Testing the class 'File'.\n",$file->getRawData());
+    }
 }

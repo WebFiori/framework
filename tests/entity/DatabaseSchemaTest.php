bddc53fba2033863498b9db92e8698457d7ffc82
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-24 23:27:52 +0300

    Update DatabaseSchemaTest.php

--- tests/entity/DatabaseSchemaTest.php
+++ tests/entity/DatabaseSchemaTest.php
@@ -1,12 +1,22 @@
 <?php
 namespace webfiori\tests\entity;
 use PHPUnit\Framework\TestCase;
-
+use webfiori\entity\DatabaseSchema;
+use webfiori\tests\entity\TestQuery_1;
+use webfiori\tests\entity\TestQuery_2;
 /**
  * A test class for testing the class 'webfiori\entity\DatabaseSchema'.
  *
  * @author Ibrahim
  */
 class DatabaseSchemaTest extends TestCase{
-    
+    /**
+     * @test
+     */
+    public function test00() {
+        $schema = DatabaseSchema::get();
+        //$schema->add('webfiori\tests\entity\TestQuery_1', 20);
+        $this->assertEquals("create database if not exists hello_world;\n"
+                . "use hello_world;\n",$schema->getCreateDatabaseStatement('hello_world'));
+    }
 }

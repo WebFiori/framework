c06a44406c36fd8f97a8ea86ef85ab6ab49dafe0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-29 04:43:19 +0300

    Update EmailMessageTest.php

--- tests/entity/mail/EmailMessageTest.php
+++ tests/entity/mail/EmailMessageTest.php
@@ -2,6 +2,8 @@
 namespace webfiori\tests\entity\mail;
 use PHPUnit\Framework\TestCase;
 use webfiori\entity\mail\EmailMessage;
+use webfiori\entity\mail\SMTPAccount;
+use webfiori\conf\MailConfig;
 /**
  * A test class for testing the class 'webfiori\entity\mail\EmailMessage'.
  *
@@ -16,4 +18,42 @@ class EmailMessageTest extends TestCase{
         $this->expectExceptionMessage('No SMTP account was found which has the name "not exist".');
         $message = EmailMessage::createInstance('not exist');
     }
+    /**
+     * @test
+     */
+    public function test01() {
+        $smtp = new SMTPAccount();
+        //$smtp->setServerAddress('mail.invalid.com');
+        MailConfig::get()->addSMTPAccount('smtp-acc-00', $smtp);
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('The account "smtp-acc-00" has inalid host or port number. Port: 465, Host: .');
+        $message = EmailMessage::createInstance('smtp-acc-00');
+    }
+    /**
+     * @test
+     */
+//    public function test02() {
+//        $smtp = new SMTPAccount();
+//        $smtp->setPassword('iz2)1Iimu#z');
+//        $smtp->setAddress('test@programmingacademia.com');
+//        $smtp->setUsername('test@programmingacademia.com');
+//        $smtp->setServerAddress('mail.programmingacademia.com ');
+//        $smtp->setPort(25);
+//        MailConfig::get()->addSMTPAccount('smtp-acc-00', $smtp);
+//        $message = EmailMessage::createInstance('smtp-acc-00');
+//    }
+    /**
+     * @test
+     */
+//    public function test03() {
+//        $smtp = new SMTPAccount();
+//        $smtp->setPassword('iz2)X1Iimu#z');
+//        $smtp->setAddress('test@programmingacademia.com');
+//        $smtp->setUsername('test@programmingacademia.com');
+//        $smtp->setServerAddress('mail.programmingacademia.com ');
+//        $smtp->setPort(25);
+//        MailConfig::get()->addSMTPAccount('smtp-acc-00', $smtp);
+//        $message = EmailMessage::createInstance('smtp-acc-00');
+//        $this->assertTrue($message instanceof EmailMessage);
+//    }
 }

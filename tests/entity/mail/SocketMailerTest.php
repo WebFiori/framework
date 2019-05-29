0d8588320db99b0cf3d11ed20ddeb23a0c695972
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-05-30 00:05:08 +0300

    Update SocketMailerTest.php

--- tests/entity/mail/SocketMailerTest.php
+++ tests/entity/mail/SocketMailerTest.php
@@ -37,4 +37,52 @@ class SocketMailerTest extends TestCase{
         $sm->setPriority(0);
         $this->assertSame(0,$sm->getPriority());
     }
+    /**
+     * @test
+     */
+    public function testAddReciver00() {
+        $sm = new SocketMailer();
+        $this->assertFalse($sm->addReceiver('', ''));
+        $this->assertFalse($sm->addReceiver('Hello', ''));
+        $this->assertFalse($sm->addReceiver('', 'hello@web.com'));
+    }
+    /**
+     * @test
+     */
+    public function testAddReciver01() {
+        $sm = new SocketMailer();
+        $this->assertTrue($sm->addReceiver('  <Hello  ', '  hello@>hello.com'));
+        $this->assertEquals('Hello <hello@hello.com>',$sm->getReceiversStr());
+        $this->assertEquals('Hello',$sm->getReceivers()['hello@hello.com']);
+        $this->assertTrue($sm->addReceiver('  <Hello2  ', '  hello@>hello.com'));
+        $this->assertEquals('Hello2 <hello@hello.com>',$sm->getReceiversStr());
+        $this->assertTrue($sm->addReceiver('Hel>lo-9  ', '  hello-9@>hello.com'));
+        $this->assertEquals('Hello2 <hello@hello.com>,Hello-9 <hello-9@hello.com>',$sm->getReceiversStr());
+    }
+    /**
+     * @test
+     */
+    public function testAddReciver02() {
+        $sm = new SocketMailer();
+        $this->assertTrue($sm->addReceiver('  <Hello  ', '  hello@>hello.com',true));
+        $this->assertEquals('Hello <hello@hello.com>',$sm->getCCStr());
+        $this->assertEquals('Hello',$sm->getCC()['hello@hello.com']);
+        $this->assertTrue($sm->addReceiver('  <Hello2  ', '  hello@>hello.com',true));
+        $this->assertEquals('Hello2 <hello@hello.com>',$sm->getCCStr());
+        $this->assertTrue($sm->addReceiver('Hel>lo-9  ', '  hello-9@>hello.com',true));
+        $this->assertEquals('Hello2 <hello@hello.com>,Hello-9 <hello-9@hello.com>',$sm->getCCStr());
+    }
+    /**
+     * @test
+     */
+    public function testAddReciver03() {
+        $sm = new SocketMailer();
+        $this->assertTrue($sm->addReceiver('  <Hello  ', '  hello@>hello.com',true,true));
+        $this->assertEquals('Hello <hello@hello.com>',$sm->getBCCStr());
+        $this->assertEquals('Hello',$sm->getBCC()['hello@hello.com']);
+        $this->assertTrue($sm->addReceiver('  <Hello2  ', '  hello@>hello.com',true,true));
+        $this->assertEquals('Hello2 <hello@hello.com>',$sm->getBCCStr());
+        $this->assertTrue($sm->addReceiver('Hel>lo-9  ', '  hello-9@>hello.com',true,true));
+        $this->assertEquals('Hello2 <hello@hello.com>,Hello-9 <hello-9@hello.com>',$sm->getBCCStr());
+    }
 }

2f6c5f68f88643522feaae938b563c23b51f599c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-07-23 01:04:57 +0300

    Update WebFioriTheme.php

--- src/themes/webfiori/WebFioriTheme.php
+++ src/themes/webfiori/WebFioriTheme.php
@@ -48,8 +48,7 @@ class WebFioriTheme extends Theme{
             Page::document()->getChildByID('page-body')->setClassName('wf-row');
             Page::document()->getChildByID('page-header')->setClassName('wf-row-np');
             Page::document()->getChildByID('page-footer')->setClassName('wf-row');
-            //WebFioriGUI::createTitleNode();
-
+            Page::siteName(WebFiori::getSiteConfig()->getWebsiteNames()[Page::lang()]);
             LangExt::extLang();
             $translation = &Page::translation();
             //adding menu items 
@@ -130,7 +129,7 @@ class WebFioriTheme extends Theme{
         $p->addText('youremail@example.com',array('new-line'=>true));
         $contactInfo->addChild($p);
         $node->addChild($contactInfo);
-        $p->addText('Your Copyright Notice © 2018');
+        $p->addText('Your Copyright Notice © '. date('Y'));
         $div = new HTMLNode('div');
         $div->setAttribute('class', 'wf-ltr-col-12');
         $div->addTextNode('<b style="color:gray;font-size:8pt;">Powered By: <a href="https://github.com/usernane/webfiori" '
@@ -146,6 +145,7 @@ class WebFioriTheme extends Theme{
         $headTag->addCSS(Page::cssDir().'/Grid.css');
         $headTag->addCSS(Page::cssDir().'/colors.css');
         $headTag->addCSS(Page::cssDir().'/theme.css');
+        $headTag->addJs('sys-files/res/js/webfiori.js');
         $headTag->addMeta('robots', 'index, follow');
         return $headTag;
     }
@@ -214,6 +214,8 @@ class WebFioriTheme extends Theme{
      * <li>"with-margin", a boolean. If set to true, the column will have margins. Default is true.</li>
      * </ul>
      * </li>
+     * <li>"status-label". A row with a label inside it which has a paragraph 
+     * with ID = "status-label"</li>
      * <li>"input-element". A row which represents input element alongside its components. 
      * This type has the following options:
      * <ul>
@@ -225,6 +227,10 @@ class WebFioriTheme extends Theme{
      * <li>"placeholder" A text to show as a placeholder.</li>
      * <li>"on-input". A String that represents JavaScript code which 
      * will be executed when input element value changes.</li>
+     * <li>
+     * "name". A string that is used when input type is "radio". Its the value 
+     * of the attribute "name" of the radio button.
+     * </li>
      * <li>"select-data". An array of sub-associative arrays that has an options which are 
      * used if input element type is "select". Each sub array can have the following indices:
      * <ul>
@@ -289,15 +295,23 @@ class WebFioriTheme extends Theme{
             $titleRow->addChild($h1);
             return $titleRow;
         }
+        else if($nodeType == 'status-label'){
+            $statusContainer = $this->createHTMLNode(['type'=>'wf-row']);
+            $statusContainer->setClassName($statusContainer->getAttributeValue('class').' status-label-container');
+            $statusLabel = new PNode();
+            $statusLabel->setID('status-label');
+            $statusContainer->addChild($statusLabel);
+            return $statusContainer;
+        }
         else if($nodeType == 'input-element'){
             $row = $this->createHTMLNode(['type'=>'wf-row']);
             $label = isset($options['label']) ? $options['label'] : 'Input_label';
             $labelNode = new Label($label);
             $inputId = isset($options['input-id']) ? $options['input-id'] : 'input-el';
             $labelNode->setAttribute('for', $inputId);
-            $row->addChild($labelNode);
             $inputType = isset($options['input-type']) ? $options['input-type'] : 'text';
             if($inputType == 'select'){
+                $row->addChild($labelNode);
                 $inputEl = new HTMLNode('select');
                 $inputEl->setID($inputId);
                 if(isset($options['select-data'])){
@@ -323,10 +337,29 @@ class WebFioriTheme extends Theme{
             else{
                 $inputEl = new Input($inputType);
                 $inputEl->setID($inputId);
-                $placeholder = isset($options['placeholder']) ? $options['placeholder'] : '';
-                $inputEl->setAttribute('placeholder', $placeholder);
                 $onInput = isset($options['on-input']) ? $options['on-input'] : "console.log(this.id+' has changed value.');";
-                $inputEl->setAttribute('oninput', $onInput);
+                if($inputType == 'submit'){
+                    $inputEl->setAttribute('onclick', $onInput);
+                    $inputEl->setAttribute('value', $label);
+                }
+                else if($inputType == 'checkbox' || $inputType == 'radio'){
+                    $labelNode->setStyle([
+                        'display'=>'inline-block'
+                    ]);
+                    $row->addChild($inputEl);
+                    $row->addChild($labelNode);
+                    if($inputType == 'radio'){
+                        $name = isset($options['name']) ? $options['name'] : 'radio-group';
+                        $inputEl->setName($name);
+                    }
+                    return $row;
+                }
+                else{
+                    $row->addChild($labelNode);
+                    $placeholder = isset($options['placeholder']) ? $options['placeholder'] : '';
+                    $inputEl->setAttribute('placeholder', $placeholder);
+                    $inputEl->setAttribute('oninput', $onInput);
+                }
             }
             $row->addChild($inputEl);
             return $row;

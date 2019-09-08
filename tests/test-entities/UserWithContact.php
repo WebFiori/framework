5868f00b7173ad03d16e69f7194a9bb57f8ed997
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-09 02:53:49 +0300

    Create UserWithContact.php

---
+++ tests/test-entities/UserWithContact.php
@@ -0,0 +1,22 @@
+<?php
+namespace webfiori\tests\testEntity;
+use webfiori\entity\User;
+
+/**
+ * Description of UserWithContact
+ *
+ * @author Ibrahim
+ */
+class UserWithContact extends User{
+    private $contactInfoArr;
+    public function __construct() {
+        parent::__construct();
+        $this->contactInfoArr = [];
+    }
+    public function addContactInfo($infoName,$info) {
+        $this->contactInfoArr[$infoName] = $info;
+    }
+    public function getContactInfo() {
+        return $this->contactInfoArr;
+    }
+}

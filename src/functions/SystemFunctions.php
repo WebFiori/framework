63d7a3ce3af16d3c44875535fb1bc4970fdfbea5
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-10 19:15:14 +0300

    Update SystemFunctions.php

--- src/functions/SystemFunctions.php
+++ src/functions/SystemFunctions.php
@@ -77,9 +77,9 @@ class SystemFunctions extends Functions{
      */
     const INITIAL_CONFIG_VARS = array(
         'is-config'=>'false',
-        'release-date'=>'2019-07-25',
+        'release-date'=>'2019-09-10',
         'version'=>'1.0.2',
-        'version-type'=>'Beta 1',
+        'version-type'=>'Stable',
         'config-file-version'=>'1.3.4',
         'databases'=>array()
     );

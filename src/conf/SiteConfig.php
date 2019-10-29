6395e596652ce61729ef4d4a21bac47113675154
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-10-30 02:22:46 +0300

    Updated config files

--- src/conf/SiteConfig.php
+++ src/conf/SiteConfig.php
@@ -1,34 +1,5 @@
 <?php
-/*
- * The MIT License
- *
- * Copyright 2019 Ibrahim, WebFiori Framework.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
- 
 namespace webfiori\conf;
-if(!defined('ROOT_DIR')){
-    header("HTTP/1.1 404 Not Found");
-    die('<!DOCTYPE html><html><head><title>Not Found</title></head><body>'
-    . '<h1>404 - Not Found</h1><hr><p>The requested resource was not found on the server.</p></body></html>');
-}
 use webfiori\entity\Util;
 /** 
  * Website configuration class.
@@ -121,8 +92,8 @@ class SiteConfig{
         $this->baseUrl = Util::getBaseURL();
         $this->titleSep = ' | ';
         $this->primaryLang = 'EN';
-        $this->baseThemeName = 'Greeny By Ibrahim Ali';
-        $this->adminThemeName = 'Greeny By Ibrahim Ali';
+        $this->baseThemeName = 'WebFiori Theme';
+        $this->adminThemeName = 'WebFiori Theme';
         $this->homePage = Util::getBaseURL();
         $this->descriptions = array('EN'=>'','AR'=>'',);
     }

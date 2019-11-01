4b2860233f2cdd913fff0f5a5ebce30a418ecc42
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-11-01 23:00:03 +0300

    Refactor

--- src/entity/mail/SMTPAccount.php
+++ src/entity/mail/SMTPAccount.php
@@ -23,11 +23,6 @@
  * THE SOFTWARE.
  */
 namespace webfiori\entity\mail;
-if(!defined('ROOT_DIR')){
-    header("HTTP/1.1 404 Not Found");
-    die('<!DOCTYPE html><html><head><title>Not Found</title></head><body>'
-    . '<h1>404 - Not Found</h1><hr><p>The requested resource was not found on the server.</p></body></html>');
-}
 /**
  * A class that represents an email account which is used to send or receive messages.
  *

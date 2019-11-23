38ea341e313dd8d92e730e170488b176f6a71fa3
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-11-23 23:42:44 +0300

    Update InitCron.php

--- src/ini/InitCron.php
+++ src/ini/InitCron.php
@@ -42,14 +42,14 @@ class InitCron {
     public static function init() {
         //set an optional password to protect jobs from 
         //unauthorized execution access
-        Cron::password('123456');
-        
+        //default password: 123456
+        Cron::password('8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
         //enable job execution log
         Cron::execLog(true);
         
         //add jobs
-        //Cron::dailyJob("13:00", "Test Job", function (){
-        //
-        //});
+//        Cron::dailyJob("13:00", "Test Job", function (){
+//
+//        });
     }
 }

35500715ac86d5a9c8fd555754f773f189e27961
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-05 04:32:27 +0300

    Update Router.php

--- src/entity/router/Router.php
+++ src/entity/router/Router.php
@@ -849,7 +849,6 @@ class Router {
                 call_user_func($route->getRouteTo(),$route->getClosureParams());
             }
 
-            return;
         } else {
             if ($route->getType() == self::API_ROUTE && !defined('API_CALL')) {
                 define('API_CALL', true);

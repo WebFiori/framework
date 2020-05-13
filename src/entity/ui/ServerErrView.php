ef306e68877243d4a916376c4a0fd3605cea3666
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-14 01:38:28 +0300

    Update ServerErrView.php

--- src/entity/ui/ServerErrView.php
+++ src/entity/ui/ServerErrView.php
@@ -44,6 +44,78 @@ class ServerErrView {
      * @since 1.0
      */
     public function __construct($throwableOrErr) {
+        if(class_exists('phpStructs\html\HTMLNode')){
+            $this->_phpStructsExist($throwableOrErr);
+        } else {
+            $this->_phpStructsDoesNotexist($throwableOrErr);
+        }
+    }
+    private function _phpStructsDoesNotexist($throwableOrErr) {
+        //this is a fall back if the library php-structs does not exist. 
+        //Output HTML as string.
+        echo '<!DOCTYPE html>'
+            . '<html>'
+            . '<head>';
+        if ($throwableOrErr instanceof Throwable) {
+            echo '<title>Uncaught Exception</title>'
+            . '<link href="'.Util::getBaseURL().'assets/css/server-err.css" rel="stylesheet">'
+            . '</head>'
+            . '<body>'
+            . '<h1>500 - Server Error: Uncaught Exception.</h1>'
+            . '<hr>'
+            . '<p>'
+            . '<b class="nice-red mono">Exception Class:</b> <span class="mono">'. get_class($throwableOrErr) ."</span><br/>"
+            . '<b class="nice-red mono">Exception Message:</b> <span class="mono">'.$throwableOrErr->getMessage()."</span><br/>"
+            . '<b class="nice-red mono">Exception Code:</b> <span class="mono">'.$throwableOrErr->getCode()."</span><br/>";
+
+            if (defined('VERBOSE') && VERBOSE) {
+                echo '<b class="nice-red mono">File:</b> <span class="mono">'.$throwableOrErr->getFile()."</span><br/>"
+                . '<b class="nice-red mono">Line:</b> <span class="mono">'.$throwableOrErr->getLine()."</span><br>"
+                . '<b class="nice-red mono">Stack Trace:</b> '."<br/>"
+                . '</p>'
+                . '<pre>'.$throwableOrErr->getTraceAsString().'</pre>';
+
+            } else {
+                $this->_showTip();
+            }
+            echo '</body></html>';
+        } else {
+            echo '<title>Server Error - 500</title>'
+                . '<link href="'.Util::getBaseURL().'assets/css/server-err.css" rel="stylesheet">'
+                . '</head>'
+                . '<body style="color:white;background-color:#1a000d;">'
+                . '<h1 style="color:#ff4d4d">500 - Server Error</h1>'
+                . '<hr>'
+                . '<p>'
+                .'<b class="nice-red mono">Type:</b> <span class="mono">'.Util::ERR_TYPES[$error["type"]]['type']."</span><br/>"
+                .'<b class="nice-red mono">Description:</b> <span class="mono">'.Util::ERR_TYPES[$error["type"]]['description']."</span><br/>"
+                .'<b class="nice-red mono">Message:</b> <span class="mono">'.$error["message"]."</span><br>";
+            if (defined('VERBOSE') && VERBOSE) {
+                echo '<b class="nice-red mono">File:</b> <span class="mono">'.$error["file"]."</span><br/>"
+                .'<b class="nice-red mono">Line:</b> <span class="mono">'.$error["line"]."</span><br/>" ;
+            } else {
+                $this->_showTip();
+            }
+        }
+        echo '</body></html>';
+    }
+    private function _showTip() {
+        if (class_exists('phpStructs\html\HTMLNode')) {
+            $paragraph = new HTMLNode('p');
+            $paragraph->setClassName('mono');
+            $paragraph->addTextNode('<b style="color:yellow">Tip</b>: To'
+                . ' display more details about the error, '
+                . 'define the constant "VERBOSE" and set its value to "true" in '
+                . 'the top of the class "WebFiori".', false);
+            Page::insert($paragraph);
+        } else {
+            echo '<p class="mono"><b style="color:yellow">Tip</b>: To'
+                . ' display more details about the error, '
+                . 'define the constant "VERBOSE" and set its value to "true" in '
+                . 'the top of the file "WebFiori.php".</p>';
+        }
+    }
+    private function _phpStructsExist($throwableOrErr) {
         Page::reset();
         Page::title('Uncaught Exception');
         Page::document()->getHeadNode()->addCSS('assets/css/server-err.css',[],false);
@@ -56,19 +128,27 @@ class ServerErrView {
             Page::insert($this->_createMessageLine('Exception Class:', get_class($throwableOrErr)));
             Page::insert($this->_createMessageLine('Exception Message:', $throwableOrErr->getMessage()));
             Page::insert($this->_createMessageLine('Exception Code:', $throwableOrErr->getCode()));
-            Page::insert($this->_createMessageLine('File:', $throwableOrErr->getFile()));
-            Page::insert($this->_createMessageLine('Line:', $throwableOrErr->getLine()));
-            Page::insert($this->_createMessageLine('Stack Trace:', ''));
-            $stackTrace = new HTMLNode('pre');
-            $stackTrace->addTextNode($throwableOrErr->getTraceAsString());
-            Page::insert($stackTrace);
+            if (defined('VERBOSE') && VERBOSE) {
+                Page::insert($this->_createMessageLine('File:', $throwableOrErr->getFile()));
+                Page::insert($this->_createMessageLine('Line:', $throwableOrErr->getLine()));
+                Page::insert($this->_createMessageLine('Stack Trace:', ''));
+                $stackTrace = new HTMLNode('pre');
+                $stackTrace->addTextNode($throwableOrErr->getTraceAsString());
+                Page::insert($stackTrace);
+            } else {
+                $this->_showTip();
+            }
         } else {
             $hNode->addTextNode('500 - Server Error');
             Page::insert($this->_createMessageLine('Type:', Util::ERR_TYPES[$throwableOrErr["type"]]['type']));
             Page::insert($this->_createMessageLine('Description:', Util::ERR_TYPES[$throwableOrErr["type"]]['description']));
             Page::insert($this->_createMessageLine('Message: ', $throwableOrErr["message"]));
-            Page::insert($this->_createMessageLine('File: ', $throwableOrErr["file"]));
-            Page::insert($this->_createMessageLine('Line: ', $throwableOrErr["line"]));
+            if (defined('VERBOSE') && VERBOSE) {
+                Page::insert($this->_createMessageLine('File: ', $throwableOrErr["file"]));
+                Page::insert($this->_createMessageLine('Line: ', $throwableOrErr["line"]));
+            } else {
+                $this->_showTip();
+            }
         }
     }
     /**

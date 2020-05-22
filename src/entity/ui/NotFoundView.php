f27c830343d1082b19d5f8121218ea7845ede08e
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-22 08:32:43 +0300

    Update NotFoundView.php

--- src/entity/ui/NotFoundView.php
+++ src/entity/ui/NotFoundView.php
@@ -41,6 +41,7 @@ class NotFoundView {
     public function __construct() {
         Page::theme(WebFiori::getSiteConfig()->getBaseThemeName());
         Page::lang(WebFiori::getSysController()->getSessionLang());
+        Page::siteName(WebFiori::getSiteConfig()->getWebsiteNames()[Page::lang()]);
         $labels = Page::translation()->get('general/http-codes/404');
         Page::title($labels['code'].' - '.$labels['type']);
         http_response_code($labels['code']);

c95ff95ef943fbeb9bf24438ee68d5b36b679a7a
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-24 05:23:25 +0300

    Update ListCronCommand.php

--- src/entity/cli/ListCronCommand.php
+++ src/entity/cli/ListCronCommand.php
@@ -48,22 +48,22 @@ class ListCronCommand extends CLICommand {
     public function exec() {
         $jobs = Cron::jobsQueue();
         $i = 1;
-        fprintf(STDOUT, "Number Of Jobs: ".$jobs->size()."\n");
+        $this->println("Number Of Jobs: ".$jobs->size());
 
         while ($job = $jobs->dequeue()) {
             if ($i < 10) {
-                fprintf(STDOUT, self::formatOutput("--------- Job #0$i ---------\n", [
+                $this->println("--------- Job #0$i ---------", [
                     'color' => 'light-blue',
                     'bold' => true
-                ]));
+                ]);
             } else {
-                fprintf(STDOUT, self::formatOutput("--------- Job #$i ---------\n", [
+                $this->println("--------- Job #$i ---------", [
                     'color' => 'light-blue',
                     'bold' => true
-                ]));
+                ]);
             }
-            fprintf(STDOUT, "Job Name %".(18 - strlen('Job Name'))."s %s\n",":",$job->getJobName());
-            fprintf(STDOUT, "Cron Expression %".(18 - strlen('Cron Expression'))."s %s\n",":",$job->getExpression());
+            $this->println("Job Name %".(18 - strlen('Job Name'))."s %s",[], ":",$job->getJobName());
+            $this->println("Cron Expression %".(18 - strlen('Cron Expression'))."s %s",[],":",$job->getExpression());
             $i++;
         }
 

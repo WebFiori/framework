7e2b61b1a1b00145e5533edeb3006902b56fa120
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-06-08 23:22:22 +0300

    Added support for redirecting www to non-www

--- src/entity/router/Router.php
+++ src/entity/router/Router.php
@@ -135,6 +135,7 @@ class Router {
         $this->onNotFound = function ()
         {
             http_response_code(404);
+
             if (!defined('API_CALL')) {
                 $notFoundView = new NotFoundView();
                 $notFoundView->display();
@@ -838,6 +839,10 @@ class Router {
 
         if (count($this->routes) != 0) {
             $routeUri = new RouterUri($uri, '');
+
+            if ($routeUri->hasWWW() && defined('NO_WWW') && NO_WWW === true) {
+                $this->redirectToNonWWW($routeUri);
+            }
             //first, search for the URI wuthout checking variables
             if ($this->_searchRoute($routeUri, $uri, $loadResource)) {
                 return;
@@ -950,5 +955,35 @@ class Router {
         self::$router = new Router();
 
         return self::$router;
+    }
+    /**
+     * Send http 301 response code and redirect the request to non-www URI.
+     * @param RouterUri $uriObj
+     */
+    private function redirectToNonWWW($uriObj) {
+        http_response_code(301);
+        $path = '';
+
+        $host = substr($uriObj->getHost(), strpos($uriObj->getHost(), '.'));
+
+        for ($x = 1 ; $x < count($uriObj->getPathArray()) ; $x++) {
+            $path .= '/'.$uriObj->getPathArray()[$x];
+        }
+        $queryString = '';
+
+        if (strlen($uriObj->getQueryString()) > 0) {
+            $queryString = '?'.$uriObj->getQueryString();
+        }
+        $fragment = '';
+
+        if (strlen($uriObj->getFragment()) > 0) {
+            $fragment = '#'.$uriObj->getFragment();
+        }
+        $port = '';
+
+        if (strlen($uriObj->getPort()) > 0) {
+            $port = ':'.$uriObj->getPort();
+        }
+        header('location: '.$uriObj->getScheme().'://'.$host.$port.$path.$queryString.$fragment);
     }
 }
---
+++ src/ini/GlobalConstants.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Ibrahim, WebFiori Framework.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace webfiori\ini;
+
+/**
+ * A class which is used to initialize global constants.
+ * This class has one static method which is used to define the constants.
+ * The class can be used to initialize any constant that the application depends 
+ * on. The constants that this class will initialize are the constants which 
+ * uses the function <code>define()</code>.
+ * Also, the developer can modify existing ones as needed to change some of the 
+ * default settings of the framework.
+ * @author Ibrahim
+ * @version 1.0
+ */
+class GlobalConstants {
+    /**
+     * Initialize the constants.
+     * Include your own in the body of this method or modify existing ones 
+     * to suite your configuration.
+     * @since 1.0
+     */
+    public static function defineConstants() {
+        /**
+         * Fallback for older php versions that does not support the constant 
+         * PHP_INT_MIN.
+         * @since 1.0
+         */
+        if (!defined('PHP_INT_MIN')) {
+            define('PHP_INT_MIN', ~PHP_INT_MAX);
+        }
+        /**
+         * This constant is used to tell the core if the application uses composer 
+         * packages or not. If set to true, then composer packages will be loaded.
+         * @since 1.0
+         */
+        define('LOAD_COMPOSER_PACKAGES', true);
+        /**
+         * A constant which is used to enable or disable HTTP access to cron.
+         * If the constant value is set to true, the framework will add routes to the 
+         * components which is used to allow access to cron control panel. The control 
+         * panel is used to execute jobs and check execution status. Default value is false.
+         * @since 1.0
+         */
+        define('CRON_THROUGH_HTTP', false);
+        /**
+         * This constant is used to tell the framework if more information should 
+         * be displayed if an exception is thrown or an error happens. The main aim 
+         * of this constant is to hide some sensitive information from users if the 
+         * system is in production environment. Note that the constant will have effect 
+         * only if the framework is accessed through HTTP protocol. If used in CLI 
+         * environment, everything will appear. Default value of the constant is 
+         * false.
+         * @since 1.0
+         */
+        define('VERBOSE', false);
+        /**
+         * This constant is used to redirect a URI with www to non-www.
+         * If this constant is defined and is set to true and a user tried to 
+         * access a resource using a URI that contains www in the host part,
+         * the router will send a 301 - permanent redirect HTTP response code and 
+         * send the user to non-www host. For example, if a request is sent to 
+         * 'https://www.example.com/my-page', it will be redirected to 
+         * 'https://example.com/my-page'. Default value of the constant is false which 
+         * means no redirection will be performed.
+         * @since 1.0
+         */
+        define('NO_WWW', false);
+    }
+}

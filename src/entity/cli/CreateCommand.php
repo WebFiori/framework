c1c93d57759576d61621f97e087713dce90810bd
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-06-10 09:04:50 +0300

    Update CreateCommand.php

--- src/entity/cli/CreateCommand.php
+++ src/entity/cli/CreateCommand.php
@@ -107,7 +107,44 @@ class CreateCommand extends CLICommand {
     }
     public function _createQueryClass() {
         $classInfo = $this->getClassInfo();
-        Util::print_r($classInfo);
+        $tempQuery = new MySQLQuery();
+        $addMoreCols = true;
+        $invalidTableName = true;
+        do {
+            $tableName = $this->getInput('Enter database table name:');
+            $invalidTableName = !$tempQuery->getTable()->setName($tableName);
+            if ($invalidTableName) {
+                $this->error('The given name is invalid.');
+            }
+        } while ($invalidTableName);
+        $addDefaultCols = $this->confirm('Would you like to include default columns? Default columns include "id", "created-on" and "last-updated".');
+        if ($addDefaultCols) {
+            $tempQuery->getTable()->addDefaultCols();
+        }
+        $this->println('Now we have to add columns to the table.');
+        do {
+            $colKey = $this->getInput('Enter a name for column key:');
+            $colDatatype = $this->select('Select column data type:', MySQLColumn::DATATYPES, 0);
+            $isAdded = $tempQuery->getTable()->addColumn($colKey, [
+                'datatype' => $colDatatype
+            ]);
+            if (!$isAdded) {
+                $this->warning('The column was not added. Mostly, key name is invalid. Try again.');
+            } else {
+                $dataSize = $this->getInput('Enter column size:');
+                $isPrimary = $this->confirm('Is this column primary?', false);
+                $isUnique = $this->confirm('Is this column unique?', false);
+                $isNull = $this->confirm('Can this column have null values?', false);
+                if ($this->confirm('Would you like to include default value for the column?', false)) {
+                    $defaultVal = $this->getInput('Enter default value:');
+                }
+                if ($this->confirm('Would you like to add your own comment about the column?', false)) {
+                    $defaultVal = $this->getInput('Enter your comment:');
+                }
+                $this->success('Column added.');
+            }
+            $addMoreCols = $this->confirm('Would you like to add another column?');
+        } while ($addMoreCols);
         return 0;
     }
     private function _getNamespace() {

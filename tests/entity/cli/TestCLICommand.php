b12aafaf6537d144ac452cb763170248b0958c7b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-06-20 17:42:10 +0300

    Update TestCLICommand.php

--- tests/entity/cli/TestCLICommand.php
+++ tests/entity/cli/TestCLICommand.php
@@ -12,4 +12,45 @@ class TestCLICommand extends TestCase{
         $this->assertEquals('<NO DESCRIPTION>', $command->getDescription());
         $this->assertEquals(0, count($command->getArgs()));
     }
+    /**
+     * @test
+     */
+    public function testAddArg00() {
+        $command = new TestCommand('new-command');
+        $this->assertFalse($command->addArg(''));
+        $this->assertFalse($command->addArg('       '));
+        $this->assertFalse($command->addArg('invalid name'));
+        $this->assertTrue($command->addArg('valid'));
+        $this->assertTrue($command->addArg('--valid-name'));
+        $this->assertTrue($command->addArg('0invalid'));
+        $this->assertTrue($command->addArg('valid-1'));
+    }
+    /**
+     * @test
+     */
+    public function testAddArg01() {
+        $command = new TestCommand('new-command');
+        $this->assertTrue($command->addArg('default-options'));
+        $argDetails = $command->getArgInfo('default-options');
+        $this->assertEquals([
+            'optional' => false,
+            'description' => '<NO DESCRIPTION>',
+            'values' => []
+        ], $argDetails);
+    }
+    /**
+     * @test
+     */
+    public function testAddArg02() {
+        $command = new TestCommand('new-command');
+        $this->assertTrue($command->addArg('default-options', [
+            'optional' => true
+        ]));
+        $argDetails = $command->getArgInfo('default-options');
+        $this->assertEquals([
+            'optional' => true,
+            'description' => '<NO DESCRIPTION>',
+            'values' => []
+        ], $argDetails);
+    }
 }

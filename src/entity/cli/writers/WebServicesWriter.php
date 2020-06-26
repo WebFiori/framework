d1707c8276f22dce1bad4f30aa2e15e14ec14815
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-06-27 02:42:39 +0300

    Update WebServicesWriter.php

--- src/entity/cli/writers/WebServicesWriter.php
+++ src/entity/cli/writers/WebServicesWriter.php
@@ -27,7 +27,7 @@ namespace webfiori\entity\cli;
 use InvalidArgumentException;
 use restEasy\APIAction;
 use restEasy\RequestParameter;
-use restEasy\WebServices;
+use restEasy\WebServicesSet;
 
 /**
  * A writer class which is used to create new web services class.
@@ -38,12 +38,12 @@ use restEasy\WebServices;
 class WebServicesWriter extends ClassWriter {
     /**
      *
-     * @var WebServices 
+     * @var WebServicesSet
      */
     private $servicesObj;
     /**
      * Creates new instance of the class.
-     * @param WebServices $webServicesObj The object that will be written to the 
+     * @param WebServicesSet $webServicesObj The object that will be written to the 
      * class.
      * @param array $classInfoArr An associative array that contains the information 
      * of the class that will be created. The array must have the following indices: 
@@ -59,8 +59,8 @@ class WebServicesWriter extends ClassWriter {
     public function __construct($webServicesObj, $classInfoArr) {
         parent::__construct($classInfoArr);
 
-        if (!$webServicesObj instanceof WebServices) {
-            throw new InvalidArgumentException('Given parameter is not an instance of \'WebServices\'');
+        if (!$webServicesObj instanceof WebServicesSet) {
+            throw new InvalidArgumentException('Given parameter is not an instance of \'WebServicesSet\'');
         }
         $this->servicesObj = $webServicesObj;
         $this->_writeHeaderSec();
@@ -123,7 +123,7 @@ class WebServicesWriter extends ClassWriter {
      * @param APIAction $service
      */
     private function _appendService($service, $requireAuth = false) {
-        $this->append('$this->addAction(APIAction::createService([', 2);
+        $this->append('$this->addService(WebService::createService([', 2);
         $this->append("'name' => '".$service->getName()."',", 3);
         $this->append("'request-methods' => [", 3);
 
@@ -162,7 +162,7 @@ class WebServicesWriter extends ClassWriter {
         $this->append(" * @return boolean If the client is authorized, the method will return true.", 1);
         $this->append(" */", 1);
         $this->append("public function isAuthorized() {", 1);
-        $this->append('$calledServiceName = $this->getAction();', 2);
+        $this->append('$calledServiceName = $this->getCalledServiceName();', 2);
 
         $authActionIndex = 0;
         $authCount = count($this->servicesObj->getAuthActions());
@@ -189,7 +189,7 @@ class WebServicesWriter extends ClassWriter {
         $this->append(" * Process the request.", 1);
         $this->append(" */", 1);
         $this->append("public function processRequest() {", 1);
-        $this->append('$calledServiceName = $this->getAction();', 2);
+        $this->append('$calledServiceName = $this->getCalledServiceName();', 2);
 
 
         $totalActions = count($this->servicesObj->getActions()) + $authCount;
@@ -232,7 +232,7 @@ class WebServicesWriter extends ClassWriter {
         $this->append("<?php\n");
         $this->append('namespace '.$this->getNamespace().";\n");
         $this->append("use webfiori\\entity\\ExtendedWebServices;");
-        $this->append("use restEasy\\APIAction;");
+        $this->append("use restEasy\\WebService;");
         $this->append('');
         $this->append("/**\n"
                 ." * A class that contains a set of web services.\n"
@@ -241,6 +241,20 @@ class WebServicesWriter extends ClassWriter {
                 );
 
         foreach ($this->servicesObj->getActions() as $service) {
+            if (count($service->getParameters()) != 0) {
+                $this->append(" * <li><b>".$service->getName()."</b>: This service has the following parameters:");
+                $this->append(' * <ul>');
+
+                foreach ($service->getParameters() as $param) {
+                    $this->append(' * <li><b>'.$param->getName().'</b>: Data type: '.$param->getType().'.');
+                }
+                $this->append(' * </ul>');
+                $this->append(' * </li>');
+            } else {
+                $this->append(" * <li><b>".$service->getName()."</b></li>");
+            }
+        }
+        foreach ($this->servicesObj->getAuthActions() as $service) {
             if (count($service->getParameters()) != 0) {
                 $this->append(" * <li><b>".$service->getName()."</b>: This service has the following parameters:");
                 $this->append(' * <ul>');

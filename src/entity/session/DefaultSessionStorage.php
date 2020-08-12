d6c81613eba11d3ff19337eeab28f62fafb66825
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-08-12 18:30:02 +0300

    Added PHPdocs

--- src/entity/session/DefaultSessionStorage.php
+++ src/entity/session/DefaultSessionStorage.php
@@ -11,8 +11,12 @@ namespace webfiori\entity\sesstion;
 use webfiori\entity\Util;
 use webfiori\entity\File;
 /**
- * Description of DefaultSesstionStorage
+ * The default sessions storage engine.
  *
+ * This storage engine will store session state as a file in the folder 
+ * 'app/storage/sessions'. The name of the file that contains session state 
+ * will be the ID of the session.
+ * 
  * @author Ibrahim
  * 
  * @since 1.1.0
@@ -21,14 +25,25 @@ use webfiori\entity\File;
  */
 class DefaultSessionStorage implements SessionStorage {
     private $storeLoc;
+    /**
+     * Creates new instance of the class.
+     * 
+     * @since 1.0
+     */
     public function __construct() {
         $this->storeLoc = ROOT_DIR.DS.'app'.DS.'storage'.DS.'sessions';
         Util::isDirectory($this->storeLoc, true);
     }
     /**
+     * Reads a session from session file.
      * 
-     * @param string $sessionId
-     * @return Session
+     * @param string $sessionId The ID of the session.
+     * 
+     * @return Session|null If the method successfully accessed session state, 
+     * the method will return an object of type 'Session'. Other than that, 
+     * the method will return null.
+     * 
+     * @since 1.0
      */
     public function read($sessionId) {
         $file = new File($sessionId, $this->storeLoc);
@@ -44,13 +59,22 @@ class DefaultSessionStorage implements SessionStorage {
             }
         }
     }
-
+    /**
+     * Removes session file.
+     * 
+     * @param string $sessionId The ID of the session.
+     * 
+     * @since 1.0
+     */
     public function remove($sessionId) {
         unlink($this->storeLoc.DS.$sessionId);
     }
     /**
+     * Stores session state to a file.
      * 
-     * @param Session $session
+     * @param Session $session The session that will be stored.
+     * 
+     * @since 1.0
      */
     public function save($session) {
         if ($session instanceof Session) {
@@ -58,6 +82,33 @@ class DefaultSessionStorage implements SessionStorage {
             $file = new File($session->getId(), $this->storeLoc);
             $file->setRawData($serializedSesstion);
             $file->write(false, true);
+        }
+    }
+    /**
+     * Removes all inactive sessions.
+     * 
+     * This method will check if the constant 'SESSION_GC' is exist and its value 
+     * is valid. If exist and valid, it will be used as reference for removing 
+     * old sessions. If it does not exist, the method will remove any inactive 
+     * session which is older than 30 days.
+     * 
+     * @since 1.0
+     */
+    public function gc() {
+        $sessionsFiles = array_diff(scandir($this->storeLoc), ['.','..']);
+        
+        if (defined('SESSION_GC') && SESSION_GC > 0) {
+            $olderThan = SESSION_GC;
+        } else {
+            //Clear any sesstion which is older than 30 days
+            $olderThan = time() - 60*60*24*30;
+        }
+        
+        foreach ($sessionsFiles as $file) {
+            $fileObj = new File($this->storeLoc.DS.$file);
+            if ($fileObj->getLastModified() < $olderThan) {
+                $fileObj->remove();
+            }
         }
     }
 
--- src/entity/session/SessionStorage.php
+++ src/entity/session/SessionStorage.php
@@ -3,7 +3,7 @@ namespace webfiori\entity\sesstion;
 
 use webfiori\entity\sesstion\Session;
 /**
- * Description of SesstionStore
+ * An interface which can be used to implement different types of sessions storage.
  *
  * @author Ibrahim
  * 
@@ -13,26 +13,45 @@ use webfiori\entity\sesstion\Session;
  */
 interface SessionStorage {
     /**
+     * Store session state.
      * 
-     * @param Session $sesstion
+     * This method must store serialized session. The developer can use the method 
+     * <code>Session::serialize()</code> to serialize any session.
+     * 
+     * @param Session $sesstion The session that will be stored.
      * 
      * @since 1.0
      */
     public function save($sesstion);
     /**
+     * Reads session state.
      * 
-     * @param type $sesstionId
+     * This method must be implemented in a way that it returns an object of type 
+     * Session when session state is loaded. Note that the stored session 
+     * will be serialized. To unserialize the session, the developer can use 
+     * the method <code>Sestion::unserialize()</code>
      * 
-     * @return Sesstion Description
+     * @param string $sesstionId The unique identifier of the session.
+     * 
+     * @return Session|null If the session state was restored, the method must 
+     * return an object of type <code>Session</code>. Other than that, the method 
+     * should return null.
      * 
      * @since 1.0
      */
     public function read($sesstionId);
     /**
+     * Kill a session and remove its state from the storage.
      * 
-     * @param type $sesstionId
+     * @param string $sesstionId The unique identifier of the session.
      * 
      * @since 1.0
      */
-    public function remove($sesstionId); 
+    public function remove($sesstionId);
+    /**
+     * Removes all inactive sessions from the storage.
+     * 
+     * @since 1.0
+     */
+    public function gc();
 }

8db0f821cffe73485bf77edc4f886ddbf33fe9e9
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-12-11 21:11:25 +0300

    Update Config.php

--- src/conf/Config.php
+++ src/conf/Config.php
@@ -56,9 +56,9 @@ class Config {
      * Initialize configuration.
      */
     private function __construct() {
-        $this->releaseDate = '2020-12-08';
+        $this->releaseDate = '2020-12-11';
         $this->version = '2.0.0';
-        $this->versionType = 'Beta 3';
+        $this->versionType = 'Beta 4';
         $this->configVision = '1.3.5';
         $this->dbConnections = [
 

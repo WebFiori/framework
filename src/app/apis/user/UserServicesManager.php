6e32520e454c09086eb0c1c18c6ded3048e4e2e4
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-04-29 01:27:41 +0300

    Create UserServicesManager.php

---
+++ src/app/apis/user/UserServicesManager.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\apis;
+
+use webfiori\framework\ExtendedWebServicesManager;
+/**
+ * A services manager which is used to manage user related APIs.
+ *
+ * @author Ibrahim
+ */
+class UserServicesManager extends ExtendedWebServicesManager {
+    public function __construct() {
+        parent::__construct();
+        
+        $this->addService(new AddUserService());
+    }
+}

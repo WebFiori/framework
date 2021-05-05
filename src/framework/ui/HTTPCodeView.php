3c9d48e7ecf44c136fc40daef104920be90843ad
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-05-06 00:28:55 +0300

    Update HTTPCodeView.php
    Renamed class.

--- src/framework/ui/HTTPCodeView.php
+++ src/framework/ui/HTTPCodeView.php
@@ -27,29 +27,28 @@ namespace webfiori\framework\ui;
 use webfiori\framework\WebFioriApp;
 use webfiori\ui\HTMLNode;
 /**
- * A basic view which is used to display 404 HTTP error code and 
- * messages.
+ * A basic view which is used to display HTTP error codes taken from 
+ * language file.
  *
  * @author Ibrahim
  */
-class NotFoundView extends WebPage {
+class HTTPCodeView extends WebPage {
     /**
      * Creates new instance of the class.
      */
-    public function __construct() {
+    public function __construct($errCode) {
         parent::__construct();
         $this->setTheme(WebFioriApp::getAppConfig()->getBaseThemeName());
 
-        $labels = $this->get('general/http-codes/404');
-        $this->setTitle($labels['code'].' - '.$labels['type']);
-        http_response_code($labels['code']);
+        $this->setTitle($this->get("general/http-codes/$errCode/code").' - '.$this->get("general/http-codes/$errCodetype/type"));
+        http_response_code(intval($this->get("general/http-codes/$errCode/code")));
         $h1 = new HTMLNode('h1');
         $h1->text($this->getTitle());
         $this->insert($h1);
         $hr = new HTMLNode('hr');
         $this->insert($hr);
         $paragraph = new HTMLNode('p');
-        $paragraph->text($labels['message']);
+        $paragraph->text($this->get("general/http-codes/$errCode/message"));
         $this->insert($paragraph);
     }
 }

0ce1bcb10245020a7dec9074f2e8a8ddfbdcac19
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-07-18 20:49:33 +0300

    Update RunSQLQueryCommand.php

--- webfiori/framework/cli/RunSQLQueryCommand.php
+++ webfiori/framework/cli/RunSQLQueryCommand.php
@@ -29,6 +29,9 @@ namespace webfiori\framework\cli;
 use webfiori\framework\cli\CLICommand;
 use webfiori\framework\DB;
 use webfiori\framework\WebFioriApp;
+use webfiori\framework\DB;
+use webfiori\database\DatabaseException;
+use webfiori\database\Table;
 
 /**
  * A command which can be used to execute SQL queries on 
@@ -54,21 +57,82 @@ class RunSQLQueryCommand extends CLICommand {
         ], 'Execute SQL query on specific database.');
     }
     public function exec() {
-        $connName = $this->getArgValue('connection');
         $dbConnections = array_keys(WebFioriApp::getAppConfig()->getDBConnections());
-        
+        $schema = $this->getArgValue('schema');
         if (count($dbConnections) != 0) {
-            if ($connName === null) {
-                $connName = $connName = $this->select('Select database connection:', $connsArr, 0);
-            } else if (!in_array($connName, $dbConnections)){
-                $this->error('No connection with name "'.$connName.'" was found!');
-                $connName = $connName = $this->select('Select database connection:', $connsArr, 0);
+            if ($schema !== null && class_exists($schema)) {
+                $schemaInst = new $schema();
+                if ($schemaInst instanceof DB) {
+                    $this->queryOnSchema($schemaInst);
+                } else {
+                    $this->error('Given class is not an instance of "webfiori\\framework\\DB"!');
+                    return -1;
+                }
+            } else {
+                $connName = $this->getArgValue('connection');
+            
+
+            
+                if ($connName === null) {
+                    $connName = $this->select('Select database connection:', $dbConnections, 0);
+                    $schema = new DB($connName);
+                    $this->generalQuery($schema);
+                } else if (!in_array($connName, $dbConnections)){
+                    $this->error('No connection with name "'.$connName.'" was found!');
+                    return -1;
+                }
             }
         } else {
             $this->error('No database connections available. Add connections inside the class \'AppConfig\' or use the command "add".');
         }
     }
-    
+    private function generalQuery(DB $schema) {
+        $options = [
+            'Run general query.',
+            'Run query on table instance.'
+        ];
+        $selected = $this->select('What type of query you would like to run?', $options);
+        
+        if ($selected == 'Run general query.') {
+            $query = $this->getInput('Please provide us with the query:');
+            $this->println('Executing the query...');
+            $schema->setQuery($query);
+            try {
+                $schema->execute();
+            } catch (DatabaseException $ex) {
+                $this->error('The query finished execution with an error: '.$ex->getCode().' - '.$ex->getMessage());
+                return -1;
+            }
+            $this->success('Query executed without errors.');
+            return 0;
+        } else if ($selected == 'Run query on table instance.') {
+            $tableClassName = '';
+            $tableClassNameValidity = false;
+            do {
+                if (strlen($tableClassName) == 0) {
+                    $tableClassName = $this->getInput('Enter database table class name (include namespace):');
+                }
+
+                if (!class_exists($tableClassName)) {
+                    $this->error('Class not found.');
+                    $tableClassName = '';
+                    continue;
+                }
+                $tableObj = new $tableClassName();
+
+                if (!$tableObj instanceof Table) {
+                    $this->error('The given class is not a child of the class "webfiori\database\Table".');
+                    $tableClassName = '';
+                    continue;
+                }
+                $tableClassNameValidity = true;
+            } while (!$tableClassNameValidity);
+            
+        }
+    }
+    private function queryOnSchema(DB $schema) {
+        
+    }
     private function getConnName($connsArr) {
         
     }

3090295f088aca2fe6c1b8046c9d9646601632ed
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-08-05 17:19:42 +0300

    Update DatabaseSessionStorage.php

--- webfiori/framework/session/DatabaseSessionStorage.php
+++ webfiori/framework/session/DatabaseSessionStorage.php
@@ -1,7 +1,7 @@
 <?php
 namespace webfiori\framework\session;
 
-use app\database\SessionOperations;
+use webfiori\framework\session\SessionOperations;
 /**
  * A session storage engine which uses database to store session state.
  *

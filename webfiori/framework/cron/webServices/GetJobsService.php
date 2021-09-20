0ee7c1d94fe3b4b17671ea73d6ebab83aef29c25
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-09-20 21:56:00 +0300

    Create GetJobsService.php

---
+++ webfiori/framework/cron/webServices/GetJobsService.php
@@ -0,0 +1,32 @@
+<?php
+namespace webfiori\framework\cron\webServices;
+
+use webfiori\http\AbstractWebService;
+use webfiori\json\Json;
+use webfiori\framework\cron\Cron;
+/**
+ * A web service which is used to fetch a list of all scheduled jobs.
+ *
+ * @author Ibrahim
+ * 
+ * @version 1.0
+ */
+class GetJobsService extends AbstractWebService {
+    public function __construct() {
+        parent::__construct('get-jobs');
+        $this->addRequestMethod('get');
+    }
+    public function isAuthorized() {
+        SessionsManager::start('cron-session');
+
+        return SessionsManager::get('cron-login-status') === true;
+    }
+    
+    public function processRequest() {
+        $json = new Json([
+            'jobs' => Cron::jobsQueue()->toArray()
+        ]);
+        $this->send('application/json', $json);
+    }
+
+}

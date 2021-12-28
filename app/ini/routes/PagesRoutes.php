2f8ee4c31e0006aacab730aa5edea41408c618f0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-12-29 00:24:07 +0300

    New Names

--- app/ini/routes/PagesRoutes.php
+++ app/ini/routes/PagesRoutes.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace app\ini\routes;
 
-class APIRoutes {
+use webfiori\framework\router\Router;
+
+class PagesRoutes {
     /**
      * Initialize system routes.
      * 
      * @since 1.0
      */
     public static function create() {
+        //TODO: Add your own routes here.
     }
 }

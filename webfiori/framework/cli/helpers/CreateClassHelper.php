52d83df748add8243f7b82986e5010c9ad22cfe2
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-22 17:45:52 +0300

    Create CreateClassHelper.php

---
+++ webfiori/framework/cli/helpers/CreateClassHelper.php
@@ -0,0 +1,104 @@
+<?php
+namespace webfiori\framework\cli\helpers;
+
+use webfiori\framework\cli\commands\CreateCommand;
+use webfiori\framework\cli\writers\ClassWriter;
+/**
+ * A wrapper class which helps in creating classes using CLI.
+ *
+ * @author Ibrahim
+ */
+class CreateClassHelper {
+    /**
+     * 
+     * @var ClassWriter
+     */
+    private $classWriter;
+    /**
+     * 
+     * @var CreateCommand
+     */
+    private $command;
+    public function __construct(CreateCommand $command) {
+        $this->command = $command;
+        $this->classWriter = new ClassWriter([]);
+    }
+    /**
+     * 
+     * @return ClassWriter
+     */
+    public function getWriter() {
+        return $this->classWriter;
+    }
+    public function setPath($path) {
+        $this->getWriter()->setPath($path);
+    }
+    public function setNamespace($ns) {
+        $this->getWriter()->setNamespace($ns);
+    }
+    public function setClassName($name) {
+        $this->getWriter()->setClassName($name);
+    }
+    public function setClassInfo($ns, $suffix) {
+        $classInfo = $this->getClassInfo($ns, $suffix);
+        $this->setNamespace($classInfo['namespace']);
+        $this->setClassName($classInfo['name']);
+        $this->setPath($classInfo['path']);
+    }
+    public function appendTop() {
+        $this->append([
+            '<?php',
+            "namespace ".$this->getWriter()->getNamespace().";\n"
+        ], 0);
+    }
+    public function append($strArr, $tapsCount = 0) {
+        if (gettype($strArr) == 'array') {
+            foreach ($strArr as $str) {
+                $this->getWriter()->append($str, $tapsCount);
+            }
+        } else {
+            $this->getWriter()->append($strArr, $tapsCount);
+        }
+    }
+    public function writeClass() {
+        $this->getWriter()->writeClass();
+        $this->info('New CLI class was created at "'.$this->getWriter()->getPath().'".');
+    }
+    /**
+     * 
+     * @return CreateCommand
+     */
+    public function getCommand() {
+        return $this->command;
+    }
+    public function error($message) {
+        $this->getCommand()->error($message);
+    }
+    public function prints($str, ...$_) {
+        $this->getCommand()->prints($str, $_);
+    }
+    public function warning($message) {
+        $this->getCommand()->warning($message);
+    }
+    public function info($message) {
+        $this->getCommand()->info($message);
+    }
+    public function success($message) {
+        $this->getCommand()->success($message);
+    }
+    public function confirm($confirmTxt, $default = null) {
+        $this->getCommand()->confirm($confirmTxt, $default);
+    }
+    public function getInput($prompt, $default = null, $validator = null) {
+        return $this->getCommand()->getInput($prompt, $default, $validator);
+    }
+    public function println($str = '', ...$_) {
+        $this->getCommand()->println($str, $_);
+    }
+    public function select($prompt, $choices, $defaultIndex = null) {
+        $this->getCommand()->select($prompt, $choices, $defaultIndex);
+    }
+    public function getClassInfo($defaltNs) {
+        return $this->getCommand()->getClassInfo($defaltNs);
+    }
+}

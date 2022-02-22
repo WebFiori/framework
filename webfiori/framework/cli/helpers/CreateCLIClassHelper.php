fa990d96ab5dde16599d533c516410e306683324
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-23 01:18:41 +0300

    Update CreateCLIClassHelper.php

--- webfiori/framework/cli/helpers/CreateCLIClassHelper.php
+++ webfiori/framework/cli/helpers/CreateCLIClassHelper.php
@@ -4,71 +4,68 @@ namespace webfiori\framework\cli\helpers;
 use webfiori\framework\cli\commands\CreateCommand;
 use webfiori\framework\cli\writers\ClassWriter;
 /**
- * A helper class which is used to help in creating cron jobs classes using CLI.
+ * A helper class which is used to help in creating CLI command classes using CLI.
  *
  * @author Ibrahim
  * 
  * @version 1.0
  */
-class CreateCLIClassHelper {
-    /**
-     *
-     * @var CLICommand 
-     */
-    private $command;
+class CreateCLIClassHelper extends CreateClassHelper {
     /**
      * Creates new instance of the class.
      * 
      * @param CreateCommand $command A command that is used to call the class.
      */
     public function __construct(CreateCommand $command) {
-        $this->command = $command;
-        $classInfo = $command->getClassInfo(APP_DIR_NAME.'\\commands');
+        parent::__construct($command);
+        $this->setClassInfo(APP_DIR_NAME.'\\commands', 'Command');
         $commandName = $this->_getCommandName();
-        $commandDesc = $this->_getCommand()->getInput('Give a short description of the command:');
+        $commandDesc = $this->getInput('Give a short description of the command:');
 
         if ($command->confirm('Would you like to add arguments to the command?', false)) {
             $argsArr = $this->_getArgs();
         } else {
             $argsArr = [];
         }
-        $writer = new ClassWriter($classInfo);
-
-        $writer->append('<?php');
-        $writer->append("namespace ".$writer->getNamespace().";\n");
-        $writer->append("use webfiori\\framework\\cli\\CLICommand;");
-        $writer->append('/**');
-        $writer->append(' * A CLI command  which was created using the command "create".');
-        $writer->append(' *');
-        $writer->append(" * The command will have the name '$commandName'.");
+        $this->appendTop();
+        $topArr = [
+            "use webfiori\\framework\\cli\\CLICommand;",
+            '/**',
+            ' * A CLI command  which was created using the command "create".',
+            ' *',
+            " * The command will have the name '$commandName'."
+        ];
 
         if (count($argsArr) != 0) {
-            $writer->append(' * In addition, the command have the following args:');
-            $writer->append(' * <ul>');
+            $topArr[] = ' * In addition, the command have the following args:';
+            $topArr[] = ' * <ul>';
 
             foreach ($argsArr as $argArr) {
-                $writer->append(" * <li>".$argArr['name']."</li>");
+                $topArr[] = " * <li>".$argArr['name']."</li>";
             }
-            $writer->append(' * </ul>');
+            $topArr[] = ' * </ul>';
         }
-        $writer->append(' */');
-        $writer->append('class '.$writer->getName().' extends CLICommand {');
+        $topArr[] = ' */';
+        $topArr[] = 'class '.$this->getWriter()->getName().' extends CLICommand {';
+        $this->append($topArr, 0);
+        $this->_writeConstructor($commandName, $argsArr, $commandDesc);
+        
+        $this->append([
+            '/**',
+            ' * Execute the command.',
+            ' */',
+            'public function exec() {',
+        ], 1);
+        $this->append([
+            '//TODO: Write the code that represents the command.',
+            'return 0;',
+        ], 2);
+        $this->append('}', 1);
 
-        $this->_writeConstructor($writer, $commandName, $argsArr, $commandDesc);
+        $this->append("}");
+        $this->append("return __NAMESPACE__;");
 
-        $writer->append('/**', 1);
-        $writer->append(' * Execute the command.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function exec() {', 1);
-        $writer->append('//TODO: Write the code that represents the command.', 2);
-        $writer->append('return 0;', 2);
-        $writer->append('}', 1);
-
-        $writer->append("}");
-        $writer->append("return __NAMESPACE__;");
-
-        $writer->writeClass();
-        $command->info('New CLI class was created at "'.$writer->getPath().'".');
+        $this->writeClass();
     }
     private function _getArgs() {
         $argsArr = [];
@@ -76,31 +73,24 @@ class CreateCLIClassHelper {
 
         while ($addToMore) {
             $argArr = [];
-            $groupName = $this->_getCommand()->getInput('Enter argument name:');
+            $groupName = $this->getInput('Enter argument name:');
 
             if (strlen($groupName) > 0) {
                 $argArr['name'] = $groupName;
             }
-            $argArr['description'] = $this->_getCommand()->getInput('Describe this argument and how to use it:', '');
+            $argArr['description'] = $this->getInput('Describe this argument and how to use it:', '');
             $argArr['values'] = $this->_getFixedVals();
-            $argArr['optional'] = $this->_getCommand()->confirm('Is this argument optional or not?', true);
-            $argArr['default'] = $this->_getCommand()->getInput('Enter default value:');
+            $argArr['optional'] = $this->confirm('Is this argument optional or not?', true);
+            $argArr['default'] = $this->getInput('Enter default value:');
 
             $argsArr[] = $argArr;
-            $addToMore = $this->_getCommand()->confirm('Would you like to add more arguments?', false);
+            $addToMore = $this->confirm('Would you like to add more arguments?', false);
         }
 
         return $argsArr;
     }
-    /**
-     * 
-     * @return CreateCommand
-     */
-    private function _getCommand() {
-        return $this->command;
-    }
     private function _getCommandName() {
-        return $this->_getCommand()->getInput('Enter a name for the command:', null, function ($val)
+        return $this->getInput('Enter a name for the command:', null, function ($val)
         {
             if (strlen($val) > 0 && strpos($val, ' ') === false) {
                 return true;
@@ -110,19 +100,19 @@ class CreateCLIClassHelper {
         });
     }
     private function _getFixedVals() {
-        if (!$this->_getCommand()->confirm('Does this argument have a fixed set of values?', false)) {
+        if (!$this->confirm('Does this argument have a fixed set of values?', false)) {
             return [];
         }
         $addVals = true;
         $valsArr = [];
 
         while ($addVals) {
-            $val = $this->_getCommand()->getInput('Enter the value:');
+            $val = $this->getInput('Enter the value:');
 
             if (!in_array($val, $valsArr)) {
                 $valsArr[] = $val;
             }
-            $addVals = $this->_getCommand()->confirm('Would you like to add more values?', false);
+            $addVals = $this->confirm('Would you like to add more values?', false);
         }
 
         return $valsArr;
@@ -136,39 +126,40 @@ class CreateCLIClassHelper {
      * @param array $args
      * @param string $commandDesc Description
      */
-    private function _writeConstructor($writer, $name,array $args, $commandDesc) {
-        $writer->append('/**', 1);
-        $writer->append(' * Creates new instance of the class.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function __construct(){', 1);
-
+    private function _writeConstructor($name, array $args, $commandDesc) {
+        $this->append([
+            '/**',
+            ' * Creates new instance of the class.',
+            ' */',
+            'public function __construct(){'
+        ], 1);
 
         if (count($args) > 0) {
-            $writer->append("parent::__construct('$name', [", 2);
+            $this->append(["parent::__construct('$name', ["], 2);
 
             foreach ($args as $argArr) {
-                $writer->append("'".$argArr['name']."' => [", 3);
+                $this->append("'".$argArr['name']."' => [", 3);
 
                 if (strlen($argArr['description']) != 0) {
-                    $writer->append("'description' => '".str_replace("'", "\'", $argArr['description'])."',", 4);
+                    $this->append("'description' => '".str_replace("'", "\'", $argArr['description'])."',", 4);
                 }
-                $writer->append("'optional' => ".($argArr['optional'] === true ? 'true' : 'false').",", 4);
+                $this->append("'optional' => ".($argArr['optional'] === true ? 'true' : 'false').",", 4);
 
                 if (count($argArr['values']) != 0) {
                     $writer->append("'values' => [", 4);
 
                     foreach ($argArr['values'] as $val) {
-                        $writer->append("'".str_replace("'", "\'", $val)."',", 5);
+                        $this->append("'".str_replace("'", "\'", $val)."',", 5);
                     }
-                    $writer->append("]", 4);
+                    $this->append("]", 4);
                 }
-                $writer->append("],", 3);
+                $this->append("],", 3);
             }
-            $writer->append("], '".str_replace("'", "\'", $commandDesc)."');", 2);
+            $this->append("], '".str_replace("'", "\'", $commandDesc)."');", 2);
         } else {
-            $writer->append("parent::__construct('$name', '".str_replace("'", "\'", $commandDesc)."');", 2);
+            $this->append("parent::__construct('$name', '".str_replace("'", "\'", $commandDesc)."');", 2);
         }
 
-        $writer->append('}', 1);
+        $this->append('}', 1);
     }
 }

fd520ac446350828e9869c3a63e6b7bd4ad07cd0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-23 01:51:02 +0300

    Update CreateCronJob.php

--- webfiori/framework/cli/helpers/CreateCronJob.php
+++ webfiori/framework/cli/helpers/CreateCronJob.php
@@ -2,7 +2,6 @@
 namespace webfiori\framework\cli\helpers;
 
 use webfiori\framework\cli\commands\CreateCommand;
-use webfiori\framework\cli\writers\ClassWriter;
 /**
  * A helper class which is used to help in creating cron jobs classes using CLI.
  *
@@ -10,100 +9,109 @@ use webfiori\framework\cli\writers\ClassWriter;
  * 
  * @version 1.0
  */
-class CreateCronJob {
-    /**
-     *
-     * @var CLICommand 
-     */
-    private $command;
+class CreateCronJob extends CreateClassHelper {
     /**
      * Creates new instance of the class.
      * 
      * @param CreateCommand $command A command that is used to call the class.
      */
     public function __construct(CreateCommand $command) {
-        $this->command = $command;
-        $classInfo = $command->getClassInfo(APP_DIR_NAME.'\\jobs');
+        parent::__construct($command);
+        
+        $this->setClassInfo(APP_DIR_NAME.'\\jobs', 'Job');
         $jobName = $this->_getJobName();
         $jobDesc = $this->_getJobDesc();
         
-        if ($command->confirm('Would you like to add arguments to the job?', false)) {
+        if ($this->confirm('Would you like to add arguments to the job?', false)) {
             $argsArr = $this->_getArgs();
         } else {
             $argsArr = [];
         }
-        $writer = new ClassWriter($classInfo);
-
-        $writer->append('<?php');
-        $writer->append("namespace ".$writer->getNamespace().";\n");
-        $writer->append("use webfiori\\framework\\cron\\AbstractJob;");
-        $writer->append("use webfiori\\framework\\cron\\CronEmail;");
-        $writer->append("use webfiori\\framework\\cron\\Cron;\n");
-        $writer->append('/**');
-        $writer->append(' * A background process which was created using the command "create".');
-        $writer->append(' *');
-        $writer->append(" * The process will have the name '$jobName'.");
+        
+        $this->appendTop();
+        
+        $this->append([
+            "use webfiori\\framework\\cron\\AbstractJob;",
+            "use webfiori\\framework\\cron\\CronEmail;",
+            "use webfiori\\framework\\cron\\Cron;\n",
+            '/**',
+            ' * A background process which was created using the command "create".',
+            ' *',
+            " * The process will have the name '$jobName'."
+        ]);
+        
 
         if (count($argsArr) != 0) {
-            $writer->append(' * In addition, the proces have the following args:');
-            $writer->append(' * <ul>');
+            $argsArr = [
+                ' * In addition, the proces have the following args:',
+                ' * <ul>'
+            ];
+            
 
             foreach ($argsArr as $argName => $options) {
-                $writer->append(" * <li>$argName: ".$options['description']."</li>");
+                $argsArr[] = " * <li>$argName: ".$options['description']."</li>";
             }
-            $writer->append(' * </ul>');
+            $argsArr[] = ' * </ul>';
         }
-        $writer->append(' */');
-        $writer->append('class '.$writer->getName().' extends AbstractJob {');
+        $argsArr[] = ' */';
+        $argsArr[] = 'class '.$this->getWriter()->getName().' extends AbstractJob {';
+        $this->append($argsArr);
 
-        $this->_writeConstructor($writer, $jobName, $argsArr, $jobDesc);
+        $this->_writeConstructor($jobName, $argsArr, $jobDesc);
+        
+        $this->append([
+            '/**',
+            ' * Execute the process.',
+            ' */',
+            'public function execute() {'
+        ], 1);
+        
+        $this->append('//TODO: Write the code that represents the process.', 2);
+        $this->append([
+            '}',
+            '/**',
+            ' * Execute a set of instructions when the job faild to complete without errors.',
+            ' */',
+            'public function onFail() {'
+        ], 1);
+        $this->append('//TODO: Implement the action to perform when the job fails to complete without errors.', 2);
+        $this->append([
+            '}',
+            '/**',
+            ' * Execute a set of instructions when the job completed without errors.',
+            ' */',
+            'public function onSuccess() {',
+        ], 1);
 
-        $writer->append('/**', 1);
-        $writer->append(' * Execute the process.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function execute() {', 1);
-        $writer->append('//TODO: Write the code that represents the process.', 2);
-        $writer->append('}', 1);
+        $this->append('//TODO: Implement the action to perform when the job executes without errors.', 2);
+        $this->append([
+            '}',
+            '/**',
+            ' * Execute a set of instructions after the job has finished to execute.',
+            ' */',
+            'public function afterExec() {',
+        ], 1);
 
-        $writer->append('/**', 1);
-        $writer->append(' * Execute a set of instructions when the job faild to complete without errors.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function onFail() {', 1);
-        $writer->append('//TODO: Implement the action to perform when the job fails to complete without errors.', 2);
-        $writer->append('}', 1);
+        $this->append('//TODO: Implement the action to perform when the job finishes to execute.', 2);
+        $this->append("//\$email = new CronEmail('no-reply', [", 2);
+        $this->append("//    'webfiori@example.com' => 'Ibrahim Ali'", 2);
+        $this->append('//]);', 2);
+        $this->append('}', 1);
 
-        $writer->append('/**', 1);
-        $writer->append(' * Execute a set of instructions when the job completed without errors.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function onSuccess() {', 1);
-        $writer->append('//TODO: Implement the action to perform when the job executes without errors.', 2);
-        $writer->append('}', 1);
+        $this->append("}");
 
-        $writer->append('/**', 1);
-        $writer->append(' * Execute a set of instructions after the job has finished to execute.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function afterExec() {', 1);
-        $writer->append('//TODO: Implement the action to perform when the job finishes to execute.', 2);
-        $writer->append("//\$email = new CronEmail('no-reply', [", 2);
-        $writer->append("//    'webfiori@example.com' => 'Ibrahim Ali'", 2);
-        $writer->append('//]);', 2);
-        $writer->append('}', 1);
-
-        $writer->append("}");
-
-        $writer->writeClass();
-        $command->info('New background job class was created at "'.$writer->getPath().'".');
+        $this->writeClass();
     }
     private function _getArgs() {
         $argsArr = [];
         $addToMore = true;
 
         while ($addToMore) {
-            $argName = $this->_getCommand()->getInput('Enter argument name:');
+            $argName = $this->getInput('Enter argument name:');
 
             if (strlen($argName) > 0) {
                 $argsArr[$argName] = [
-                    'description' => $this->_getCommand()->getInput('Enter argument name:', 'No Description.', function ($val) {
+                    'description' => $this->getInput('Enter argument name:', 'No Description.', function ($val) {
                         if (strlen($val) != 0) {
                             return $val;
                         }
@@ -113,20 +121,13 @@ class CreateCronJob {
                 
                 
             }
-            $addToMore = $this->_getCommand()->confirm('Would you like to add more arguments?', false);
+            $addToMore = $this->confirm('Would you like to add more arguments?', false);
         }
 
         return $argsArr;
     }
-    /**
-     * 
-     * @return CreateCommand
-     */
-    private function _getCommand() {
-        return $this->command;
-    }
     private function _getJobDesc() {
-        return $this->_getCommand()->getInput('Provide short description of what does the job will do:', null, function ($val)
+        return $this->getInput('Provide short description of what does the job will do:', null, function ($val)
         {
             if (strlen($val) > 0) {
                 return true;
@@ -136,7 +137,7 @@ class CreateCronJob {
         });
     }
     private function _getJobName() {
-        return $this->_getCommand()->getInput('Enter a name for the job:', null, function ($val)
+        return $this->getInput('Enter a name for the job:', null, function ($val)
         {
             if (strlen($val) > 0) {
                 return true;
@@ -145,41 +146,40 @@ class CreateCronJob {
             return false;
         });
     }
-    /**
-     * 
-     * @param ClassWriter $writer
-     * @param type $name
-     * @param type $priority
-     * @param array $args
-     */
-    private function _writeConstructor($writer, $name, array $args, $jobDesc) {
-        $writer->append('/**', 1);
-        $writer->append(' * Creates new instance of the class.', 1);
-        $writer->append(' */', 1);
-        $writer->append('public function __construct(){', 1);
-        $writer->append("parent::__construct('$name');", 2);
-        $writer->append("\$this->setDescription('". str_replace('\'', '\\\'', $jobDesc)."');", 2);
+
+    private function _writeConstructor($name, array $args, $jobDesc) {
+        $this->append([
+            '/**',
+            ' * Creates new instance of the class.',
+            ' */',
+            'public function __construct(){'
+        ], 1);
+        $this->append([
+            "parent::__construct('$name');",
+            "\$this->setDescription('". str_replace('\'', '\\\'', $jobDesc)."');"
+        ], 2);
+        
         
         if (count($args) > 0) {
-            $writer->append('$this->addExecutionArgs([', 2);
+            $this->append('$this->addExecutionArgs([', 2);
 
             foreach ($args as $argName => $argOptions) {
-                $writer->append("'$argName' => [", 3);
-                $writer->append("'description' => '".str_replace('\'', '\\\'', $argOptions['description'])."'", 4);
-                $writer->append("],", 3);
+                $this->append("'$argName' => [", 3);
+                $this->append("'description' => '".str_replace('\'', '\\\'', $argOptions['description'])."'", 4);
+                $this->append("],", 3);
             }
-            $writer->append(']);', 2);
+            $this->append(']);', 2);
         }
-        $writer->append('// TODO: Specify the time at which the process will run at.', 2);
-        $writer->append('// You can use one of the following methods to specifiy the time:', 2);
-
-        $writer->append('//$this->dailyAt(4, 30)', 2);
-        $writer->append('//$this->everyHour();', 2);
-        $writer->append('//$this->everyMonthOn(1, \'00:00\');', 2);
-        $writer->append("//\$this->onMonth('jan', 15, '13:00');", 2);
-        $writer->append("//\$this->weeklyOn('sun', '23:00');", 2);
-        $writer->append("//\$this->cron('* * * * *');", 2);
-
-        $writer->append('}', 1);
+        $this->append([
+            '// TODO: Specify the time at which the process will run at.',
+            '// You can use one of the following methods to specifiy the time:',
+            '//$this->dailyAt(4, 30)',
+            '//$this->everyHour();',
+            '//$this->everyMonthOn(1, \'00:00\');',
+            "//\$this->onMonth('jan', 15, '13:00');",
+            "//\$this->weeklyOn('sun', '23:00');",
+            "//\$this->cron('* * * * *');"
+        ], 2);
+        $this->append('}', 1);
     }
 }

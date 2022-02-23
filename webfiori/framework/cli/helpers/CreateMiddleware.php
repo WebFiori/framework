042907dd8987a659856a418a48a417b435bb6454
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-24 01:31:56 +0300

    Update CreateMiddleware.php

--- webfiori/framework/cli/helpers/CreateMiddleware.php
+++ webfiori/framework/cli/helpers/CreateMiddleware.php
@@ -3,6 +3,7 @@ namespace webfiori\framework\cli\helpers;
 
 use webfiori\framework\cli\commands\CreateCommand;
 use webfiori\framework\cli\helpers\CreateClassHelper;
+use webfiori\framework\cli\writers\MiddlewareClassWriter;
 /**
  * A helper class that works with the create command to create a middleware.
  *
@@ -28,62 +29,8 @@ class CreateMiddleware extends CreateClassHelper {
         } else {
             $groupsArr = [];
         }
-        $this->appendTop();
-        $classTop = [
-            "use webfiori\\framework\\middleware\\AbstractMiddleware;",
-            "use webfiori\\framework\\SessionsManager;",
-            "use webfiori\\http\\Request;",
-            "use webfiori\\http\\Response;\n",
-            '/**',
-            ' * A middleware which is created using the command "create".',
-            ' *',
-            " * The middleware will have the name '$middlewareName' and ",
-            " * Priority $priority."
-        ];
-
-        if (count($groupsArr) != 0) {
-            $classTop[] = ' * In addition, the middleware is added to the following groups:';
-            $classTop[] = ' * <ul>';
-
-            foreach ($groupsArr as $gName) {
-                $classTop[] = " * <li>$gName</li>";
-            }
-            $classTop[] = ' * </ul>';
-        }
-        $classTop[] = ' */';
-        $classTop[] = 'class '.$this->getWriter()->getName().' extends AbstractMiddleware {';
-        $this->append($classTop);
-
-        $this->_writeConstructor($middlewareName, $priority, $groupsArr);
-
-        $this->append([
-            '/**',
-            ' * Execute a set of instructions before accessing the application.',
-            ' */',
-            'public function before() {',
-            
-        ], 1);
-        $this->append('//TODO: Implement the action to perform before processing the request.', 2);
-        $this->append([
-            '}',
-            '/**',
-            ' * Execute a set of instructions after processing the request and before sending back the response.',
-            ' */',
-            'public function after() {'
-        ], 1);
-        $this->append('//TODO: Implement the action to perform after processing the request.', 2);
-        $this->append([
-            '}',
-            '/**',
-            ' * Execute a set of instructions after sending the response.',
-            ' */',
-            'public function afterSend() {'
-        ], 1);
-        $this->append('//TODO: Implement the action to perform after sending the request.', 2);
-        $this->append('}', 1);
-
-        $this->append("}");
-
+        
+        $this->setWriter(new MiddlewareClassWriter([], $middlewareName, $priority, $groupsArr));
         $this->writeClass();
     }
     private function _getGroups() {
@@ -120,26 +67,5 @@ class CreateMiddleware extends CreateClassHelper {
 
             return false;
         });
-    }
-    private function _writeConstructor($name, $priority,array $groups) {
-        $this->append([
-            '/**',
-            ' * Creates new instance of the class.',
-            ' */',
-            'public function __construct(){',
-            
-        ], 1);
-        $this->append("parent::__construct('$name');", 2);
-        $this->append("\$this->setPriority($priority);", 2);
-
-        if (count($groups) > 0) {
-            $this->append('$this->addToGroups([', 2);
-
-            foreach ($groups as $gName) {
-                $this->append("'$gName',", 3);
-            }
-            $this->append(']);', 2);
-        }
-        $this->append('}', 1);
     }
 }

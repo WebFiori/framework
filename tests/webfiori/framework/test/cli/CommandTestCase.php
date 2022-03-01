b637ca0036cf80e71c788c75444852d85c22b062
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-03-02 01:13:31 +0300

    Create CommandTestCase.php

---
+++ tests/webfiori/framework/test/cli/CommandTestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace webfiori\framework\test\cli;
+use PHPUnit\Framework\TestCase;
+use webfiori\framework\cli\CommandTest;
+/**
+ * Description of CommandTest
+ *
+ * @author Ibrahim
+ */
+class CommandTestCase extends TestCase {
+    /**
+     * @test
+     */
+    public function test00() {
+        $test = new CommandTest(TESTS_PATH.DIRECTORY_SEPARATOR.'input.txt', TESTS_PATH.DIRECTORY_SEPARATOR.'output.txt');
+        $test->runCommand(new TestCommand('hello', [
+            'name' => []
+        ]), [
+            'name' => 'Ibrahim'
+        ]);
+        $this->assertTrue($test->assertExitStatusEquals(0));
+        $this->assertTrue($test->assertOutputEquals([
+            "Hello Ibrahim!",
+            "Ok",
+            ""
+        ]));
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $test = new CommandTest(TESTS_PATH.DIRECTORY_SEPARATOR.'input.txt', TESTS_PATH.DIRECTORY_SEPARATOR.'output.txt');
+        $test->runCommand(new TestCommand('hello', [
+            'name' => []
+        ]), [
+            'name' => 'Hassan Hussain'
+        ]);
+        $this->assertTrue($test->assertExitStatusEquals(0));
+        $this->assertTrue($test->assertOutputEquals([
+            "Hello Hassan Hussain!",
+            "Ok",
+            ""
+        ]));
+    }
+}

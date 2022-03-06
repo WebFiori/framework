c0f4ca6a5b1752c79e8ae8233259ab075ee87ab6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-03-07 01:24:29 +0300

    Update HelpCommandTest.php

--- tests/webfiori/framework/test/cli/HelpCommandTest.php
+++ tests/webfiori/framework/test/cli/HelpCommandTest.php
@@ -19,7 +19,7 @@ class HelpCommandTest extends TestCase {
         
         $commandRunner->runCommand(new HelpCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
-        $this->assertTrue($commandRunner->isOutputEquals([
+        $this->assertEquals([
             'Usage:',
             '    command [arg1 arg2="val" arg3...]',
             '',
@@ -64,7 +64,7 @@ class HelpCommandTest extends TestCase {
             '        Update application settings which are stored in the class "AppConfig".',
             '',
             '',
-        ], $this));
+        ], $commandRunner->getOutputsArray());
     }
     /**
      * @test
@@ -134,5 +134,80 @@ class HelpCommandTest extends TestCase {
             '',
             '',
         ], $this));
+    }
+    /**
+     * @test
+     */
+    public function test02() {
+        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input.txt', TESTS_PATH.DS.'output.txt');
+        
+        $commandRunner->runCommand(new HelpCommand(), [
+            '--command-name' => 'help'
+        ]);
+        $this->assertEquals(0, $commandRunner->getExitStatus());
+        $this->assertEquals([
+        '    help',
+        '        Display CLI Help. To display help for specific command, use the argument "--command-name" with this command.',
+        '',
+        '    Supported Arguments:',
+        '               --command-name: [Optional] An optional command name. If provided, help will be specific to the given command only.',
+        //'                       --ansi: [Optional] Force the use of ANSI output.',
+        //'                    --no-ansi: [Optional] Force the output to not use ANSI.',
+        ''
+        ], $commandRunner->getOutputsArray());
+    }
+    /**
+     * @test
+     */
+    public function test03() {
+        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input.txt', TESTS_PATH.DS.'output.txt');
+        
+        $commandRunner->runCommand(new HelpCommand(), [
+            '--command-name' => 'no-command'
+        ]);
+        $this->assertEquals(0, $commandRunner->getExitStatus());
+        $this->assertEquals([
+        'Error: Command \'no-command\' is not supported.',
+        ''
+        ], $commandRunner->getOutputsArray());
+    }
+    /**
+     * @test
+     */
+    public function test04() {
+        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input.txt', TESTS_PATH.DS.'output.txt');
+        
+        $commandRunner->runCommand(new HelpCommand(), [
+            '--command-name' => 'no-command'
+        ]);
+        $this->assertEquals(0, $commandRunner->getExitStatus());
+        $this->assertEquals([
+        'Error: Command \'no-command\' is not supported.',
+        ''
+        ], $commandRunner->getOutputsArray());
+    }
+    /**
+     * @test
+     */
+    public function test05() {
+        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input.txt', TESTS_PATH.DS.'output.txt');
+        
+        $commandRunner->runCommand(new HelpCommand(), [
+            '--command-name' => 'cron'
+        ]);
+        $this->assertEquals(0, $commandRunner->getExitStatus());
+        $this->assertEquals([
+        '    cron',
+        '               Run CRON Scheduler',
+        '',
+        '    Supported Arguments:',
+        '                            p: [Optional] CRON password. If it is set in CRON, then it must be provided here.',
+        '                      --check: [Optional] Run a check aginst all jobs to check if it is time to execute them or not.',
+        '                      --force: [Optional] Force a specific job to execute.',
+        '                   --job-name: [Optional] The name of the job that will be forced to execute.',
+        '              --show-job-args: [Optional] If this one is provided with job name and a job has custom execution args, they will be shown.',
+        '                   --show-log: [Optional] If set, execution log will be shown after execution is completed.',
+        ''
+        ], $commandRunner->getOutputsArray());
     }
 }

2aab9ffa5bf87544b44ac1687588492f7677abd1
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-03-20 02:32:20 +0300

    Update CreateCommandTest.php

--- tests/webfiori/framework/test/cli/CreateCommandTest.php
+++ tests/webfiori/framework/test/cli/CreateCommandTest.php
@@ -15,28 +15,36 @@ class CreateCommandTest extends TestCase {
      * @test
      */
     public function testCreateBackgroundJob00() {
-        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input-streams'.DS.'create-job-00.txt', TESTS_PATH.DS.'output.txt');
+        $commandRunner = new CommandRunner([
+            '3',
+            'SuperCoolJob',
+            'app\jobs',
+            'app\jobs',
+            'The Greatest Job',
+            'The job will do nothing.',
+            'N',
+            '',
+        ]);
         $commandRunner->runCommand(new CreateCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
         $this->assertTrue($commandRunner->isOutputEquals([
-            'What would you like to create?',
-            '0: Database table class.',
-            '1: Entity class from table.',
-            '2: Web service.',
-            '3: Background job.',
-            '4: Middleware.',
-            '5: Database table from class.',
-            '6: CLI Command.',
-            '7: Theme.',
-            '8: Quit. <--',
-            'Enter a name for the new class:',
-            'Enter an optional namespace for the class: Enter = "app\jobs"',
-            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter =".' "app\jobs"',
-            'Enter a name for the job:',
-            'Provide short description of what does the job will do:',
-            'Would you like to add arguments to the job?(y/N)',
-            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS.'jobs".',
-            ""
+            "What would you like to create?\n",
+            "0: Database table class.\n",
+            "1: Entity class from table.\n",
+            "2: Web service.\n",
+            "3: Background job.\n",
+            "4: Middleware.\n",
+            "5: Database table from class.\n",
+            "6: CLI Command.\n",
+            "7: Theme.\n",
+            "8: Quit. <--\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = \"app\jobs\"\n",
+            "Where would you like to store the ". "class? (must be a directory inside '".ROOT_DIR."') Enter ="." \"app\jobs\"\n",
+            "Enter a name for the job:\n",
+            "Provide short description of what does the job will do:\n",
+            "Would you like to add arguments to the job?(y/N)\n",
+            "Info: New class was created at \"".ROOT_DIR.DS.'app'.DS."jobs\".\n",
         ], $this));
         $this->assertTrue(class_exists('\\app\\jobs\\SuperCoolJob'));
         $this->removeClass('\\app\\jobs\\SuperCoolJob');
@@ -45,28 +53,36 @@ class CreateCommandTest extends TestCase {
      * @test
      */
     public function testCreateMiddleware00() {
-        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input-streams'.DS.'create-middleware-00.txt', TESTS_PATH.DS.'output.txt');
+        $commandRunner = new CommandRunner([
+            '4',
+            'NewCoolMd',
+            'app\middleware',
+            'app\middleware',
+            'Check is authorized',
+            '22',
+            '',
+            '',
+        ]);
         $commandRunner->runCommand(new CreateCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
         $this->assertTrue($commandRunner->isOutputEquals([
-            'What would you like to create?',
-            '0: Database table class.',
-            '1: Entity class from table.',
-            '2: Web service.',
-            '3: Background job.',
-            '4: Middleware.',
-            '5: Database table from class.',
-            '6: CLI Command.',
-            '7: Theme.',
-            '8: Quit. <--',
-            'Enter a name for the new class:',
-            'Enter an optional namespace for the class: Enter = "app\middleware"',
-            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter =".' "app\middleware"',
-            'Enter a name for the middleware:',
-            'Enter middleware priority: Enter = "0"',
-            'Would you like to add the middleware to a group?(y/N)',
-            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS.'middleware".',
-            ""
+            "What would you like to create?\n",
+            "0: Database table class.\n",
+            "1: Entity class from table.\n",
+            "2: Web service.\n",
+            "3: Background job.\n",
+            "4: Middleware.\n",
+            "5: Database table from class.\n",
+            "6: CLI Command.\n",
+            "7: Theme.\n",
+            "8: Quit. <--\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = \"app\middleware\"\n",
+            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter ="." \"app\middleware\"\n",
+            "Enter a name for the middleware:\n",
+            "Enter middleware priority: Enter = \"0\"\n",
+            "Would you like to add the middleware to a group?(y/N)\n",
+            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS."middleware\".\n",
         ], $this));
         $this->assertTrue(class_exists('\\app\\middleware\\NewCoolMdMiddleware'));
         $this->removeClass('\\app\\middleware\\NewCoolMdMiddleware');
@@ -75,28 +91,36 @@ class CreateCommandTest extends TestCase {
      * @test
      */
     public function testCreateCommand00() {
-        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input-streams'.DS.'create-command-00.txt', TESTS_PATH.DS.'output.txt');
+        $commandRunner = new CommandRunner([
+            '6',
+            'NewCLI',
+            'app\commands',
+            'app\commands',
+            'print-hello',
+            'Prints \'Hello World\' in the console.',
+            'N',
+            '',
+        ]);
         $commandRunner->runCommand(new CreateCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
         $this->assertTrue($commandRunner->isOutputEquals([
-            'What would you like to create?',
-            '0: Database table class.',
-            '1: Entity class from table.',
-            '2: Web service.',
-            '3: Background job.',
-            '4: Middleware.',
-            '5: Database table from class.',
-            '6: CLI Command.',
-            '7: Theme.',
-            '8: Quit. <--',
-            'Enter a name for the new class:',
-            'Enter an optional namespace for the class: Enter = "app\commands"',
-            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter =".' "app\commands"',
-            'Enter a name for the command:',
-            'Give a short description of the command:',
-            'Would you like to add arguments to the command?(y/N)',
-            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS.'commands".',
-            ""
+            "What would you like to create?\n",
+            "0: Database table class.\n",
+            "1: Entity class from table.\n",
+            "2: Web service.\n",
+            "3: Background job.\n",
+            "4: Middleware.\n",
+            "5: Database table from class.\n",
+            "6: CLI Command.\n",
+            "7: Theme.\n",
+            "8: Quit. <--\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = \"app\commands\"\n",
+            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter ="." \"app\commands\"\n",
+            "Enter a name for the command:\n",
+            "Give a short description of the command:\n",
+            "Would you like to add arguments to the command?(y/N)\n",
+            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS."commands\".\n",
         ], $this));
         $this->assertTrue(class_exists('\\app\\commands\\NewCLICommand'));
         $this->removeClass('\\app\\commands\\NewCLICommand');
@@ -105,52 +129,64 @@ class CreateCommandTest extends TestCase {
      * @test
      */
     public function testCreateWebService00() {
-        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input-streams'.DS.'create-web-service-00.txt', TESTS_PATH.DS.'output.txt');
+        $commandRunner = new CommandRunner([
+            '2',
+            'NewWeb',
+            '',
+            '',
+            'get-hello',
+            '0',
+            'y',
+            '6',
+            'name',
+            'n',
+            'n',
+            '',
+        ]);
         $commandRunner->runCommand(new CreateCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
         $this->assertTrue($commandRunner->isOutputEquals([
-            'What would you like to create?',
-            '0: Database table class.',
-            '1: Entity class from table.',
-            '2: Web service.',
-            '3: Background job.',
-            '4: Middleware.',
-            '5: Database table from class.',
-            '6: CLI Command.',
-            '7: Theme.',
-            '8: Quit. <--',
-            'Enter a name for the new class:',
-            'Enter an optional namespace for the class: Enter = "app\apis"',
-            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter =".' "app\apis"',
-            'Enter a name for the new web service:',
-            'Request method:',
-            '0: GET <--',
-            '1: HEAD',
-            '2: POST',
-            '3: PUT',
-            '4: DELETE',
-            '5: TRACE',
-            '6: OPTIONS',
-            '7: PATCH',
-            '8: CONNECT',
-            'Would you like to add request parameters to the service?(y/N)',
-            'Choose parameter type:',
-            '0: array <--',
-            '1: boolean',
-            '2: email',
-            '3: double',
-            '4: integer',
-            '5: json-obj',
-            '6: string',
-            '7: url',
-            'Enter a name for the request parameter:',
-            'Is this parameter optional?(Y/n)',
-            'Success: New parameter added to the service \'get-hello\'.',
-            'Would you like to add another parameter?(y/N)',
-            'Creating the class...',
-            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS.'apis".',
-            'Info: Don\'t forget to add the service to a services manager.',
-            ""
+            "What would you like to create?\n",
+            "0: Database table class.\n",
+            "1: Entity class from table.\n",
+            "2: Web service.\n",
+            "3: Background job.\n",
+            "4: Middleware.\n",
+            "5: Database table from class.\n",
+            "6: CLI Command.\n",
+            "7: Theme.\n",
+            "8: Quit. <--\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = \"app\apis\"\n",
+            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter ="." \"app\apis\"\n",
+            "Enter a name for the new web service:\n",
+            "Request method:\n",
+            "0: GET <--\n",
+            "1: HEAD\n",
+            "2: POST\n",
+            "3: PUT\n",
+            "4: DELETE\n",
+            "5: TRACE\n",
+            "6: OPTIONS\n",
+            "7: PATCH\n",
+            "8: CONNECT\n",
+            "Would you like to add request parameters to the service?(y/N)\n",
+            "Choose parameter type:\n",
+            "0: array <--\n",
+            "1: boolean\n",
+            "2: email\n",
+            "3: double\n",
+            "4: integer\n",
+            "5: json-obj\n",
+            "6: string\n",
+            "7: url\n",
+            "Enter a name for the request parameter:\n",
+            "Is this parameter optional?(Y/n)\n",
+            "Success: New parameter added to the service 'get-hello'.\n",
+            "Would you like to add another parameter?(y/N)\n",
+            "Creating the class...\n",
+            'Info: New class was created at "'.ROOT_DIR.DS.'app'.DS."apis\".\n",
+            "Info: Don't forget to add the service to a services manager.\n",
         ], $this));
         $this->assertTrue(class_exists('\\app\\apis\\NewWebService'));
         $this->removeClass('\\app\\apis\\NewWebService');
@@ -159,26 +195,31 @@ class CreateCommandTest extends TestCase {
      * @test
      */
     public function testCreateTheme00() {
-        $commandRunner = new CommandRunner(TESTS_PATH.DS.'input-streams'.DS.'create-theme-00.txt', TESTS_PATH.DS.'output.txt');
+        $commandRunner = new CommandRunner([
+            '7',
+            'NewTest',
+            'themes\\fiori',
+            'themes\\fiori',
+            '',
+        ]);
         $commandRunner->runCommand(new CreateCommand());
         $this->assertEquals(0, $commandRunner->getExitStatus());
         $this->assertTrue($commandRunner->isOutputEquals([
-            'What would you like to create?',
-            '0: Database table class.',
-            '1: Entity class from table.',
-            '2: Web service.',
-            '3: Background job.',
-            '4: Middleware.',
-            '5: Database table from class.',
-            '6: CLI Command.',
-            '7: Theme.',
-            '8: Quit. <--',
-            'Enter a name for the new class:',
-            'Enter an optional namespace for the class: Enter = "themes"',
-            'Where would you like to store the '. "class? (must be a directory inside '".ROOT_DIR."') Enter =".' "themes\fiori"',
-            'Creating theme at "'.ROOT_DIR.DS.'themes'.DS.'fiori"...',
-            'Info: New class was created at "'.ROOT_DIR.DS.'themes'.DS.'fiori".',
-            ""
+            "What would you like to create?\n",
+            "0: Database table class.\n",
+            "1: Entity class from table.\n",
+            "2: Web service.\n",
+            "3: Background job.\n",
+            "4: Middleware.\n",
+            "5: Database table from class.\n",
+            "6: CLI Command.\n",
+            "7: Theme.\n",
+            "8: Quit. <--\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = \"themes\"\n",
+            "Where would you like to store the ". "class? (must be a directory inside '".ROOT_DIR."') Enter ="." \"themes\\fiori\"\n",
+            'Creating theme at "'.ROOT_DIR.DS.'themes'.DS."fiori\"...\n",
+            'Info: New class was created at "'.ROOT_DIR.DS.'themes'.DS."fiori\".\n",
         ], $this));
         $this->assertTrue(class_exists('\\themes\\fiori\\NewTestTheme'));
         $this->removeClass('\\themes\\fiori\\NewTestTheme');

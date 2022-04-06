39df3542307aa033e8623b04581ac1b6c19cf3f7
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-04-07 01:48:19 +0300

    Create ArrayIndexOutOfBoundsException.php

---
+++ webfiori/framework/exceptions/ArrayIndexOutOfBoundsException.php
@@ -0,0 +1,13 @@
+<?php
+namespace webfiori\framework\exceptions;
+
+use Exception;
+/**
+ * An exception which is thrown to indicate that array index is out of bound
+ * for indexed arrays.
+ *
+ * @author Ibrahim
+ */
+class ArrayIndexOutOfBoundsException extends Exception{
+    
+}

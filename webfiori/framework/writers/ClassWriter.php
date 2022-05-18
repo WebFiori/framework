777f2bc3fc5432a01dd2003a1753702c09a5c0a7
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-19 00:25:28 +0300

    Refactoring Class Writers

--- webfiori/framework/writers/ClassWriter.php
+++ webfiori/framework/writers/ClassWriter.php
@@ -1,32 +1,9 @@
 <?php
-/**
- * MIT License
- *
- * Copyright (c) 2020 Ibrahim BinAlshikh, phMysql library.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-namespace webfiori\framework\cli\writers;
+namespace webfiori\framework\writers;
 
 use webfiori\framework\File;
 /**
- * A utility class which is used as a helper class with the command 'create'.
+ * A utility class which is used as a helper class to auto-generate PHP classes.
  * This class can be used to write .php classes.
  * 
  * @author Ibrahim
@@ -34,6 +11,7 @@ use webfiori\framework\File;
  * @version 1.0.1
  */
 abstract class ClassWriter {
+    private $suffix;
     private $useArr;
     /**
      * The generated class as string.
@@ -66,61 +44,153 @@ abstract class ClassWriter {
     /**
      * Creates new instance of the class.
      * 
+     * @param string $name The name of the class that will be created. If not provided, the 
+     * string 'NewClass' is used.
+     * 
+     * @param string $path The location at which the class will be created on. If not 
+     * provided, the constant ROOT_DIR is used.
+     * 
+     * @param string $ns The namespace that the class will belong to. If not provided, 
+     * the global namespace is used.
+     * 
      * @param array $classInfoArr An associative array that contains the information 
      * of the class that will be created. The array must have the following indices: 
-     * <ul>
-     * <li><b>name</b>: The name of the class that will be created. If not provided, the 
-     * string 'NewClass' is used.</li>
-     * <li><b>namespace</b>: The namespace that the class will belong to. If not provided, 
-     * the namespace 'webfiori' is used.</li>
-     * <li><b>path</b>: The location at which the class will be created on. If not 
-     * provided, the constant ROOT_DIR is used. </li>
-     * 
-     * </ul>
      */
-    public function __construct($classInfoArr = []) {
-        if (isset($classInfoArr['namespace']) && strlen($classInfoArr['namespace']) != 0) {
-            $this->ns = $classInfoArr['namespace'];
-        } else {
-            $this->ns = 'webfiori';
-        }
-
-        if (isset($classInfoArr['path'])) {
-            $this->path = $classInfoArr['path'];
-        } else {
-            $this->path = ROOT_DIR;
-        }
-
-        if (isset($classInfoArr['name'])) {
-            $this->className = $classInfoArr['name'];
-        } else {
-            $this->className = 'NewClass';
-        }
+    public function __construct(string $name, string $path, string $namespace) {
+        $this->suffix = '';
         $this->useArr = [];
+        if (!$this->setClassName($name)) {
+            $this->setClassName('NewClass');
+        }
+
+        if (!$this->setPath($path)) {
+            $this->setPath(ROOT_DIR);
+        }
+
+        if (!$this->setNamespace($namespace)) {
+            $this->setNamespace('\\');
+        }
+        
     }
+    /**
+     * Sets a string as a suffix to the class name.
+     * 
+     * @param string $suffix A string to append to class name such as 'Table' or
+     * 'Service'. It must be a string which is considered as valid class name.
+     * 
+     * @return bool If set, the method will return true. False otherises.
+     */
+    public function setSuffix(string $suffix) : bool {
+        if (self::isValidClassName($suffix)) {
+            $this->suffix = $suffix;
+            $this->className = $this->fixClassName($this->className);
+            return true;
+        }
+        return false;
+    }
+    private function fixClassName($className) {
+        $suffix = $this->getSuffix();
+        if ($suffix != '') {
+            $subSuffix = substr($className, strlen($className) - strlen($suffix));
+
+            if ($subSuffix != $suffix) {
+                return $className;
+            } else {
+                return substr($className, 0, -strlen($suffix));
+            }
+        } else {
+            return $className;
+        }
+    }
+
     /**
      * Sets the namespace of the class that will be created.
      * 
      * @param string $namespace
+     * 
+     * @return boolean If the namespace is successfully set, the method will return true.
+     * Other than that, false is returned.
      */
-    public function setNamespace($namespace) {
-        $this->ns = $namespace;
+    public function setNamespace(string $namespace) {
+        $trimmed = trim($namespace);
+        
+        if (strlen($trimmed) == 0) {
+            return false;
+        }
+        $this->ns = $trimmed;
     }
     /**
      * Sets the location at which the class will be created on.
      * 
      * @param string $path A string that represents folder path.
+     * 
+     * @return boolean If the path is successfully set, the method will return true.
+     * Other than that, false is returned.
      */
-    public function setPath($path) {
+    public function setPath(string $path) : bool {
+        $trimmed = trim($path);
+        
+        if (strlen($trimmed) == 0) {
+            return false;
+        }
         $this->path = $path;
+        return true;
     }
     /**
      * Sets the name of the class will be created on.
      * 
      * @param string $name A string that represents class name.
+     * 
+     * @return boolean If the name is successfully set, the method will return true.
+     * Other than that, false is returned.
      */
-    public function setClassName($name) {
-        $this->className = $name;
+    public function setClassName(string $name) : bool {
+        $trimmed = trim($name);
+        if (self::isValidClassName($trimmed)) {
+            $this->className = $this->fixClassName($trimmed);
+            
+            return true;
+        }
+
+        return false;
+    }
+    /**
+     * Returns the string that will be appended to the name of the class.
+     * 
+     * @return string The string that will be appended to the name of the class.
+     * Default is empty string.
+     */
+    public function getSuffix() : string {
+        return $this->suffix;
+    }
+    /**
+     * Checks if a given string represents a valid class name or not.
+     * 
+     * @param string $name A string to check such as 'My_Super_Class'.
+     * 
+     * @return bool If the given string is a valid class name, the method
+     * will return true. False otherwise.
+     */
+    public static function isValidClassName(string $name) : bool {
+        $len = strlen($name);
+
+        if ($len > 0) {
+            for ($x = 0 ; $x < $len ; $x++) {
+                $char = $name[$x];
+
+                if ($x == 0 && $char >= '0' && $char <= '9') {
+                    return false;
+                }
+
+                if (!(($char <= 'Z' && $char >= 'A') || ($char <= 'z' && $char >= 'a') || ($char >= '0' && $char <= '9') || $char == '_')) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
     }
     /**
      * Appends a string or array of strings to the string that represents the
@@ -143,6 +213,9 @@ abstract class ClassWriter {
             $this->_a($strOrArr, $tabsCount);
         }
     }
+    /**
+     * Writes the top section of the class that contains class comment.
+     */
     public abstract function writeClassComment();
     public abstract function writeClassDeclaration();
     public abstract function writeClassBody();
@@ -162,7 +235,7 @@ abstract class ClassWriter {
      * 
      * @return array An array of strings.
      */
-    public function getUseStatements() {
+    public function getUseStatements() : array {
         return $this->useArr;
     }
     /**
@@ -187,9 +260,10 @@ abstract class ClassWriter {
      * The method will add the tag '&lt;php?' in addition to namespace declaration.
      */
     public function writeNsDeclaration() {
+        $nsStr = $this->getNamespace() != '\\' ? 'namespace '.$this->getNamespace().";" : '';
         $this->append([
             '<?php',
-            'namespace '.$this->getNamespace().";\n",
+            $nsStr,
             ''
         ]);
     }
@@ -204,38 +278,52 @@ abstract class ClassWriter {
      * 
      * @since 1.0.1
      */
-    public function getAbsolutePath() {
-        return $this->getPath().DS.$this->className.'.php';
+    public function getAbsolutePath() : string {
+        return $this->getPath().DS.$this->getName().'.php';
     }
     /**
      * Returns the name of the class that will be created.
      * 
-     * @return string The name of the class that will be created.
+     * Note that the suffix will be appended to the name of the class
+     * if it is set.
+     * 
+     * @return string The name of the class that will be created. Default is
+     * 'NewClass'
      * 
      * @since 1.0
      */
-    public function getName() {
-        return $this->className;
+    public function getName() : string {
+        
+        return $this->className.$this->getSuffix();
     }
     /**
      * Returns the namespace at which the generated class will be added to.
      * 
      * @return string The namespace at which the generated class will be added to.
+     * default is '\' which is the global namespace.
      * 
      * @since 1.0
      */
-    public function getNamespace() {
+    public function getNamespace() : string {
         return $this->ns;
     }
     /**
      * Returns the location at which the class will be created on.
      * 
      * @return string The location at which the class will be created on.
+     * default is the value of the contstant ROOT_DIR
      * 
      * @since 1.0
      */
-    public function getPath() {
+    public function getPath() : string {
         return $this->path;
+    }
+    /**
+     * Remove the class at which the writer represents.
+     */
+    public function removeClass() {
+        $classFile = new File($this->getAbsolutePath());
+        $classFile->remove();
     }
     /**
      * Write the new class to a .php file.

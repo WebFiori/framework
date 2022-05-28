e0fc2888b51f3654d3478ba51d9a60ab44418f72
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-28 17:17:04 +0300

    Update WebServiceWritterTest.php

--- tests/webfiori/framework/test/writers/WebServiceWritterTest.php
+++ tests/webfiori/framework/test/writers/WebServiceWritterTest.php
@@ -3,6 +3,7 @@ namespace webfiori\framework\test\writers;
 
 use webfiori\framework\cli\writers\WebServiceWriter;
 use PHPUnit\Framework\TestCase;
+use webfiori\http\AbstractWebService;
 /**
  * Description of CronWritterTest
  *
@@ -20,5 +21,30 @@ class WebServiceWritterTest extends TestCase {
         $this->assertEquals([
             "webfiori\\framework\\EAbstractWebService",
         ], $writter->getUseStatements());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $writter = new WebServiceWriter();
+        $writter->setClassName('SuperService');
+        $this->assertEquals('SuperService', $writter->getName());
+        $this->assertEquals('app\\apis', $writter->getNamespace());
+        $this->assertEquals('Service', $writter->getSuffix());
+        $this->assertEquals([
+            "webfiori\\framework\\EAbstractWebService",
+        ], $writter->getUseStatements());
+        $writter->addRequestParam([
+            'name' => 'param-1',
+            'type' => 'boolean'
+        ]);
+        $writter->addRequestMethod('get');
+        $writter->writeClass();
+        $clazz = '\\'.$writter->getNamespace().'\\'.$writter->getName();
+        $this->assertTrue(class_exists($clazz));
+        $clazzObj = new $clazz();
+        $this->assertTrue($clazzObj instanceof AbstractWebService);
+        $this->assertEquals(1, count($clazzObj->getParameters()));
+        $writter->removeClass();
     }
 }

3ef052799a3dbcdfef34e8ab05c4bd49aba28329
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-30 00:55:50 +0300

    Update ThemeWritterTest.php

--- tests/webfiori/framework/test/writers/ThemeWritterTest.php
+++ tests/webfiori/framework/test/writers/ThemeWritterTest.php
@@ -3,6 +3,7 @@ namespace webfiori\framework\test\writers;
 
 use webfiori\framework\cli\writers\ThemeClassWriter;
 use PHPUnit\Framework\TestCase;
+use webfiori\framework\Theme;
 /**
  * Description of CronWritterTest
  *
@@ -20,5 +21,28 @@ class ThemeWritterTest extends TestCase {
         $this->assertEquals([
         ], $writter->getUseStatements());
         $this->assertEquals('New Theme', $writter->getThemeName());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $writter = new ThemeClassWriter();
+        $writter->setClassName('SuperHotNew');
+        $writter->setThemeName('Cool Theme');
+        $this->assertEquals('SuperHotNewTheme', $writter->getName());
+        $writter->setNamespace('app\\themes\\cool');
+        $writter->setPath(ROOT_DIR.DS.APP_DIR_NAME.DS.'themes'.DS.'cool');
+        $this->assertEquals('app\\themes\\cool', $writter->getNamespace());
+        $this->assertEquals('Theme', $writter->getSuffix());
+        $this->assertEquals([
+        ], $writter->getUseStatements());
+        $this->assertEquals('Cool Theme', $writter->getThemeName());
+        $writter->writeClass();
+        $clazz = $writter->getName(true);
+        $this->assertTrue(class_exists($clazz));
+        $writter->removeClass();
+        $clazzObj = new $clazz();
+        $this->assertTrue($clazzObj instanceof Theme);
+        $this->assertEquals('Cool Theme', $clazzObj->getName());
     }
 }

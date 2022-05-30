d532cc16b56d2984c142205040ab3cff39d46838
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-30 18:38:00 +0300

    Update MiddlewareWritterTest.php

--- tests/webfiori/framework/test/writers/MiddlewareWritterTest.php
+++ tests/webfiori/framework/test/writers/MiddlewareWritterTest.php
@@ -3,6 +3,7 @@ namespace webfiori\framework\test\writers;
 
 use webfiori\framework\cli\writers\MiddlewareClassWriter;
 use PHPUnit\Framework\TestCase;
+use webfiori\framework\middleware\AbstractMiddleware;
 /**
  * Description of CronWritterTest
  *
@@ -26,5 +27,45 @@ class MiddlewareWritterTest extends TestCase {
         $this->assertEquals('New Middleware', $writter->getMiddlewareName());
         $this->assertEquals(0, $writter->getMiddlewarePriority());
         $this->assertEquals([], $writter->getGroups());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $writter = new MiddlewareClassWriter();
+        $writter->setClassName('XMd');
+        $writter->setMiddlewareName('Super Middleware');
+        $writter->setMiddlewarePriority(100);
+        $writter->addGroup('one-group');
+        $writter->addGroup('two-group');
+        $writter->addGroup('global');
+        $this->assertEquals('XMdMiddleware', $writter->getName());
+        $this->assertEquals('app\\middleware', $writter->getNamespace());
+        $this->assertEquals('Middleware', $writter->getSuffix());
+        $this->assertEquals([
+            "webfiori\\framework\\middleware\\AbstractMiddleware",
+            "webfiori\\framework\\SessionsManager",
+            "webfiori\\http\\Request",
+            "webfiori\\http\\Response",
+        ], $writter->getUseStatements());
+        $this->assertEquals('Super Middleware', $writter->getMiddlewareName());
+        $this->assertEquals(100, $writter->getMiddlewarePriority());
+        $this->assertEquals([
+            'one-group', 'two-group', 'global'
+        ], $writter->getGroups());
+        
+        $writter->writeClass();
+        
+        $clazz = $writter->getName(true);
+        $this->assertTrue(class_exists($clazz));
+        $writter->removeClass();
+        $md = new $clazz();
+        $this->assertTrue($md instanceof AbstractMiddleware);
+        
+        $this->assertEquals('Super Middleware', $md->getName());
+        $this->assertEquals(100, $md->getPriority());
+        $this->assertEquals([
+            'one-group', 'two-group', 'global'
+        ], $md->getGroups());
     }
 }

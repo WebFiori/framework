4148722e25e0c4dc729f7f8467449c244fb4c016
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-30 19:00:06 +0300

    Update TablewWritterTest.php

--- tests/webfiori/framework/test/writers/TablewWritterTest.php
+++ tests/webfiori/framework/test/writers/TablewWritterTest.php
@@ -3,6 +3,8 @@ namespace webfiori\framework\test\writers;
 
 use webfiori\framework\cli\writers\TableClassWriter;
 use PHPUnit\Framework\TestCase;
+use webfiori\database\mssql\MSSQLTable;
+use webfiori\database\mysql\MySQLTable;
 /**
  * Description of CronWritterTest
  *
@@ -20,5 +22,29 @@ class TablewWritterTest extends TestCase {
         $this->assertEquals([
             
         ], $writter->getUseStatements());
+        $this->assertNull($writter->getEntityName());
+        $this->assertNull($writter->getEntityNamespace());
+        $this->assertNull($writter->getEntityPath());
+        $this->assertTrue($writter->getTable() instanceof MySQLTable);
+        $this->assertFalse($writter->getTable() instanceof MSSQLTable);
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $writter = new TableClassWriter();
+        $writter->setClassName('CoolT');
+        $writter->setEntityInfo('MyEntity', 'app\\entity', ROOT_DIR.DS.APP_DIR_NAME.DS.'entity', true);
+        $this->assertEquals('CoolTTable', $writter->getName());
+        $this->assertEquals('app\\database', $writter->getNamespace());
+        $this->assertEquals('Table', $writter->getSuffix());
+        $this->assertEquals([
+            
+        ], $writter->getUseStatements());
+        $this->assertEquals('MyEntity', $writter->getEntityName());
+        $this->assertEquals('app\\entity', $writter->getEntityNamespace());
+        $this->assertEquals(ROOT_DIR.DS.APP_DIR_NAME.DS.'entity', $writter->getEntityPath());
+        $this->assertTrue($writter->getTable() instanceof MySQLTable);
+        $this->assertFalse($writter->getTable() instanceof MSSQLTable);
     }
 }

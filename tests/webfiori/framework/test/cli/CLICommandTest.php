979c66a974a6e285a410d4133a8f1efa662e6521
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-06-26 00:32:45 +0300

    Update CLICommandTest.php

--- tests/webfiori/framework/test/cli/CLICommandTest.php
+++ tests/webfiori/framework/test/cli/CLICommandTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace webfiori\framework\test\cli;
 use PHPUnit\Framework\TestCase;
+use webfiori\framework\cli\Runner;
 
 class CLICommandTest extends TestCase {
     /**
@@ -78,5 +79,223 @@ class CLICommandTest extends TestCase {
         $this->assertEquals('<NO DESCRIPTION>', $argDetails->getDescription());
         $this->assertTrue($argDetails->isOptional());
         $this->assertEquals([], $argDetails->getAllowedValues());
+    }
+    /**
+     * @test
+     */
+    public function testAddArg04() {
+        $command = new TestCommand('new');
+        $this->assertTrue($command->addArg('default-options', [
+            'optional' => true
+        ]));
+        $this->assertFalse($command->addArg('default-options'));
+    }
+    /**
+     * @test
+     */
+    public function testClear00() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clearConsole();
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\ec"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testClear01() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clearLine();
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[2K\r"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testClear02() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clear(1);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[1D \e[1D\e[1C"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testClear03() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clear(2);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[1D \e[1D\e[1D \e[1D\e[2C"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testClear05() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clear(1, false);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[1C \e[2D"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testClear06() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->clear(2, false);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[1C  \e[3D"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testMove00() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->moveCursorDown(3);
+        $command->moveCursorDown(6);
+        $command->moveCursorLeft(88);
+        $command->moveCursorRight(4);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[3B\e[6B\e[88D\e[4C"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testMove01() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello', [
+            'name' => [
+                
+            ]
+        ]);
+        Runner::runCommand($command, [
+            'name' => 'Ibrahim'
+        ]);
+        $command->moveCursorDown(3);
+        $command->moveCursorDown(6);
+        $command->moveCursorLeft(88);
+        $command->moveCursorRight(4);
+        $this->assertEquals([
+            "Hello Ibrahim!\n",
+            "Ok\n",
+            "\e[3B\e[6B\e[88D\e[4C"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testPrintList00() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello');
+        Runner::runCommand($command);
+        $command->printList([
+            'one',
+            'two',
+            'three'
+        ]);
+        $this->assertEquals([
+            "Hello !\n",
+            "Ok\n",
+            "- one\n",
+            "- two\n",
+            "- three\n"
+        ], Runner::getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testPrintList01() {
+        Runner::setInput([]);
+        $command = new TestCommand('hello');
+        Runner::runCommand($command, [
+            '--ansi'
+        ]);
+        $command->printList([
+            'one',
+            'two',
+            'three'
+        ]);
+        $this->assertEquals([
+            "\e[31mHello !\e[0m\n",
+            "Ok\n",
+            "\e[32m- \e[0mone\n",
+            "\e[32m- \e[0mtwo\n",
+            "\e[32m- \e[0mthree\n"
+        ], Runner::getOutput());
     }
 }

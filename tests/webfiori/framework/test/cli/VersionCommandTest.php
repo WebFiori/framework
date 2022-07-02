62df0a16d063ed32298926df07149a15ad64df31
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-03 01:05:27 +0300

    Update VersionCommandTest.php

--- tests/webfiori/framework/test/cli/VersionCommandTest.php
+++ tests/webfiori/framework/test/cli/VersionCommandTest.php
@@ -4,9 +4,7 @@ namespace webfiori\framework\test\cli;
 
 use PHPUnit\Framework\TestCase;
 use webfiori\framework\cli\commands\VersionCommand;
-use webfiori\framework\cli\Runner;
-use webfiori\framework\cli\ArrayInputStream;
-use webfiori\framework\cli\ArrayOutputStream;
+use webfiori\cli\Runner;
 /**
  * Description of VersionCommandTest
  *
@@ -17,25 +15,13 @@ class VersionCommandTest extends TestCase {
      * @test
      */
     public function test00() {
-        Runner::setInput([]);
-        
-        $this->assertEquals(0, Runner::runCommand(new VersionCommand()));
+        $runner = new Runner();
+        $runner->setInput();
+        $this->assertEquals(0, $runner->runCommand(new VersionCommand()));
         $this->assertEquals([
             'Framework Version: '.WF_VERSION."\n",
             'Release Date: '.WF_RELEASE_DATE."\n",
             'Version Type: '.WF_VERSION_TYPE."\n",
-        ], Runner::getOutput());
-    }
-    /**
-     * @TEST
-     */
-    public function test01() {
-        Runner::setInput([]);
-        $this->assertEquals(0, Runner::runCommand(new VersionCommand()));
-        $this->assertEquals([
-            'Framework Version: '.WF_VERSION."\n",
-            'Release Date: '.WF_RELEASE_DATE."\n",
-            'Version Type: '.WF_VERSION_TYPE."\n",
-        ], Runner::getOutput());
+        ], $runner->getOutput());
     }
 }

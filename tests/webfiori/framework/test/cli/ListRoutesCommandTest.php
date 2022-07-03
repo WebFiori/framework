6772561127116ec11a4b4a65d0df713c316fd806
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-04 00:31:41 +0300

    Create ListRoutesCommandTest.php

---
+++ tests/webfiori/framework/test/cli/ListRoutesCommandTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace webfiori\framework\test\cli;
+
+use webfiori\cli\Runner;
+use webfiori\framework\cli\commands\ListRoutesCommand;
+use PHPUnit\Framework\TestCase;
+use webfiori\framework\WebFioriApp;
+use webfiori\framework\router\Router;
+/**
+ * Description of ListRoutesCommandTest
+ *
+ * @author Ibrahim
+ */
+class ListRoutesCommandTest extends TestCase {
+    /**
+     * @test
+     */
+    public function test00() {
+        $runner = WebFioriApp::getRunner();
+        $runner->setInput();
+        $runner->setArgsVector([
+            'webfiori',
+            'list-routes'
+        ]);
+        $runner->start();
+        $this->assertEquals([
+            
+        ], $runner->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $runner = WebFioriApp::getRunner();
+        $runner->setInput();
+        $runner->setArgsVector([
+            'webfiori',
+            'list-routes'
+        ]);
+        Router::addRoute([
+            'path' => 'xyz',
+            'route-to' => Router::class
+        ]);
+        $runner->start();
+        $this->assertEquals([
+            "https://example.com/xyz  =>  webfiori\\framework\\router\\Router\n"
+        ], $runner->getOutput());
+        Router::removeAll();
+    }
+}

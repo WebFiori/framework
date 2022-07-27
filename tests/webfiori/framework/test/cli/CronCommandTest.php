ffae61f5072eccc4022437a4be3d5f27bed0dd44
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-27 12:35:52 +0300

    Update CronCommandTest.php

--- tests/webfiori/framework/test/cli/CronCommandTest.php
+++ tests/webfiori/framework/test/cli/CronCommandTest.php
@@ -40,7 +40,7 @@ class CronCommandTest extends TestCase {
         ]);
         $this->assertEquals(0, $runner->start());
         $this->assertEquals([
-            "Total number of jobs: 0\n",
+            "Total number of jobs: 5\n",
             "Executed Jobs: 0\n",
             "Successfully finished jobs:\n",
             "    <NONE>\n",
@@ -62,6 +62,120 @@ class CronCommandTest extends TestCase {
         $this->assertEquals(-1, $runner->start());
         $this->assertEquals([
             "Error: The argument 'p' is missing. It must be provided if cron password is set.\n",
+        ], $runner->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test03() {
+        $runner = WebFioriApp::getRunner();
+        $runner->setInput([
+            '0'
+        ]);
+        $runner->setArgsVector([
+            'webfiori',
+            'cron',
+            '--force',
+            'p' => '123456'
+        ]);
+        $this->assertEquals(0, $runner->start());
+        $this->assertEquals([
+            "Select one of the scheduled jobs to force:\n",
+            "0: Fail 1\n",
+            "1: Fail 2\n",
+            "2: Fail 3\n",
+            "3: Success Every Minute\n",
+            "4: Success 1\n",
+            "5: Cancel <--\n",
+            "Total number of jobs: 5\n",
+            "Executed Jobs: 1\n",
+            "Successfully finished jobs:\n",
+            "    <NONE>\n",
+            "Failed jobs:\n",
+            "    Fail 1\n"
+        ], $runner->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test04() {
+        $runner = WebFioriApp::getRunner();
+        $runner->setInput([
+            '0'
+        ]);
+        $runner->setArgsVector([
+            'webfiori',
+            'cron',
+            '--force',
+            '--show-log',
+            'p' => '123456'
+        ]);
+        $this->assertEquals(0, $runner->start());
+        $this->assertEquals([
+            "Select one of the scheduled jobs to force:\n",
+            "0: Fail 1\n",
+            "1: Fail 2\n",
+            "2: Fail 3\n",
+            "3: Success Every Minute\n",
+            "4: Success 1\n",
+            "5: Cancel <--\n",
+            "Running job(s) check...\n",
+            "Forceing job 'Fail 1' to execute...\n",
+            "Active job: \"Fail 1\" ...\n",
+            "Calling the method app\jobs\Fail1TestJob::execute()\n",
+            "Calling the method app\jobs\Fail1TestJob::onFail()\n",
+            "Calling the method app\jobs\Fail1TestJob::afterExec()\n",
+            "Check finished.\n",
+            "Total number of jobs: 5\n",
+            "Executed Jobs: 1\n",
+            "Successfully finished jobs:\n",
+            "    <NONE>\n",
+            "Failed jobs:\n",
+            "    Fail 1\n"
+        ], $runner->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test05() {
+        $runner = WebFioriApp::getRunner();
+        $runner->setInput([
+            '1'
+        ]);
+        $runner->setArgsVector([
+            'webfiori',
+            'cron',
+            '--force',
+            '--show-log',
+            'p' => '123456'
+        ]);
+        $this->assertEquals(0, $runner->start());
+        $this->assertEquals([
+            "Select one of the scheduled jobs to force:\n",
+            "0: Fail 1\n",
+            "1: Fail 2\n",
+            "2: Fail 3\n",
+            "3: Success Every Minute\n",
+            "4: Success 1\n",
+            "5: Cancel <--\n",
+            "Running job(s) check...\n",
+            "Forceing job 'Fail 2' to execute...\n",
+            "Active job: \"Fail 2\" ...\n",
+            "Calling the method app\jobs\Fail2TestJob::execute()\n",
+            "WARNING: An exception was thrown while performing the operation app\jobs\Fail2TestJob::execute. The output of the job might be not as expected.\n",
+            "Exception class: Error\n",
+            "Exception message: Call to undefined method app\jobs\Fail2TestJob::undefined()\n",
+            "Thrown in: Fail2TestJob\n",
+            "Line: 36\n",
+            "Calling the method app\jobs\Fail2TestJob::onFail()\n",
+            "Calling the method app\jobs\Fail2TestJob::afterExec()\n",
+            "Check finished.\n",
+            "Total number of jobs: 5\n",
+            "Executed Jobs: 1\n",
+            "Successfully finished jobs:\n",
+            "    <NONE>\n",
+            "Failed jobs:\n",
+            "    Fail 2\n"
         ], $runner->getOutput());
     }
 }

d730bbca850ef794f4ed9dc37480c5ddcc361302
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-31 01:53:13 +0300

    Update WebServiceWriter.php

--- webfiori/framework/cli/writers/WebServiceWriter.php
+++ webfiori/framework/cli/writers/WebServiceWriter.php
@@ -123,7 +123,7 @@ class WebServiceWriter extends ClassWriter {
             if (($param->getType() == 'string' || $param->getType() == 'url' || $param->getType() == 'email') && strlen($param->getDefault()) > 0) {
                 $toAppend = "'default' => '".$param->getDefault()."',";
             } else if ($param->getType() == 'boolean') {
-                $toAppend = $param->getDefault() === true ? "'default' => false" : "'default' => false";
+                $toAppend = $param->getDefault() === true ? "'default' => false," : "'default' => false,";
             }
             $this->append($toAppend, 4);
         }

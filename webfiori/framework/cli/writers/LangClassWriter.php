01666add66c0bb2cc538cc5771838caab8e313b0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-31 01:53:10 +0300

    Update LangClassWriter.php

--- webfiori/framework/cli/writers/LangClassWriter.php
+++ webfiori/framework/cli/writers/LangClassWriter.php
@@ -47,7 +47,6 @@ class LangClassWriter extends ClassWriter {
             " * Creates new instance of the class.",
             " */",
             $this->f('__construct'),
-            'public function __construct() {'
         ], 1);
 
         $this->append([

d00b3f55edee54016ad0de73386b9fcfc83e68c9
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-08-01 19:05:33 +0300

    Update DBClassWriter.php

--- webfiori/framework/writers/DBClassWriter.php
+++ webfiori/framework/writers/DBClassWriter.php
@@ -10,8 +10,9 @@
  */
 namespace webfiori\framework\writers;
 
-use webfiori\framework\writers\ClassWriter;
 use webfiori\database\Table;
+use webfiori\framework\DB;
+use webfiori\framework\writers\ClassWriter;
 /**
  * A class which is used to create a database access controller.
  *
@@ -34,7 +35,7 @@ class DBClassWriter extends ClassWriter {
     public function __construct($className, $ns, Table $table) {
         parent::__construct($className, $ns, $ns);
         $this->associatedTable = $table;
-        $this->addUseStatement('\\webfiori\\framework\\DB');
+        $this->addUseStatement(DB::class);
         $mapper = $this->getTable()->getEntityMapper();
         $this->entityName = $mapper->getEntityName();
         $this->addUseStatement($mapper->getNamespace().'\\'.$mapper->getEntityName());
@@ -47,7 +48,7 @@ class DBClassWriter extends ClassWriter {
             "/**",
             " * Creates new instance of the class.",
             " */",
-            'public function __construct() {'
+            $this->f('__construct')
         ], 1);
 
         $this->append([
@@ -62,7 +63,7 @@ class DBClassWriter extends ClassWriter {
             " * @param ".$this->getEntityName().' $entity An object that holds record information.',
             " *",
             " */",
-            'public function addRecord('.$this->getEntityName().' $entity) {'
+            $this->f('addRecord', [$this->getEntityName() => 'entity'])
         ], 1);
         $this->append('}', 1);
         $this->append([
@@ -72,19 +73,10 @@ class DBClassWriter extends ClassWriter {
             " * @param ".$this->getEntityName().' $entity An object that holds record information.',
             " *",
             " */",
-            'public function deleteRecord('.$this->getEntityName().' $entity) {'
-        ], 1);
-        $this->append([
-            "/**",
-            " * Returns the information of a record from the table '".$this->getTable()->getName()."'.",
-            " *",
-            " * @return ".$this->getEntityName().'|null If a record with given information exist,',
-            " * The method will return an object which holds all record information.",
-            " * Other than that, null is returned.",
-            " */",
-            'public function getRecord() {'
+            $this->f('deleteRecord', [$this->getEntityName() => 'entity']),
         ], 1);
         $this->append('}', 1);
+        $this->writeGetRecord();
         $this->append([
             "/**",
             " * Returns all the records from the table '".$this->getTable()->getName()."'.",
@@ -107,6 +99,50 @@ class DBClassWriter extends ClassWriter {
         $this->append('}', 1);
         
         $this->append('}', 0);
+    }
+    private function writeGetRecord() {
+        $cols = $this->getUniqueColsKeys();
+        $paramsArr = [];
+        $where = [];
+        foreach ($cols as $key) {
+            $colObj = $this->getTable()->getColByKey($key);
+            $paramsArr[$colObj->getPHPType()] = $colObj->getNormalName();
+            $where[] = count($where) == 0 ? "->where('$key', '=', $".$colObj->getNormalName().")"
+                    : "->andWhere('$key', '=', $'.$colObj->getNormalName())" ;
+        }
+        $this->append([
+            "/**",
+            " * Returns the information of a record from the table '".$this->getTable()->getName()."'.",
+            " *",
+            " * @return ".$this->getEntityName().'|null If a record with given information exist,',
+            " * The method will return an object which holds all record information.",
+            " * Other than that, null is returned.",
+            " */",
+            $this->f('getRecord', $paramsArr, $this->getEntityName())
+        ], 1);
+        if (count($paramsArr) != 0) {
+            $this->append("return \$this->table('".$this->getTable()->getNormalName()."')", 2);
+            $this->append("->select()", 3);
+            $this->append($where, 3);
+            $this->append("->execute()", 3);
+            $this->append("->map(function (array \$records) {", 3);
+            $this->append("if (count(\$records) == 1) {", 4);
+            $this->append("return [".$this->getEntityName().'::map($records[0])];', 5);
+            $this->append("}", 4);
+            $this->append("return [];", 4);
+            $this->append("});", 3);
+        }
+        $this->append('}', 1);
+    }
+    
+    private function getUniqueColsKeys() {
+        $table = $this->getTable();
+        $recordUniqueCols  = $table->getPrimaryKeyColsKeys();
+        
+        if (count($recordUniqueCols ) == 0) {
+            $recordUniqueCols = $table->getUniqueColsKeys();
+        } 
+        return $recordUniqueCols;
     }
 
     public function writeClassComment() {

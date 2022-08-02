c740b9359bbe640ab5f627eb92db9e6c00e63484
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-08-02 16:30:06 +0300

    Update DBClassWritterTest.php

--- tests/webfiori/framework/test/cli/DBClassWritterTest.php
+++ tests/webfiori/framework/test/cli/DBClassWritterTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace webfiori\framework\test\cli;
 
+use tables\EmployeeInfoTable;
+use tables\PositionInfoTable;
 use tables\UserInfoTable;
 use webfiori\framework\writers\DBClassWriter;
 
@@ -23,5 +25,30 @@ class DBClassWritterTest extends CreateTestCase {
         $this->assertTrue(class_exists($writter->getName(true)));
         $this->removeClass($writter->getName(true));
     }
-
+    /**
+     * @test
+     */
+    public function test01() {
+        $table = new EmployeeInfoTable();
+        $mapper = $table->getEntityMapper();
+        $mapper->setEntityName('Employee');
+        $mapper->setNamespace('webfiori\\entity');
+        $writter = new DBClassWriter('EmployeeDB', 'webfiori\\db', $table);
+        $writter->writeClass();
+        $this->assertTrue(class_exists($writter->getName(true)));
+        $this->removeClass($writter->getName(true));
+    }
+    /**
+     * @test
+     */
+    public function test02() {
+        $table = new PositionInfoTable();
+        $mapper = $table->getEntityMapper();
+        $mapper->setEntityName('Position');
+        $mapper->setNamespace('webfiori\\entity');
+        $writter = new DBClassWriter('PositionDB', 'webfiori\\db', $table);
+        $writter->writeClass();
+        $this->assertTrue(class_exists($writter->getName(true)));
+        $this->removeClass($writter->getName(true));
+    }
 }

dc5ba22b8f5903f359bc1c1928d659a4acb2b685
Ibrahim <ibinshikh@hotmail.com>
2022-11-10 15:46:55 +0300

    Update AddCommandTest.php

--- tests/webfiori/framework/test/cli/AddCommandTest.php
+++ tests/webfiori/framework/test/cli/AddCommandTest.php
@@ -112,7 +112,7 @@ class AddCommandTest extends TestCase {
             "Give your connection a friendly name: Enter = '$connName'\n",
             "Trying to connect to the database...\n",
             "Error: Unable to connect to the database.\n",
-            "Error: \n",
+            "Unable to connect to database: 2002 - No such file or directory\n",
             "Would you like to store connection information anyway?(y/N)\n",
             "Success: Connection information was stored in application configuration.\n"
         ], $runner->getOutput());
@@ -155,7 +155,8 @@ class AddCommandTest extends TestCase {
             "Database name:\n",
             "Give your connection a friendly name: Enter = '$connName'\n",
             "Trying to connect to the database...\n",
-            "Error: Unable to connect to database: 2002 - No connection could be made because the target machine actively refused it\n",
+            "Error: Unable to connect to the database.\n",
+            "Unable to connect to database: 2002 - No such file or directory\n",
             "Would you like to store conne...y/N)\n",
         ], $runner->getOutput());
     }

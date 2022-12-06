5ab55fa9c678c9d8fb0ea0ffa1beb35395b9b94b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-12-07 00:47:49 +0300

    Update SessionsManagerTest.php

--- tests/webfiori/framework/test/session/SessionsManagerTest.php
+++ tests/webfiori/framework/test/session/SessionsManagerTest.php
@@ -2,8 +2,13 @@
 namespace webfiori\framework\test\session;
 
 use PHPUnit\Framework\TestCase;
-use webfiori\framework\session\SessionsManager;
+use webfiori\database\ConnectionInfo;
+use webfiori\database\DatabaseException;
+use webfiori\framework\exceptions\SessionException;
+use webfiori\framework\session\DatabaseSessionStorage;
 use webfiori\framework\session\Session;
+use webfiori\framework\session\SessionsManager;
+use webfiori\framework\WebFioriApp;
 
 /**
  * Description of SessionsManagerTest
@@ -151,5 +156,132 @@ class SessionsManagerTest extends TestCase {
         $this->assertEquals([
             'hello='.$sessions[0]->getId().'; expires='.$sessions[0]->getCookie()->getLifetime().'; domain=example.com; path=/; Secure; HttpOnly; SameSite=Lax' 
             ], SessionsManager::getCookiesHeaders());
+    }
+    /**
+     * @test
+     */
+    public function testDatabaseSession00() {
+        $this->expectException(SessionException::class);
+        $this->expectExceptionMessage("Connection 'sessions-connection' was not found in application configuration.");
+        SessionsManager::reset();
+        SessionsManager::setStorage(new DatabaseSessionStorage());
+    }
+    /**
+     * @test
+     */
+    public function testDatabaseSession01() {
+        $this->expectException(DatabaseException::class);
+        $conn = new ConnectionInfo('mysql', 'root', '123456', 'testing_db', '127.0.0.1');
+        $conn->setName('sessions-connection');
+        WebFioriApp::getAppConfig()->addDbConnection($conn);
+        SessionsManager::reset();
+        SessionsManager::setStorage(new DatabaseSessionStorage());
+        SessionsManager::start('hello');
+    }
+    /**
+     * @test
+     */
+    public function testDatabaseSession02() {
+        $conn = new ConnectionInfo('mysql', 'root', '123456', 'testing_db', '127.0.0.1');
+        $conn->setName('sessions-connection');
+        WebFioriApp::getAppConfig()->addDbConnection($conn);
+        SessionsManager::reset();
+        $sto = new DatabaseSessionStorage();
+        $sto->getController()->createTables();
+        $sto->getController()->execute();
+        SessionsManager::setStorage($sto);
+        
+        $this->assertEquals(0, count(SessionsManager::getSessions()));
+        $this->assertNull(SessionsManager::getActiveSession());
+        $this->assertNull(SessionsManager::get('xyz'));
+        $this->assertFalse(SessionsManager::remove('xyz'));
+        $this->assertFalse(SessionsManager::set('xyz','hello'));
+        $this->assertNull(SessionsManager::pull('xyz'));
+        
+        SessionsManager::start('hello');
+        $this->assertEquals(1, count(SessionsManager::getSessions()));
+        $activeSesstion = SessionsManager::getActiveSession();
+        $this->assertFalse($activeSesstion->isRefresh());
+        $this->assertTrue($activeSesstion->isRunning());
+        
+        $this->assertNotNull($activeSesstion);
+        $this->assertEquals('hello', $activeSesstion->getName());
+        $this->assertEquals(7200, $activeSesstion->getDuration());
+        $this->assertEquals(Session::STATUS_NEW, $activeSesstion->getStatus());
+        
+        $activeSesstion->set('var-1', 'Good');
+        $activeSesstion->set('var-2', 'Bad');
+        $activeSesstion->set('var-3', 'Average');
+        $activeSesstion->set('var-4', 'Almost Good');
+        $this->assertEquals('Good', SessionsManager::get('var-1 '));
+        $this->assertEquals('Bad', SessionsManager::get(' var-2 '));
+        $this->assertEquals('Average', SessionsManager::get('var-3 '));
+        $this->assertEquals('Almost Good', SessionsManager::get('var-4 '));
+        
+        SessionsManager::pauseAll();
+        $this->assertFalse($activeSesstion->isRunning());
+        $this->assertNull(SessionsManager::get('var-1 '));
+        $this->assertNull(SessionsManager::get(' var-2 '));
+        $this->assertNull(SessionsManager::get('var-3 '));
+        $this->assertNull(SessionsManager::get('var-4 '));
+        
+        $this->assertNull(SessionsManager::getActiveSession());
+        $this->assertEquals(Session::STATUS_PAUSED, $activeSesstion->getStatus());
+        
+        $activeSesstion->start();
+        $this->assertTrue($activeSesstion->isRunning());
+        
+        $this->assertNotNull(SessionsManager::getActiveSession());
+        $this->assertEquals(Session::STATUS_RESUMED, $activeSesstion->getStatus());
+        $this->assertEquals('Good', SessionsManager::get('var-1 '));
+        $this->assertEquals('Bad', SessionsManager::get(' var-2 '));
+        $this->assertEquals('Average', SessionsManager::get('var-3 '));
+        $this->assertEquals('Almost Good', SessionsManager::get('var-4 '));
+        
+        $this->assertEquals('Good', SessionsManager::pull('var-1 '));
+        $this->assertNull(SessionsManager::pull('var-1 '));
+        $this->assertTrue(SessionsManager::remove('var-2'));
+        $this->assertFalse(SessionsManager::remove('var-2'));
+        $this->assertNull(SessionsManager::get('var-1 '));
+        $this->assertNull(SessionsManager::get(' var-2 '));
+        
+        //Start New Sesstion
+        SessionsManager::start('another-one', [
+            'duration' => 5,
+            'refresh' => true
+        ]);
+        $this->assertFalse($activeSesstion->isRunning());
+        $this->assertEquals(Session::STATUS_PAUSED, $activeSesstion->getStatus());
+        $active2 = SessionsManager::getActiveSession();
+        $this->assertEquals(300, $active2->getDuration());
+        $this->assertTrue($active2->isRefresh());
+        $this->assertTrue($active2->isRunning());
+        $this->assertNull(SessionsManager::get('var-3 '));
+        $this->assertNull(SessionsManager::get(' var-4 '));
+        
+        $active2->set('super-var', 'I m super.');
+        $this->assertEquals('I m super.', SessionsManager::get('super-var'));
+        
+        $active2->close();
+        $this->assertNull(SessionsManager::getActiveSession());
+        $activeSesstion->start();
+        $this->assertNotNull(SessionsManager::getActiveSession());
+        $this->assertNull(SessionsManager::get('super-var'));
+        $this->assertEquals(7200, SessionsManager::getActiveSession()->getDuration());
+        $this->assertEquals(Session::STATUS_PAUSED, $active2->getStatus());
+        
+        SessionsManager::start('another-one');
+        $this->assertEquals(Session::STATUS_PAUSED, $activeSesstion->getStatus());
+        $this->assertEquals(Session::STATUS_RESUMED, $active2->getStatus());
+        $this->assertEquals('I m super.', SessionsManager::get('super-var'));
+        SessionsManager::destroy();
+        $this->assertNull(SessionsManager::getActiveSession());
+        $this->assertEquals(Session::STATUS_KILLED, $active2->getStatus());
+        $active2->start();
+        $this->assertEquals(Session::STATUS_NEW, $active2->getStatus());
+        $this->assertNotNull(SessionsManager::getActiveSession());
+        $this->assertEquals(300, SessionsManager::getActiveSession()->getDuration());
+        $this->assertNull(SessionsManager::get('super-var'));
+        SessionsManager::validateStorage();
     }
 }

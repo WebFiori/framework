3c525b1a668d20753eaee9602e1ac4ee980efd7f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-02-02 00:42:34 +0300

    Update CreateCommandTest.php

--- tests/webfiori/framework/test/cli/CreateCommandTest.php
+++ tests/webfiori/framework/test/cli/CreateCommandTest.php
@@ -65,48 +65,7 @@ class CreateCommandTest extends CreateTestCase {
     }
     
     
-    /**
-     * @test
-     */
-    public function testCreateMiddleware00() {
-        $runner = $runner = WebFioriApp::getRunner();
-        $runner->setArgsVector([
-            'webfiori',
-            'create'
-        ]);
-        $runner->setInput([
-            '4',
-            'NewCoolMd',
-            'app\middleware',
-            'Check is authorized',
-            '22',
-            '',
-            '',
-        ]);
-        
-        $this->assertEquals(0, $runner->start());
-        $this->assertEquals([
-            "What would you like to create?\n",
-            "0: Database table class.\n",
-            "1: Entity class from table.\n",
-            "2: Web service.\n",
-            "3: Background job.\n",
-            "4: Middleware.\n",
-            "5: CLI Command.\n",
-            "6: Theme.\n",
-            "7: Database access class based on table.\n",
-            "8: Complete REST backend (Database table, entity, database access and web services).\n",
-            "9: Quit. <--\n",
-            "Enter a name for the new class:\n",
-            "Enter an optional namespace for the class: Enter = 'app\middleware'\n",
-            "Enter a name for the middleware:\n",
-            "Enter middleware priority: Enter = '0'\n",
-            "Would you like to add the middleware to a group?(y/N)\n",
-            'Info: New class was created at "'.ROOT_PATH.DS.'app'.DS."middleware\".\n",
-        ], $runner->getOutput());
-        $this->assertTrue(class_exists('\\app\\middleware\\NewCoolMdMiddleware'));
-        $this->removeClass('\\app\\middleware\\NewCoolMdMiddleware');
-    }
+    
     /**
      * @test
      */

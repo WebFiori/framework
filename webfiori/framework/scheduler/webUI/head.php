df2afa7f54c9eedb8b9329914b002908699639e1
Ibrahim <ibinshikh@hotmail.com>
2023-04-10 02:16:22 +0300

    Create head.php

---
+++ webfiori/framework/scheduler/webUI/head.php
@@ -0,0 +1,20 @@
+<?php
+namespace webfiori\framework\scheduler\webUI;
+?>
+<head>
+    <base href="<?= baseURL()?>">
+    <title>
+        <?= title() ?>
+    </title>
+    <link rel="canonical" href="<?= canonical()?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js">
+    </script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js">
+    </script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/usernane/AJAXRequestJs@2.x.x/AJAXRequest.js">
+    </script>
+</head>

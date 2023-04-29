1250eb07b75462851e996da0407948810d5ddccb
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-04-29 04:16:20 +0300

    Update head.php

--- webfiori/framework/ui/serverErrPage/head.php
+++ webfiori/framework/ui/serverErrPage/head.php
@@ -8,11 +8,9 @@ namespace webfiori\framework\ui\serverErrPage;
     </title>
     <link rel="canonical" href="<?= canonical()?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;cv=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css?cv=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css?cv=1.0">
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js?jv=1.0">
-    </script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js?jv=1.0">
-    </script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.x/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vuetify@2.6.15/dist/vuetify.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.min.js" integrity="sha256-1gHyKSR7Jh0YGBmI9zN7P2UhZRh/PCKhCYIaUOqWoPk=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.6.15/dist/vuetify.min.js" integrity="sha256-Fetn6QcWnO5THt0I/81iDeUNWAIFQ+K3ZGmMCh270UU=" crossorigin="anonymous"></script>
 </head>

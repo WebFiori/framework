39f68df3bd2ec3ca78bd920d711a253dc56754e4
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-05-14 16:35:23 +0300

    Update App.php

--- webfiori/framework/App.php
+++ webfiori/framework/App.php
@@ -563,7 +563,7 @@ class App {
          * 
          * @since 2.1
          */
-        define('WF_VERSION', '3.0.0-RC6');
+        define('WF_VERSION', '3.0.0-RC7');
         /**
          * A constant that tells the type of framework version.
          * 
@@ -579,7 +579,7 @@ class App {
          * 
          * @since 2.1
          */
-        define('WF_RELEASE_DATE', '2023-05-13');
+        define('WF_RELEASE_DATE', '2023-05-14');
     }
 
     /**

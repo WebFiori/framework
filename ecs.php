bc77a920c9a0f4e1b56b2cbfb9b3f1ed9754ddc4
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-16 15:01:08 +0300

    Create ecs.php

---
+++ ecs.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/public',
+        __DIR__ . '/tests',
+        __DIR__ . '/themes',
+        __DIR__ . '/webfiori',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+        // SetList::SPACES,
+        // SetList::ARRAY,
+        // SetList::DOCBLOCK,
+        // SetList::NAMESPACES,
+        // SetList::COMMENTS,
+        // SetList::PSR_12,
+    ]);
+};

6b69ce9f8746e50a7028766933c14d9dbcb3fb6c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2024-01-03 19:25:11 +0300

    Update TableClassWriter.php

--- webfiori/framework/writers/TableClassWriter.php
+++ webfiori/framework/writers/TableClassWriter.php
@@ -368,7 +368,7 @@ class TableClassWriter extends ClassWriter {
                 return 'DataType::VARCHAR';
             } 
             default : {
-                return 'DataType::VARCHAR';
+                return "'mixed'";
             }
         }
     }

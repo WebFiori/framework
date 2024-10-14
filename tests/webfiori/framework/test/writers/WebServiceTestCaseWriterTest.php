df104d97276e6123cd3c2c5550f235dc5d9995f0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2024-10-14 23:44:48 +0300

    test: Updated Test Case

--- tests/webfiori/framework/test/writers/WebServiceTestCaseWriterTest.php
+++ tests/webfiori/framework/test/writers/WebServiceTestCaseWriterTest.php
@@ -1,21 +1,36 @@
 <?php
 namespace webfiori\framework\test\writers;
 
-use PHPUnit\Framework\TestCase;
 use webfiori\framework\scheduler\webServices\ForceTaskExecutionService;
 use webfiori\framework\scheduler\webServices\TasksServicesManager;
+use webfiori\framework\test\cli\CreateTestCase;
 use webfiori\framework\writers\APITestCaseWriter;
 /**
  *
  * @author Ibrahim
  */
-class WebServiceTestCaseWriterTest extends TestCase {
+class WebServiceTestCaseWriterTest extends CreateTestCase {
     /**
      * @test
      */
     public function test00() {
         $w = new APITestCaseWriter(new TasksServicesManager(), ForceTaskExecutionService::class);
+        $this->assertEquals('tests\\apis\\WebServiceTest', $w->getName(true));
         $w->writeClass();
-        $this->assertTrue(true);
+        $this->assertTrue(class_exists($w->getName(true)));
+        $this->removeClass($w->getAbsolutePath());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $w = new APITestCaseWriter(new TasksServicesManager(), ForceTaskExecutionService::class);
+        $w->setClassName('Cool');
+        $w->setNamespace('\\tests\\cool');
+        $w->setPath(ROOT_PATH.DS.'tests'.DS.'cool');
+        $this->assertEquals('tests\\cool\\CoolTest', $w->getName(true));
+        $w->writeClass();
+        $this->assertTrue(class_exists($w->getName(true)));
+        $this->removeClass($w->getAbsolutePath());
     }
 }

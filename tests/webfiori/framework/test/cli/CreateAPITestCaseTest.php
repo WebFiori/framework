4f192a290f914fc6144bf9ab7d4fc1fd512a4bee
Ibrahim <ibinshikh@hotmail.com>
2024-10-15 17:42:19 +0300

    test: Added Additional Test Cases

--- tests/webfiori/framework/test/cli/CreateAPITestCaseTest.php
+++ tests/webfiori/framework/test/cli/CreateAPITestCaseTest.php
@@ -3,10 +3,7 @@ namespace webfiori\framework\test\cli;
 
 use webfiori\framework\App;
 use webfiori\framework\scheduler\webServices\TasksServicesManager;
-use webfiori\framework\writers\ServiceHolder;
-use webfiori\framework\writers\WebServiceWriter;
-use webfiori\http\ParamOption;
-use webfiori\http\ParamType;
+use webfiori\http\WebServicesManager;
 /**
  * @author Ibrahim
  */
@@ -201,6 +198,75 @@ class CreateAPITestCaseTest extends CreateTestCase {
 
         $this->assertEquals([
             "Info: Provided services manager has 0 registered services.\n",
+        ], $runner->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function testCreateAPITestCase06() {
+        $runner = App::getRunner();
+        $runner->setArgsVector([
+            'webfiori',
+            'create',
+            '--c' => 'api-test',
+            '--service' => 'get-tasks',
+        ]);
+        
+
+        $runner->setArgsVector([
+            'webfiori',
+            'create',
+            '--c' => 'api-test',
+            '--service' => 'say-hi-service',
+        ]);
+        $runner->setInputs([
+            '\\tests\\apis\\multiple\\WebService00',
+            '\\tests\\apis\\multiple\\ServicesManager00',
+            'n',
+            '10',
+            '',
+            '',
+            
+        ]);
+        $exitCode = $runner->start();
+        $this->assertEquals(0, $exitCode);
+        $path = ROOT_PATH.DS."tests".DS."tests".DS."apis".DS."multiple";
+        $this->assertEquals([
+            "Please enter services manager information:\n",
+            "Error: Provided class is not an instance of ".WebServicesManager::class."\n",
+            "Please enter services manager information:\n",
+            "Test case will be created with following parameters:\n",
+            "PHPUnit Version: 9\n",
+            'Name: tests\tests\apis\multiple\WebService00Test'."\n",
+            "Path: ".$path."\n",
+            "Would you like to use default parameters?(Y/n)\n",
+            "PHPUnit Version: Enter = '11'\n",
+            "Enter a name for the new class:\n",
+            "Enter an optional namespace for the class: Enter = 'tests\\tests\apis\multiple'\n",
+            "Info: New class was created at \"".$path."\".\n"
+        ], $runner->getOutput());
+        $clazz = '\\tests\\tests\\apis\\multiple\\WebService00Test';
+        $this->assertTrue(class_exists($clazz));
+        $this->removeClass($clazz);
+    }
+    /**
+     * @test
+     */
+    public function testCreateAPITestCase07() {
+        $runner = App::getRunner();
+        $runner->setArgsVector([
+            'webfiori',
+            'create',
+            '--c' => 'api-test',
+            '--manager' => '\\tests\\apis\\emptyService\\Xyz',
+        ]);
+        $runner->setInputs();
+
+        $exitCode = $runner->start();
+        $this->assertEquals(-1, $exitCode);
+
+        $this->assertEquals([
+            "Error: The argument --manager has invalid value.\n",
         ], $runner->getOutput());
     }
 }

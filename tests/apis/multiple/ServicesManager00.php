d4ce07f5f5855b1b36cedfd6ad89f270e385ff91
Ibrahim <ibinshikh@hotmail.com>
2024-10-16 00:34:05 +0300

    Update ServicesManager00.php

--- tests/apis/multiple/ServicesManager00.php
+++ tests/apis/multiple/ServicesManager00.php
@@ -10,5 +10,6 @@ class ServicesManager00 extends WebServicesManager {
     public function __construct(string $version = '1.0.0') {
         parent::__construct($version);
         $this->addService(new WebService00());
+        $this->addService(new WebService01());
     }
 }

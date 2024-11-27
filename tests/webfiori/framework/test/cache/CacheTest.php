277fed8f40ca247f7855a80517ad0a9496e61c9c
Ibrahim <ibinshikh@hotmail.com>
2024-11-27 18:48:36 +0300

    test: Added More Tests to Cache

--- tests/webfiori/framework/test/cache/CacheTest.php
+++ tests/webfiori/framework/test/cache/CacheTest.php
@@ -11,6 +11,7 @@ class CacheTest extends TestCase {
      */
     public function test00() {
         $key = 'first';
+        $this->assertTrue(Cache::isEnabled());
         $data = Cache::get($key, function () {
             return 'This is a test.';
         });
@@ -83,5 +84,63 @@ class CacheTest extends TestCase {
         foreach ($keys as $key) {
             $this->assertFalse(Cache::has($key));
         }
+    }
+    /**
+     * @test
+     */
+    public function test06() {
+        $key = 'bbuu';
+        $this->assertTrue(Cache::isEnabled());
+        Cache::setEnabled(false);
+        $data = Cache::get($key, function () {
+            return 'This is a test.';
+        });
+        $this->assertEquals('This is a test.', $data);
+        $this->assertNull(Cache::get($key));
+        $this->assertFalse(Cache::isEnabled());
+    }
+    /**
+     * @test
+     */
+    public function testSet00() {
+        $key = 'new_cool_key';
+        Cache::setEnabled(true);
+        $this->assertTrue(Cache::isEnabled());
+        $this->assertTrue(Cache::set($key, 'This is a test.', 60, false));
+        $this->assertEquals('This is a test.', Cache::get($key));
+        $item = Cache::getItem($key);
+        $this->assertEquals(60, $item->getTTL());
+        
+        $this->assertFalse(Cache::set($key, 'This is a test.', 60, false));
+        $this->assertEquals('This is a test.', Cache::get($key));
+        $item = Cache::getItem($key);
+        $this->assertEquals(60, $item->getTTL());
+        
+        $this->assertTrue(Cache::set($key, 'This is a test 2.', 660, true));
+        $this->assertEquals('This is a test 2.', Cache::get($key));
+        $item = Cache::getItem($key);
+        $this->assertEquals(660, $item->getTTL());
+    }
+    /**
+     * @test
+     */
+    public function testSetTTL00() {
+        $key = 'new_cool_key2';
+        Cache::setEnabled(true);
+        $this->assertTrue(Cache::isEnabled());
+        $this->assertTrue(Cache::set($key, 'This is a test.', 60, false));
+        $item = Cache::getItem($key);
+        $this->assertEquals(60, $item->getTTL());
+        Cache::setTTL($key, 700);
+        
+        $item = Cache::getItem($key);
+        $this->assertEquals(700, $item->getTTL());
+    }
+    /**
+     * @test
+     */
+    public function testSetTTL01() {
+        $key = 'not exist cool';
+        $this->assertFalse(Cache::setTTL($key, 700));
     }
 }

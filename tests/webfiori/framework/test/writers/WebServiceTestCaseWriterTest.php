5dc4178026478e7c3f07bf89c45a348a8f13ab53
Ibrahim <ibinshikh@hotmail.com>
2024-12-04 14:32:58 +0300

    test: Added Additional Assertions

--- tests/webfiori/framework/test/writers/WebServiceTestCaseWriterTest.php
+++ tests/webfiori/framework/test/writers/WebServiceTestCaseWriterTest.php
@@ -17,6 +17,7 @@ class WebServiceTestCaseWriterTest extends CreateTestCase {
         $w = new APITestCaseWriter(new TasksServicesManager(), ForceTaskExecutionService::class);
         $this->assertEquals('tests\\apis\\WebServiceTest', $w->getName(true));
         $this->assertEquals(9, $w->getPhpUnitVersion());
+        $this->assertEquals(ROOT_PATH.DS.'tests'.DS.'apis'.DS.'WebServiceTest.php', $w->getAbsolutePath());
         $w->writeClass();
         $this->assertTrue(class_exists('\\'.$w->getName(true)));
         unlink($w->getAbsolutePath());
@@ -31,6 +32,7 @@ class WebServiceTestCaseWriterTest extends CreateTestCase {
         $w->setPath(ROOT_PATH.DS.'tests'.DS.'cool');
         $this->assertEquals('tests\\cool\\CoolTest', $w->getName(true));
         $w->writeClass();
+        $this->assertEquals(ROOT_PATH.DS.'tests'.DS.'cool'.DS.'CoolTest.php', $w->getAbsolutePath());
         $this->assertTrue(file_exists($w->getAbsolutePath()));
         require_once $w->getAbsolutePath();
         $this->assertTrue(class_exists('\\'.$w->getName(true)));

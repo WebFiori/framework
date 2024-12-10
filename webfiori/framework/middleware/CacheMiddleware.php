19837d95432382d54508b1a6e0f7a693ce42b08c
Ibrahim <ibinshikh@hotmail.com>
2024-12-11 00:41:58 +0300

    doc: Added Cache Middleware Docs

--- webfiori/framework/middleware/CacheMiddleware.php
+++ webfiori/framework/middleware/CacheMiddleware.php
@@ -11,17 +11,33 @@ use webfiori\http\Response;
 
 class CacheMiddleware extends AbstractMiddleware {
     private $fromCache;
+    /**
+     * Creates new instance of the class.
+     * 
+     * By default, the middleware is part of the group 'web' and 'api'.
+     * The priority of the middleware is 50.
+     */
     public function __construct() {
         parent::__construct('cache');
         $this->setPriority(50);
         $this->addToGroups(['web', 'api']);
         $this->fromCache = false;
     }
+    /**
+     * Checks if the response is loaded from the cache or caching must be performed.
+     *
+     * @param Request $request An object that represents the request that
+     * will be received.
+     *
+     * @param Response $response An object that represents the response
+     * that will be sent back.
+     *
+     */
     public function after(Request $request, Response $response) {
         
         if (!$this->fromCache) {
             $uriObj = Router::getRouteUri();
-        
+            
             if ($uriObj !== null) {
                 $key = $this->getKey();
                 $data = [
@@ -33,11 +49,28 @@ class CacheMiddleware extends AbstractMiddleware {
             }
         }
     }
-
+    /**
+     * This method will do nothing.
+     *
+     * @param Request $request An object that represents the request that
+     * will be received.
+     *
+     * @param Response $response An object that represents the response
+     * that will be sent back.
+     */
     public function afterSend(Request $request, Response $response) {
         
     }
-
+    /**
+     * Attempt to load an item from the cache and send the response back if
+     * cached.
+     *
+     * @param Request $request An object that represents the request that
+     * will be received.
+     *
+     * @param Response $response An object that represents the response
+     * that will be sent back.
+     */
     public function before(Request $request, Response $response) {
         $key = $this->getKey();
         $data = Cache::get($key);
@@ -52,7 +85,19 @@ class CacheMiddleware extends AbstractMiddleware {
             $response->send();
         }
     }
-    private function getKey() {
+    /**
+     * Creates the key of cache item.
+     * 
+     * This method will attempt to use 3 items to create a unique key. The items include:
+     * <ul>
+     * <li>Requested URI</li>
+     * <li>Session ID (if applicable)</li>
+     * <li>Authorization header (if applicable)</li>
+     * </ul>
+     * 
+     * @return string
+     */
+    public function getKey() : string {
         $key = Request::getUri()->getUri(true, true);
             
         //Following steps are used to make cached response unique per user.

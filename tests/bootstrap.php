b86f3f249676ca0c72f4db4bfda08edce76398a6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2024-12-24 16:44:50 +0300

    test: Fix Bootstrap Script

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,6 +23,8 @@ fprintf(STDOUT, "PHP Version: '".PHP_VERSION."'\n");
 fprintf(STDOUT, "Version ID: '".PHP_VERSION_ID."'\n");
 fprintf(STDOUT, "Bootstrap Path: '".__DIR__."'\n");
 fprintf(STDOUT,"Tests Directory: '".TESTS_DIRECTORY."'.\n");
+$ROOT = substr(__DIR__, 0, strlen(__DIR__) - strlen(TESTS_DIRECTORY));
+fprintf(STDOUT,"Project Directory: '".$ROOT."'.\n");
 fprintf(STDOUT,'Include Path: \''.get_include_path().'\''."\n");
 fprintf(STDOUT,"Tryning to load the class 'ClassLoader'...\n");
 $isAutoloaderLoaded = false;
@@ -72,16 +74,16 @@ ClassLoader::get([
         'app'
     ],
     'define-root' => true,
-    'root' => __DIR__,
+    'root' => $ROOT,
     'on-load-failure' => 'do-nothing'
 ]);
 fprintf(STDOUT,'Autoloader Initialized.'."\n");
 fprintf(STDOUT,"---------------------------------\n");
 fprintf(STDOUT,"Initializing application...\n");
-App::initiate('app');
+App::initiate('app', 'public', $ROOT);
 App::start();
 fprintf(STDOUT,'Done.'."\n");
-fprintf(STDOUT,'Root Directory: \''.ClassLoader::get()->root().'\'.'."\n");
+fprintf(STDOUT,'Autoload Root Directory: \''.ClassLoader::get()->root().'\'.'."\n");
 define('TESTS_PATH', ClassLoader::get()->root().$DS.TESTS_DIRECTORY);
 fprintf(STDOUT,'Tests Path: '.TESTS_PATH."\n");
 fprintf(STDOUT,'App Path: '.APP_PATH."\n");

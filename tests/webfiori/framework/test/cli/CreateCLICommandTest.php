3b606561ab7b6eee617ffdf5fd39fd379e17b683
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-01-30 01:40:29 +0300

    Update CreateCLICommandTest.php

--- tests/webfiori/framework/test/cli/CreateCLICommandTest.php
+++ tests/webfiori/framework/test/cli/CreateCLICommandTest.php
@@ -38,6 +38,7 @@ class CreateCLICommandTest extends CreateTestCase {
             "6: Theme.\n",
             "7: Database access class based on table.\n",
             "8: Complete REST backend (Database table, entity, database access and web services).\n",
+            "9: Web service test case.\n",
             "10: Database migration.\n",
             "11: Quit. <--\n",
             "Enter a name for the new class:\n",

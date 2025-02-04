54936c8e2c4f3da04d2f652a152ac23c97dfe473
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-02-04 23:42:52 +0300

    Update CreateTestCase.php

--- tests/webfiori/framework/test/cli/CreateTestCase.php
+++ tests/webfiori/framework/test/cli/CreateTestCase.php
@@ -6,7 +6,7 @@ use webfiori\file\File;
 
 class CreateTestCase extends TestCase {
     public function removeClass($classPath) {
-        $file = new File(ROOT_PATH.DS.trim($classPath,'\\').'.php');
+        $file = new File(APP_PATH.DS.trim($classPath,'\\').'.php');
         $file->remove();
     }
 }

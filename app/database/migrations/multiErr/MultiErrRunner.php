65821fba105aefda63aafe9e1ce82bbdea82ba2c
Ibrahim <ibinshikh@hotmail.com>
2025-02-06 14:40:36 +0300

    Update MultiErrRunner.php

--- app/database/migrations/multiErr/MultiErrRunner.php
+++ app/database/migrations/multiErr/MultiErrRunner.php
@@ -10,7 +10,7 @@ use const DS;
 class MultiErrRunner extends MigrationsRunner {
     
     public function __construct() {
-        $conn = new ConnectionInfo('mssql', 'sa', '1234567890@Eu', 'testing_db', SQL_SERVER_HOST, 1433, [
+        $conn = new ConnectionInfo('mssql', SQL_SERVER_USER, SQL_SERVER_PASS, SQL_SERVER_DB, SQL_SERVER_HOST, 1433, [
             'TrustServerCertificate' => 'true'
         ]);
         parent::__construct(APP_PATH.'database'.DS.'migrations'.DS.'multiErr', '\\app\\database\\migrations\\multiErr', $conn);

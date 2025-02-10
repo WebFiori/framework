37be67b66d0403b9e3075731cb2897a232b8f508
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-02-11 01:15:22 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -105,7 +105,7 @@ register_shutdown_function(function()
     App::getConfig()->remove();
     JsonDriver::setConfigFileName('super-confx.json');
     App::getConfig()->remove();
-    $conn = new ConnectionInfo(SQL_SERVER_USER, SQL_SERVER_PASS, SQL_SERVER_DB, SQL_SERVER_HOST, 1433, [
+    $conn = new ConnectionInfo('mssql',SQL_SERVER_USER, SQL_SERVER_PASS, SQL_SERVER_DB, SQL_SERVER_HOST, 1433, [
         'TrustServerCertificate' => 'true'
     ]);
     $runner = new MigrationsRunner(APP_PATH, '', $conn);
